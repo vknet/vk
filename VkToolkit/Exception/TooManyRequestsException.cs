dbee31bdb596ff5ed6cf187b569448795d6de883
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2012-07-20 11:13:09 +0400

    Added new cases for exceptions. Added TooManyRequestsException class.

---
+++ VkToolkit/Exception/TooManyRequestsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace VkToolkit.Exception
+{
+    [Serializable]
+    public class TooManyRequestsException : VkApiMethodInvokeException
+    {
+        public TooManyRequestsException() { }
+        public TooManyRequestsException(string message) : base(message) {}
+        public TooManyRequestsException(string message, int code) : base(message, code) {}
+        public TooManyRequestsException(string message, System.Exception ex) : base(message, ex) { }
+        protected TooManyRequestsException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
