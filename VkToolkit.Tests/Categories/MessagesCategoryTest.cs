8e8737d761a805b5e7af42c0abdd73e27d447ba3
Anton Zhidkov <none@none>
2012-08-07 22:21:56 +0400

    Added implementation of GetLongPollServer method in Messages category.

--- VkToolkit.Tests/Categories/MessagesCategoryTest.cs
+++ VkToolkit.Tests/Categories/MessagesCategoryTest.cs
@@ -1,11 +1,23 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using VkToolkit.Categories;
+using VkToolkit.Enums;
 using VkToolkit.Exception;
+using VkToolkit.Utils;
 
 namespace VkToolkit.Tests.Categories
 {
     [TestFixture]
     public class MessagesCategoryTest
     {
+        private MessagesCategory GetMockedMessagesCategory(string url, string json)
+        {
+            var browser = new Mock<IBrowser>();
+            browser.Setup(m => m.GetJson(url)).Returns(json);
+
+            return new MessagesCategory(new VkApi { AccessToken = "token", Browser = browser.Object });
+        }
+
         [Test]
         [ExpectedException(typeof(AccessTokenInvalidException))]
         public void Get_AccessTokenInvalid_ThrowAccessTokenInvalidException()
@@ -147,16 +159,30 @@ namespace VkToolkit.Tests.Categories
 
         [Test]
         [ExpectedException(typeof(AccessTokenInvalidException))]
-        public void GetLongPollServer_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        public void GetLongPollHistory_AccessTokenInvalid_ThrowAccessTokenInvalidException()
         {
 
         }
 
         [Test]
         [ExpectedException(typeof(AccessTokenInvalidException))]
-        public void GetLongPollHistory_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        public void GetLongPollServer_AccessTokenInvalid_ThrowAccessTokenInvalidException()
         {
+            var cat = new MessagesCategory(new VkApi());
+            cat.GetLongPollServer();
+        }
 
+        [Test]
+        public void GetLongPollServer_NormalCase_LongPollServerResponse()
+        {
+            const string url = "https://api.vk.com/method/messages.getLongPollServer?access_token=token";
+            const string json = "{\"response\":{\"key\":\"6f4120988efaf3a7d398054b5bb5d019c5844bz3\",\"server\":\"im46.vk.com\\/im1858\",\"ts\":1627957305}}";
+
+            var response = GetMockedMessagesCategory(url, json).GetLongPollServer();
+
+            Assert.That(response.Key, Is.EqualTo("6f4120988efaf3a7d398054b5bb5d019c5844bz3"));
+            Assert.That(response.Server, Is.EqualTo("im46.vk.com/im1858"));
+            Assert.That(response.Ts, Is.EqualTo(1627957305));
         }
     }
 }
---
+++ VkToolkit/Model/LongPollServerResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace VkToolkit.Model
+{
+    public class LongPollServerResponse
+    {
+        public string Key { get; set; }
+        public string Server { get; set; }
+        public long Ts { get; set; }
+    }
+}
