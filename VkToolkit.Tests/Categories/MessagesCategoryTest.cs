0f2712f6ed607954cbb883e50ad19eba3616c154
Anton Zhidkov <none@none>
2012-08-07 21:54:48 +0400

    Added checking for invalid token tests for all methods in Messages category.

---
+++ VkToolkit.Tests/Categories/MessagesCategoryTest.cs
@@ -0,0 +1,162 @@
+﻿using NUnit.Framework;
+using VkToolkit.Exception;
+
+namespace VkToolkit.Tests.Categories
+{
+    [TestFixture]
+    public class MessagesCategoryTest
+    {
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void Get_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+            
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetDialogs_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetHistory_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetById_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void SearchDialogs_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void Search_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void Send_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void Delete_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void DeleteDialog_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void Restore_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void MarkAsNew_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void MarkAsRead_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void SetActivity_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetLastActivity_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetChat_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void CreateChat_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void EditChat_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetChatUsers_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void AddChatUser_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void RemoveChatUser_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetLongPollServer_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(AccessTokenInvalidException))]
+        public void GetLongPollHistory_AccessTokenInvalid_ThrowAccessTokenInvalidException()
+        {
+
+        }
+    }
+}
