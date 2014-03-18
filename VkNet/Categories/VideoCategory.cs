7a5c407cba62a40ced00d5c3d2ec172c826f1a2d
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-03-18 12:24:31 +0400

    Comments to methods from Video category.

--- VkNet/Categories/VideoCategory.cs
+++ VkNet/Categories/VideoCategory.cs
@@ -18,6 +18,19 @@
             _vk = vk;
         }
 
+        /// <summary>
+        /// Возвращает информацию о видеозаписях.
+        /// </summary>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежат видеозаписи.</param>
+        /// <param name="albumId">идентификатор альбома, видеозаписи из которого нужно вернуть. </param>
+        /// <param name="width">требуемая ширина изображений видеозаписей в пикселах. </param>
+        /// <param name="count">количество возвращаемых видеозаписей. </param>
+        /// <param name="offset">смещение относительно первой найденной видеозаписи для выборки определенного подмножества. </param>
+        /// <param name="extended">определяет, возвращать ли информацию о настройках приватности видео для текущего пользователя. </param>
+        /// <returns>После успешного выполнения возвращает список объектов видеозаписей с дополнительным полем comments, содержащим число комментариев у видеозаписи. </returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         [Pure]
         public ReadOnlyCollection<Video> Get(long? ownerId = null, long? albumId = null, VideoWidth width = VideoWidth.Medium160, int? count = null, int? offset = null, bool extended = false)
         {
@@ -41,6 +54,20 @@
             return response.ToReadOnlyCollectionOf<Video>(x => x);
         }
 
+        /// <summary>
+        /// Редактирует данные видеозаписи на странице пользователя.
+        /// </summary>
+        /// <param name="videoId">идентификатор видеозаписи.</param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <param name="name">новое название для видеозаписи. </param>
+        /// <param name="desc">новое описание для видеозаписи.</param>
+        /// <param name="privacyView">настройки приватности</param>
+        /// <param name="privacyComment">настройки приватности</param>
+        /// <param name="isRepeat">зацикливание воспроизведения видеозаписи.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool Edit(long videoId, long? ownerId = null, string name = null, string desc = null, string privacyView = null, string privacyComment = null, bool isRepeat = false)
         {
             VkErrors.ThrowIfNumberIsNegative(() => videoId);
@@ -60,6 +87,15 @@
             return _vk.Call("video.edit", parameters);
         }
 
+        /// <summary>
+        /// Добавляет видеозапись в список пользователя.
+        /// </summary>
+        /// <param name="videoId">идентификатор видеозаписи.</param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <returns>После успешного выполнения возвращает идентификатор созданной видеозаписи.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public long Add(long videoId, long? ownerId = null)
         {
             VkErrors.ThrowIfNumberIsNegative(() => videoId);
@@ -76,7 +112,22 @@
             return response;
         }
 
-        public Video Save(string name = "", string description = "", bool isPrivate = false, bool isPostToWall = false, string link = "", long? groupId = null, long? albumId = null, bool isRepeat = false)
+        /// <summary>
+        /// Возвращает адрес сервера (необходимый для загрузки) и данные видеозаписи.
+        /// </summary>
+        /// <param name="name">название видеофайла. </param>
+        /// <param name="description">описание видеофайла.</param>
+        /// <param name="isPrivate">указывается true в случае последующей отправки видеозаписи личным сообщением. После загрузки с этим параметром видеозапись не будет отображаться в списке видеозаписей пользователя и не будет доступна другим пользователям по id.</param>
+        /// <param name="isPostToWall">требуется ли после сохранения опубликовать запись с видео на стене</param>
+        /// <param name="link">url для встраивания видео с внешнего сайта, например, с youtube. В этом случае нужно вызвать полученный upload_url не прикрепляя файл, достаточно просто обратиться по этому адресу. </param>
+        /// <param name="groupId">идентификатор сообщества, в которое будет сохранен видеофайл. По умолчанию файл сохраняется на страницу текущего пользователя. </param>
+        /// <param name="albumId">идентификатор альбома, в который будет загружен видео файл.</param>
+        /// <param name="isRepeat">зацикливание воспроизведения видеозаписи.</param>
+        /// <returns>Возвращает объект видеозаписи, который имеет поля upload_url, video_id, title, description, owner_id. </returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        public Video Save(string name = null, string description = null, bool isPrivate = false, bool isPostToWall = false, string link = null, long? groupId = null, long? albumId = null, bool isRepeat = false)
         {
             var parameters = new VkParameters
                 {
@@ -94,6 +145,15 @@
             return _vk.Call("video.save", parameters);
         }
 
+        /// <summary>
+        /// Удаляет видеозапись со страницы пользователя.
+        /// </summary>
+        /// <param name="videoId">идентификатор видеозаписи.</param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool Delete(long videoId, long? ownerId = null)
         {
             VkErrors.ThrowIfNumberIsNegative(() => videoId);
@@ -108,6 +168,15 @@
             return _vk.Call("video.delete", parameters);
         }
 
+        /// <summary>
+        /// Восстанавливает удаленную видеозапись.
+        /// </summary>
+        /// <param name="videoId">идентификатор видеозаписи.</param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool Restore(long videoId, long? ownerId = null)
         {
             VkErrors.ThrowIfNumberIsNegative(() => videoId);
@@ -122,6 +191,21 @@
             return _vk.Call("video.restore", parameters);
         }
 
+        /// <summary>
+        /// Возвращает список видеозаписей в соответствии с заданным критерием поиска.
+        /// </summary>
+        /// <param name="query">строка поискового запроса.</param>
+        /// <param name="sort">вид сортировки. </param>
+        /// <param name="isHd">если true, то поиск производится только по видеозаписям высокого качества. </param>
+        /// <param name="isAdult">фильтр «Безопасный поиск» </param>
+        /// <param name="filters">список критериев, по которым требуется отфильтровать видео.</param>
+        /// <param name="isSearchOwn">искать по видеозаписям пользователя.</param>
+        /// <param name="count">количество возвращаемых видеозаписей.</param>
+        /// <param name="offset">смещение относительно первой найденной видеозаписи для выборки определенного подмножества. </param>
+        /// <returns>После успешного выполнения возвращает список объектов видеозаписей. </returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         [Pure]
         public ReadOnlyCollection<Video> Search(string query, VideoSort sort, bool isHd = false, bool isAdult = false, VideoFilters filters = null, bool isSearchOwn = false, int? count = null, int? offset = null)
         {
@@ -146,10 +230,20 @@
             return response.ToReadOnlyCollectionOf<Video>(x => x);
         }
 
+        /// <summary>
+        /// Возвращает список видеозаписей, на которых отмечен пользователь.
+        /// </summary>
+        /// <param name="userId">идентификатор пользователя. </param>
+        /// <param name="count">количество возвращаемых видеозаписей. </param>
+        /// <param name="offset">смещение относительно первой найденной видеозаписи для выборки определенного подмножества. </param>
+        /// <returns>После успешного выполнения возвращает список объектов видеозаписей.</returns>
+        /// <remarks>
+        /// ЭТОТ МЕТОД ВЫБРАСЫВАЕТ ИСКЛЮЧЕНИЕ НА СЕРВЕРЕ ВК!!!
+        /// </remarks>
         [Pure]
         public ReadOnlyCollection<Video> GetUserVideos(long userId, int? count = null, int? offset = null)
         {
-            throw new NotImplementedException("Метод некорректно работает на самом сервере вконтакте");
+            //throw new NotImplementedException("Метод некорректно работает на самом сервере вконтакте");
 
             VkErrors.ThrowIfNumberIsNegative(userId, "userId");
             VkErrors.ThrowIfNumberIsNegative(count, "count");
@@ -168,6 +262,17 @@
             return response.ToReadOnlyCollectionOf<Video>(x => x);
         }
 
+        /// <summary>
+        /// Возвращает список альбомов видеозаписей пользователя или сообщества.
+        /// </summary>
+        /// <param name="ownerId">идентификатор владельца альбомов (пользователь или сообщество). По умолчанию — идентификатор текущего пользователя. </param>
+        /// <param name="count">количество альбомов, информацию о которых нужно вернуть.</param>
+        /// <param name="offset">смещение, необходимое для выборки определенного подмножества альбомов.</param>
+        /// <param name="extended">True – позволяет получать поля count, photo320 и photo160 для каждого альбома.</param>
+        /// <returns>После успешного выполнения возвращает массив объектов Album</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         [Pure]
         public ReadOnlyCollection<VideoAlbum> GetAlbums(long ownerId, int? count = null, int? offset = null, bool extended = false)
         {
@@ -188,6 +293,15 @@
             return response.ToReadOnlyCollectionOf<VideoAlbum>(x => x);
         }
 
+        /// <summary>
+        /// Создает пустой альбом видеозаписей.
+        /// </summary>
+        /// <param name="title">название альбома.</param>
+        /// <param name="groupId">идентификатор сообщества (если необходимо создать альбом в сообществе).</param>
+        /// <returns>После успешного выполнения возвращает идентификатор созданного альбома.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public long AddAlbum(string title, long? groupId = null)
         {
             VkErrors.ThrowIfNullOrEmpty(() => title);
@@ -205,6 +319,16 @@
             return response["album_id"];
         }
 
+        /// <summary>
+        /// Редактирует название альбома видеозаписей.
+        /// </summary>
+        /// <param name="albumId">идентификатор альбома. </param>
+        /// <param name="title">новое название для альбома. </param>
+        /// <param name="groupId">идентификатор сообщества (если нужно отредактировать альбом, принадлежащий сообществу). </param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool EditAlbum(long albumId, string title, long? groupId = null)
         {
             VkErrors.ThrowIfNullOrEmpty(() => title);
@@ -222,6 +346,15 @@
             return _vk.Call("video.editAlbum", parameters);
         }
 
+        /// <summary>
+        /// Удаляет альбом видеозаписей.
+        /// </summary>
+        /// <param name="albumId">идентификатор альбома.</param>
+        /// <param name="groupId">идентификатор сообщества (если альбом, который необходимо удалить, принадлежит сообществу). </param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool DeleteAlbum(long albumId, long? groupId = null)
         {
             VkErrors.ThrowIfNumberIsNegative(() => albumId);
@@ -236,6 +369,16 @@
             return _vk.Call("video.deleteAlbum", parameters);
         }
 
+        /// <summary>
+        /// Перемещает видеозаписи в альбом.
+        /// </summary>
+        /// <param name="videoIds">список идентификаторов видеороликов. </param>
+        /// <param name="albumId">идентификатор альбома, в который перемещаются видеозаписи. </param>
+        /// <param name="groupId">идентификатор сообщества, которому принадлежат видеозаписи. Если параметр не указан, то работа ведется с альбомом текущего пользователя.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool MoveToAlbum(IEnumerable<long> videoIds, long albumId, long? groupId = null)
         {
             if (videoIds == null)
@@ -250,6 +393,19 @@
             return _vk.Call("video.moveToAlbum", parameters);
         }
 
+        /// <summary>
+        /// Возвращает список комментариев к видеозаписи.
+        /// </summary>
+        /// <param name="videoId">идентификатор видеозаписи. </param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <param name="needLikes">true — будет возвращено дополнительное поле likes. По умолчанию поле likes не возвращается.</param>
+        /// <param name="count">количество комментариев, информацию о которых необходимо вернуть.</param>
+        /// <param name="offset">смещение, необходимое для выборки определенного подмножества комментариев.</param>
+        /// <param name="sort">порядок сортировки комментариев (asc — от старых к новым, desc — от новых к старым)</param>
+        /// <returns>После успешного выполнения возвращает общее количество комментариев и массив объектов Comment.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         [Pure]
         public ReadOnlyCollection<Comment> GetComments(long videoId, long? ownerId = null, bool needLikes = false, int? count = null, int? offset = null, CommentsSort sort = null)
         {
@@ -273,12 +429,28 @@
             return response.ToReadOnlyCollectionOf<Comment>(x => x);
         }
 
+        /// <summary>
+        /// ДАННЫЙ МЕТОД НЕ РЕАЛИЗОВАН!!!
+        /// </summary>
+        /// <param name="attach"></param>
+        /// <returns></returns>
         public long CreateComment(Attachment attach)
         {
             // todo сделать версию с прикладыванием приложения
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Cоздает новый комментарий к видеозаписи.
+        /// </summary>
+        /// <param name="videoId">идентификатор видеозаписи. </param>
+        /// <param name="message">текст комментария</param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <param name="isFromGroup">Данный параметр учитывается, если oid &lt; false (комментарий к видеозаписи группы). True — комментарий будет опубликован от имени группы, false — комментарий будет опубликован от имени пользователя (по умолчанию).</param>
+        /// <returns>После успешного выполнения возвращает идентификатор созданного комментария.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public long CreateComment(long videoId, string message, long? ownerId, bool isFromGroup = false)
         {
             VkErrors.ThrowIfNullOrEmpty(() => message);
@@ -296,6 +468,15 @@
             return _vk.Call("video.createComment", parameters);
         }
 
+        /// <summary>
+        /// Удаляет комментарий к видеозаписи.
+        /// </summary>
+        /// <param name="commentId">идентификатор комментария. </param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool DeleteComment(long commentId, long? ownerId)
         {
             VkErrors.ThrowIfNumberIsNegative(() => commentId);
@@ -305,6 +486,15 @@
             return _vk.Call("video.deleteComment", parameters);
         }
 
+        /// <summary>
+        /// Восстанавливает удаленный комментарий к видеозаписи.
+        /// </summary>
+        /// <param name="commentId">идентификатор удаленного комментария.</param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool RestoreComment(long commentId, long? ownerId)
         {
             VkErrors.ThrowIfNumberIsNegative(() => commentId);
@@ -314,12 +504,27 @@
             return _vk.Call("video.restoreComment", parameters);
         }
 
+        /// <summary>
+        /// ДАННЫЙ МЕТОД НЕ РЕАЛИЗОВАН!!!
+        /// </summary>
+        /// <param name="attach"></param>
+        /// <returns></returns>
         public bool EditComment(Attachment attach)
         {
             // todo add version with attachment
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Изменяет текст комментария к видеозаписи.
+        /// </summary>
+        /// <param name="commentId">идентификатор комментария. </param>
+        /// <param name="message">новый текст комментария </param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool EditComment(long commentId, string message, long? ownerId)
         {
             VkErrors.ThrowIfNullOrEmpty(() => message);
@@ -409,6 +614,18 @@
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Позволяет пожаловаться на видеозапись.
+        /// </summary>
+        /// <param name="videoId">идентификатор видеозаписи. </param>
+        /// <param name="reason">тип жалобы</param>
+        /// <param name="ownerId">идентификатор пользователя или сообщества, которому принадлежит видеозапись.</param>
+        /// <param name="comment">комментарий для жалобы.</param>
+        /// <param name="searchQuery">поисковой запрос, если видеозапись была найдена через поиск.</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool Report(long videoId, VideoReportType reason, long? ownerId, string comment = null, string searchQuery = null)
         {
             VkErrors.ThrowIfNumberIsNegative(() => videoId);
@@ -426,6 +643,16 @@
             return _vk.Call("video.report", parameters);
         }
 
+        /// <summary>
+        /// Позволяет пожаловаться на комментарий к видеозаписи.
+        /// </summary>
+        /// <param name="commentId">идентификатор комментария.</param>
+        /// <param name="ownerId">идентификатор владельца видеозаписи, к которой оставлен комментарий.</param>
+        /// <param name="reason">тип жалобы</param>
+        /// <returns>После успешного выполнения возвращает true.</returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public bool ReportComment(long commentId, long ownerId, VideoReportType reason)
         {
             VkErrors.ThrowIfNumberIsNegative(() => commentId);
