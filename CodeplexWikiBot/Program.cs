8a65f926333acadafdc6ee639c80d5fbac1f7875
vsmall <none@none>
2014-03-30 22:46:10 +0400

    Added very simple program CodePlexWikiBot. It can authorize on codeplex and fill specified wiki page with new text.

---
+++ CodeplexWikiBot/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CodeplexWikiBot
+{
+    using System;
+    using System.Configuration;
+
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2)
+                {
+                    Console.WriteLine("Parameters: url new_wiki_text");
+                    return;
+                }
+
+                string login = ConfigurationManager.AppSettings["login"];
+                string password = ConfigurationManager.AppSettings["password"];
+
+                var browser = new CodeplexAuthorization().Authorize(login, password);
+
+                var url = args[0];
+                var newText = args[1];
+
+                new WikiPageFiller(browser).Fill(url, newText);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ CodeplexWikiBot/WikiPageFiller.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeplexWikiBot
+{
+    using WatiN.Core;
+
+    class WikiPageFiller
+    {
+        private readonly Browser _browser;
+
+        public WikiPageFiller(Browser browser)
+        {
+            _browser = browser;
+        }
+
+        public void Fill(string url, string newText)
+        {
+            _browser.GoTo(url);
+            _browser.TextField("inputText").TypeText(newText);
+            _browser.Button("saveWikiButton").Click();
+        }
+    }
+}
