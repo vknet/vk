c94b3e0d0d6f67822f08b06479029322d84c4321
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-04-11 10:56:49 +0400

    1. Added first simple test. 2. Added AForge libs.

---
+++ VkCaptchaAnalyzer.Test/VkCaptchaAnalyzerTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using VkNet.Utils.Tests;
+
+namespace VkCaptchaAnalyzer.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class VkCaptchaAnalyzerTest
+    {
+        private Bitmap _hhc4;
+        public Bitmap Hhc4
+        {
+            get
+            {
+                if (_hhc4 != null)
+                    _hhc4 = VkCaptchaAnalyzer.LoadImage("CaptchaImgs/hhc4.jpg");
+
+                return _hhc4;
+            }
+        }
+
+        [Test]
+        public void Load_SuccessfullLoading()
+        {
+            var analyzer = new VkCaptchaAnalyzer();
+            analyzer.Load("CaptchaImgs/1.jpg");
+
+            analyzer.Captcha.ShouldNotBeNull();
+        }
+    }
+}
---
+++ VkCaptchaAnalyzer/VkCaptchaAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+
+namespace VkCaptchaAnalyzer
+{
+    using System.Drawing;
+    using System;
+
+    public class VkCaptchaAnalyzer
+    {
+        public Bitmap Captcha { get; set; }
+
+        public static Bitmap LoadImage([PathReference] string path)
+        {
+            return AForge.Imaging.Image.FromFile(path);
+        }
+
+        public void Load([PathReference] string path)
+        {
+            Captcha = LoadImage(path);
+        }
+        public void Analyze(Bitmap img)
+        {
+                     
+        }
+
+        public static Bitmap Inverse(Bitmap img)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
