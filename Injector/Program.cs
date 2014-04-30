cb5f17f3368b95b203d0b71ed50b2581c4e4eb4a
Inok <club0174@gmail.com>
2014-05-01 01:34:52 +0400

    Injector now generates pdb. Errors handling was improved.

--- Injector/Program.cs
+++ Injector/Program.cs
@@ -1,38 +1,57 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Reflection;
+using System.Security.AccessControl;
+using System.Threading;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using Mono.Cecil.Pdb;
+using Mono.CompilerServices.SymbolWriter;
 
 namespace Injector
 {
 	class Program
 	{
+		const string dllFile = "VkNet.dll";
+		const string pdbFile = "VkNet.pdb";
+
+		const string tempDllFile = "VkNet_temp.dll";
+		const string tempPdbFile = "VkNet_temp.pdb";
+		
 		static void Main(string[] args)
 		{
-			Process();
+			try
+			{
+				Process();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Injector throws an exception:\n" + e.Message);
+				throw;
+			}
 		}
 
 		static void Process()
 		{
-			const string fileName = "VkNet.dll"; 
-			const string tempFileName = fileName + "_temp";
-			
-			File.Copy(fileName, tempFileName, true);
-			File.Delete(fileName);
+		
+			File.Copy(dllFile, tempDllFile, true);
+			File.Delete(dllFile);
+			File.Copy(pdbFile, tempPdbFile, true);
+			File.Delete(pdbFile);
 
-			var assembly = AssemblyDefinition.ReadAssembly(tempFileName);
-			var assemblyRefl = Assembly.LoadFile(Environment.CurrentDirectory + @"\" + tempFileName);
+			var assembly = AssemblyDefinition.ReadAssembly(tempDllFile, new ReaderParameters {ReadSymbols = true});
+			var assemblyRefl = Assembly.LoadFile(Environment.CurrentDirectory + @"\" + tempDllFile);
 			
 			Type attrType = assemblyRefl.GetTypes().First(t => t.IsSubclassOf(typeof(Attribute)) && t.Name == "ApiVersionAttribute");
 
 			foreach (var type in assemblyRefl.GetTypes().Where(t => t.Name.Contains("Category") && t.Namespace.Contains("Categories")))
 			{
 				Console.WriteLine(type.Name);
-				foreach (var info in type.GetMethods().Where(m => m.GetCustomAttribute(attrType) != null))
+				foreach (var info in type.GetMethods().Where(m => m.GetCustomAttributes(attrType, false).FirstOrDefault() != null))
 				{
-					dynamic attribute = info.GetCustomAttribute(attrType);
+					dynamic attribute = info.GetCustomAttributes(attrType, false).First();
 					string version = attribute.Version;
 					Console.WriteLine("\t" + info.Name + " : " + version);
 					var apiMethod = assembly.MainModule.Types.First(t => t.FullName == type.FullName).Methods.First(m => m.FullName == assembly.MainModule.Import(info).FullName);
@@ -53,8 +72,9 @@ namespace Injector
 					}
 				}
 			}
+
 			
-			assembly.Write(fileName);
+			assembly.Write(dllFile, new WriterParameters { WriteSymbols = true });
 		}
 	}
 }
