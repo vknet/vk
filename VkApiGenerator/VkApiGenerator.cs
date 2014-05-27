6ac3b6d2d1767a292fd861e9ee2f4907afbfa114
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-05-27 09:23:57 +0400

    Commit for merge.

--- VkApiGenerator/VkApiGenerator.cs
+++ VkApiGenerator/VkApiGenerator.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using RazorEngine;
 using VkApiGenerator.Model;
 using VkApiGenerator.Utils;
@@ -20,7 +24,7 @@ namespace VkApiGenerator
             return string.Format(Template.ThrowIfNumberIsNegative, paramName);
         }
 
-        public string GenerateUnitTest(string category)
+        public string GenerateUnitTest(string categoryName)
         {
             // 1. read assembly
             // 2. find methods in particular category
@@ -28,6 +32,24 @@ namespace VkApiGenerator
             // 4. invoke a method
             // 5. construct unit-test
             // 6. return it
+
+            Assembly dll = Assembly.LoadFrom("VkNet.dll");
+
+            // todo refactor it later
+            List<Type> classes = dll.GetTypes().Where(t => t.Name.Contains("Category") && t.Name.Contains(categoryName)).ToList();
+
+            Type category = classes.FirstOrDefault();
+
+            MethodInfo[] methods = category.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public);
+
+            foreach (var methodInfo in methods)
+            {
+                if (methodInfo.GetParameters().Length == 0)
+                {
+                    Debug.WriteLine(" -- " + methodInfo.Name);
+
+                }
+            }
 
             throw new NotImplementedException();
         }
