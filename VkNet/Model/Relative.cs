19de13a54c673b96bdc6bdbcbe619c3cb3cf13ba
Inok <club0174@gmail.com>
2014-08-29 11:31:43 +0400

    fixed issue https://github.com/vknet/vk/issues/27

--- VkNet/Model/Relative.cs
+++ VkNet/Model/Relative.cs
@@ -1,41 +1,47 @@
 ﻿namespace VkNet.Model
 {
-    using VkNet.Utils;
+	using VkNet.Utils;
 
-    /// <summary>
-    /// Информация о родственнике.
-    /// См. описание <see href="http://vk.com/dev/fields"/>. Раздел relatives.
-    /// </summary>
-    public class Relative
-    {
-        /// <summary>
-        /// Идентификатор родственника.
-        /// </summary>
-        public long Id { get; set; }
+	/// <summary>
+	/// Информация о родственнике.
+	/// См. описание <see href="http://vk.com/dev/fields"/>. Раздел relatives.
+	/// </summary>
+	public class Relative
+	{
+		/// <summary>
+		/// Идентификатор родственника.
+		/// </summary>
+		public long Id { get; set; }
 
-        /// <summary>
-        /// Тип родственника (sibling и т.п.)
-        /// </summary>
-        public string Type { get; set; }
+		/// <summary>
+		/// Тип родственника (sibling и т.п.)
+		/// </summary>
+		public string Type { get; set; }
 
-        /// <summary>
-        /// Имя родственника, если он не является пользователем ВКонтакте.
-        /// </summary>
-        public string Name { get; set; }
+		/// <summary>
+		/// Имя родственника, если он не является пользователем ВКонтакте.
+		/// </summary>
+		public string Name { get; set; }
 
-        #region Методы
+		#region Методы
 
-        internal static Relative FromJson(VkResponse response)
-        {
-            var relative = new Relative();
+		internal static Relative FromJson(VkResponse response)
+		{
+			var relative = new Relative();
 
-            relative.Id = response["id"];
-            relative.Type = response["type"];
-            relative.Name = response["name"];
+			// Согласно документации VK API, должно возвращаться поле id, однако фактически может возвращаться uid (для старых версий API).
+			// Можно будет парсить только id после перевода всех методов на более новые версии (как минимум Users.Search).
+			if (response.ContainsKey("id"))
+				relative.Id = response["id"];
+			else if (response.ContainsKey("uid"))
+				relative.Id = response["uid"];
+			
+			relative.Type = response["type"];
+			relative.Name = response["name"];
 
-            return relative;
-        }
+			return relative;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
