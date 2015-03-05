aeb126762c445fdce4775176b17d0a8f1451d214
CaCTuCaTu4ECKuu <cactucatu4eckuu@gmail.com>
2015-03-05 13:16:28 +0300

    Исправлена работа Groups.Get и перевод на версию API 5.28
    Исправлена ошибка #70
    Добавлены модели контактов и внешних ссылок группы (возможно еще где-то
    подойдут)

---
+++ VkNet/Model/Contact.cs
@@ -0,0 +1,45 @@
+﻿namespace VkNet.Model
+{
+    using Utils;
+    /// <summary>
+    /// Контакты группы
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
+        public long? UserId { get; set; }
+
+        /// <summary>
+        /// Должность.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Электронная почта.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Телефон.
+        /// </summary>
+        public string Phone { get; set; }
+
+        #region Методы
+
+        internal static Contact FromJson(VkResponse response)
+        {
+            var contact = new Contact();
+
+            contact.UserId = response["user_id"];
+            contact.Description = response["desc"];
+            contact.Email = response["email"];
+            contact.Phone = response["phone"]; ;
+
+            return contact;
+        }
+
+        #endregion
+    }
+}
---
+++ VkNet/Model/ExternalLink.cs
@@ -0,0 +1,57 @@
+﻿namespace VkNet.Model
+{
+    using Utils;
+    /// <summary>
+    /// Ссылки в группе
+    /// </summary>
+    public class ExternalLink
+    {
+        /// <summary>
+        /// Идентификатор ссылки.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Адрес ссылки.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Название страницы, на которую ведет ссылка.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Фото 50px.
+        /// </summary>
+        public string Photo50 { get; set; }
+
+        /// <summary>
+        /// Фото 100px.
+        /// </summary>
+        public string Photo100 { get; set; }
+
+        #region Методы
+
+        internal static ExternalLink FromJson(VkResponse response)
+        {
+            var contact = new ExternalLink();
+
+            contact.Id = response["id"];
+            contact.Url = response["url"];
+            contact.Name = response["name"];
+            contact.Description = response["desc"];
+            contact.Photo50 = response["photo_50"];
+            contact.Photo100 = response["photo_100"]; ;
+
+            return contact;
+        }
+
+        #endregion
+    }
+}
