5abfb91626b6431a8dc2bb89069a8dacb6254568
Inyutin Maxim <inyutin_maxim@mail.ru>
2015-10-12 14:36:27 +0300

    Добавил метод pages.get

---
+++ VkNet.Tests/Categories/PagesCategoryTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Moq;
+using NUnit.Framework;
+using VkNet.Categories;
+using VkNet.Enums;
+using VkNet.Utils;
+
+namespace VkNet.Tests.Categories
+{
+	[TestFixture]
+	[SuppressMessage("ReSharper", "PublicMembersMustHaveComments")]
+	public class PagesCategoryTest
+	{
+		private PagesCategory GetMockedPagesCategory(string url, string json)
+		{
+			var browser = Mock.Of<IBrowser>(b => b.GetJson(url.Replace('\'', '"')) == json);
+			return new PagesCategory(new VkApi { AccessToken = "token", Browser = browser });
+		}
+
+		[Test]
+		public void Get_NormalCase()
+		{
+			const string url = "https://api.vk.com/method/pages.get?owner_id=-103292418&global=0&site_preview=0&title=Свежие новости&need_source=0&need_html=0&v=5.37&access_token=token";
+			const string json =
+				@"{
+					'response': {
+					  'id': 50050492,
+					  'group_id': 103292418,
+					  'title': 'Свежие новости',
+					  'current_user_can_edit': 1,
+					  'current_user_can_edit_access': 1,
+					  'who_can_view': 1,
+					  'who_can_edit': 0,
+					  'edited': 1444643546,
+					  'created': 1444643546,
+					  'views': 1,
+					  'editor_id': 32190123,
+					  'creator_id': 32190123,
+					  'view_url': 'http://m.vk.com/page-103292418_50050492?api_view=bdf796b3489e4adbc46be1cb81863e'
+					}
+				  }";
+
+			var db = GetMockedPagesCategory(url, json);
+
+			var page = db.Get(-103292418, "Свежие новости");
+
+			Assert.That(page.Id, Is.EqualTo(50050492));
+			Assert.That(page.GroupId, Is.EqualTo(103292418));
+			Assert.That(page.Title, Is.EqualTo("Свежие новости"));
+			Assert.That(page.CurrentUserCanEdit, Is.EqualTo(true));
+			Assert.That(page.CurrentUserCanEditAccess, Is.EqualTo(true));
+
+			Assert.That(page.WhoCanEdit, Is.EqualTo(PageAccessKind.OnlyAdministrators));
+			Assert.That(page.WhoCanView, Is.EqualTo(PageAccessKind.OnlyMembers));
+			Assert.That(page.Edited, Is.EqualTo("1444643546"));
+			Assert.That(page.CreateTime, Is.EqualTo("1444643546"));
+			Assert.That(page.EditorId, Is.EqualTo(32190123));
+			Assert.That(page.CreatorId, Is.EqualTo(32190123));
+			Assert.That(page.ViewUrl, Is.EqualTo("http://m.vk.com/page-103292418_50050492?api_view=bdf796b3489e4adbc46be1cb81863e"));
+		}
+	}
+}
---
+++ VkNet/Categories/PagesCategory.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using VkNet.Model.Attachments;
+
+namespace VkNet.Categories
+{
+	using Utils;
+
+	/// <summary>
+	/// Методы для работы с wiki.
+	/// </summary>
+	public class PagesCategory
+	{
+		/// <summary>
+		/// API
+		/// </summary>
+		readonly VkApi _vk;
+
+		/// <summary>
+		/// Методы для работы с wiki.
+		/// </summary>
+		/// <param name="vk">API.</param>
+		internal PagesCategory(VkApi vk)
+		{
+			_vk = vk;
+		}
+		/// <summary>
+		/// get
+		/// </summary>
+		/// <param name="ownerId">Идентификатор владельца вики-страницы.</param>
+		/// <param name="pageId">Идентификатор вики-страницы.</param>
+		/// <param name="global"><c>true</c> — требуется получить информацию о глобальной вики-странице. </param>
+		/// <param name="sitePreview"><c>true</c>— получаемая wiki страница является предпросмотром для прикрепленной ссылки.</param>
+		/// <param name="title">Название страницы.</param>
+		/// <param name="needSource"><c>true</c> — требуется вернуть содержимое страницы в вики-формате.</param>
+		/// <param name="needHtml"><c>true</c> — требуется вернуть html-представление страницы.</param>
+		/// <returns>
+		/// Возвращает результат выполнения метода.
+		/// </returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.get" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public Page Get(long ownerId, string title, long? pageId = null, bool global = false, bool sitePreview = false, bool needSource = false, bool needHtml = false)
+		{
+			var parameters = new VkParameters
+			{
+				{ "owner_id", ownerId },
+				{ "page_id", pageId },
+				{ "global", global },
+				{ "site_preview", sitePreview },
+				{ "title", title },
+				{ "need_source", needSource },
+				{ "need_html", needHtml }
+			};
+
+			return _vk.Call("pages.get", parameters);
+		}
+
+
+		/// <summary>
+		/// save
+		/// </summary>
+		/// <returns>Возвращает результат выполнения метода.</returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.save" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public bool Save()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// saveAccess
+		/// </summary>
+		/// <returns>Возвращает результат выполнения метода.</returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.saveAccess" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public bool SaveAccess()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// getHistory
+		/// </summary>
+		/// <returns>Возвращает результат выполнения метода.</returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.getHistory" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public bool GetHistory()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// getTitles
+		/// </summary>
+		/// <returns>Возвращает результат выполнения метода.</returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.getTitles" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public bool GetTitles()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// getVersion
+		/// </summary>
+		/// <returns>Возвращает результат выполнения метода.</returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.getVersion" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public bool GetVersion()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// parseWiki
+		/// </summary>
+		/// <returns>Возвращает результат выполнения метода.</returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.parseWiki" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public bool ParseWiki()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// clearCache
+		/// </summary>
+		/// <returns>Возвращает результат выполнения метода.</returns>
+		/// <remarks>
+		/// Страница документации ВКонтакте <see href="https://vk.com/dev/pages.clearCache" />.
+		/// </remarks>
+		[ApiVersion("5.37")]
+		public bool ClearCache()
+		{
+			throw new NotImplementedException();
+		}
+
+
+	}
+}
