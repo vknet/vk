b4474c1dcf53d5f484dc8c2e0a46d9274bec8d58
CaCTuCaTu4ECKuu <cactucatu4eckuu@gmail.com>
2015-11-02 08:44:21 +0300

    Откат замены long на ulong в Model\Users.cs
    При замене здесь все крашиться

--- VkNet/Model/User.cs
+++ VkNet/Model/User.cs
@@ -21,7 +21,7 @@
         /// <summary>
         /// Идентификатор пользователя.
         /// </summary>
-        public ulong Id { get; set; }
+        public long Id { get; set; }
 
         /// <summary>
         /// Имя пользователя.
@@ -76,7 +76,7 @@
         /// помощью метода <see cref="FriendsCategory.GetLists"/>. Если пользователь не состоит ни в одном списке друзей, данное 
         /// поле принимает значение null.
         /// </summary>
-        public Collection<ulong> FriendLists { get; set; }
+        public Collection<long> FriendLists { get; set; }
 
         /// <summary>
         /// Короткий адрес страницы пользователя. Возвращается строка, содержащая короткий адрес страницы (возвращается только 
@@ -210,7 +210,7 @@
         /// <summary>
         /// Идентификатор языка, установленный в настройках.
         /// </summary>
-        public ulong? Language { get; set; }
+        public long? Language { get; set; }
 
         /// <summary>
         /// Признак использует ли пользователь мобильное приложение либо мобильную версию сайта.
@@ -220,7 +220,7 @@
         /// <summary>
         /// Если пользователь зашёл через приложение, то Id приложения иначе null.
         /// </summary>
-        public ulong? OnlineApp { get; set; }
+        public long? OnlineApp { get; set; }
 
         /// <summary>
         /// Партнер в семейных отношениях.
@@ -280,7 +280,7 @@
         /// <summary>
         /// Идентификатор пользователя, пригласившего пользователя в беседу (для GetChatUsers).
         /// </summary>
-        public ulong? InvitedBy { get; set; }
+        public long? InvitedBy { get; set; }
 
         #endregion
 
