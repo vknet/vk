2738f1d9f9e38ce3355177670acfb4f2bd5391a5
Inyutin Maxim <inyutin_maxim@mail.ru>
2015-12-07 17:41:33 +0300

    Добавил недостающие поля класса PostSource (пропустил 2 поля:))

---
+++ VkNet/Enums/SafetyEnums/Platform.cs
@@ -0,0 +1,50 @@
+﻿using VkNet.Utils;
+
+namespace VkNet.Enums.SafetyEnums
+{
+	public class Platform : SafetyEnum<Platform>
+	{
+		/// <summary>
+		/// Android.
+		/// </summary>
+		public static readonly Platform Android = RegisterPossibleValue("android");
+
+
+		/// <summary>
+		/// iPhone.
+		/// </summary>
+		public static readonly Platform IPhone = RegisterPossibleValue("iphone");
+
+
+		/// <summary>
+		/// wphone.
+		/// </summary>
+		public static readonly Platform WindowsPhone = RegisterPossibleValue("wphone");
+
+		/// <summary>
+		/// Разобрать из json.
+		/// </summary>
+		/// <param name="response">Ответ сервера.</param>
+		/// <returns></returns>
+		internal static Platform FromJson(VkResponse response)
+		{
+			switch (response.ToString())
+			{
+				case "android":
+					{
+						return Android;
+					}
+				case "iphone":
+					{
+						return IPhone;
+					}
+				case "wphone":
+					{
+						return WindowsPhone;
+					}
+			}
+
+			return null;
+		}
+	}
+}
--- VkNet/Model/PostSource.cs
+++ VkNet/Model/PostSource.cs
@@ -2,28 +2,37 @@
 
 namespace VkNet.Model
 {
-    using System;
-    using Utils;
+	using System;
+	using Utils;
 
-    /// <summary>
-    /// Информация информацию о том, каким образом (через интерфейс сайта, виджет и т.п.) была создана запись на стене.
-    /// Используя данные из этого поля, разработчик может вывести уточняющую информацию о том, как была создана запись на стене
-    /// в своем приложении.
-    /// См. описание <see href="http://vk.com/dev/post_source"/>.
-    /// </summary>
-    [Serializable]
-    public class PostSource
-    {
-        /// <summary>
-        /// На данный момент поддерживаются следующие типы источников записи на стене.
-        /// </summary>
-        public PostSourceType Type { get; set; }
+	/// <summary>
+	/// Информация информацию о том, каким образом (через интерфейс сайта, виджет и т.п.) была создана запись на стене.
+	/// Используя данные из этого поля, разработчик может вывести уточняющую информацию о том, как была создана запись на стене
+	/// в своем приложении.
+	/// См. описание <see href="http://vk.com/dev/post_source"/>.
+	/// </summary>
+	[Serializable]
+	public class PostSource
+	{
+		/// <summary>
+		/// На данный момент поддерживаются следующие типы источников записи на стене.
+		/// </summary>
+		public PostSourceType Type { get; set; }
 
-        /// <summary>
-        /// Поле data является опциональным и содержит следующие данные в зависимости от значения поля type:
-        /// </summary>
-        public PostSourceData Data { get; set; }
+		/// <summary>
+		/// Поле data является опциональным и содержит следующие данные в зависимости от значения поля type:
+		/// </summary>
+		public PostSourceData Data { get; set; }
 
+		/// <summary>
+		/// Название платформы, если оно доступно: android, iphone, wphone.
+		/// </summary>
+		public Platform Platform;
+
+		/// <summary>
+		/// Cодержит внешнюю ссылку на ресурс, с которого была опубликована запись.
+		/// </summary>
+		public Uri Url;
 		#region Методы
 		/// <summary>
 		/// Разобрать из json.
@@ -35,7 +44,9 @@ namespace VkNet.Model
 			var postSource = new PostSource
 			{
 				Type = response["type"],
-				Data = response["data"]
+				Data = response["data"],
+				Platform = response["source_platform"],
+				Url = response["source_url"]
 			};
 
 			return postSource;
