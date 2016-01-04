8727991cf6913f0e489994652cdf2e734996f068
ProESM <proesm@mail.ru>
2016-01-05 02:03:18 +0300

    Добавил атрибуты текстового отображения

--- VkNet/Enums/UserSort.cs
+++ VkNet/Enums/UserSort.cs
@@ -1,17 +1,21 @@
-﻿namespace VkNet.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VkNet.Enums
 {
 	/// <summary>
 	/// Сортировка результатов: 1 - по дате регистрации, 0 - по популярности целое число.
 	/// </summary>
 	public enum UserSort
 	{
-		/// <summary>
-		/// По популярности
-		/// </summary>
-		ByPopularity = 0,
-		/// <summary>
-		/// По дате регистрации
-		/// </summary>
-		ByRegDate
+        /// <summary>
+        /// По популярности
+        /// </summary>
+        [Display(Name = "По популярности")]
+        ByPopularity = 0,
+        /// <summary>
+        /// По дате регистрации
+        /// </summary>
+        [Display(Name = "По дате регистрации")]
+        ByRegDate
 	}
 }
