2fae410ed177b96db78aeae9dc9bde5e397d5fb3
Inyutin Maxim <inyutin_maxim@mail.ru>
2016-03-22 16:51:21 +0300

    #215 , #86 Заготовка для решении проблемы out параметров на версию 2.0 так как может привести к ломающим обратную совместимость изменениям

---
+++ VkNet/Utils/IVkModel.cs
@@ -0,0 +1,15 @@
+﻿namespace VkNet.Utils
+{
+	/// <summary>
+	/// Модель данных vk.com
+	/// </summary>
+	public interface IVkModel
+	{
+		/// <summary>
+		/// Преобразовать из JSON
+		/// </summary>
+		/// <param name="response">Ответ от сервера.</param>
+		/// <returns></returns>
+		IVkModel FromJson(VkResponse response);
+	}
+}
---
+++ VkNet/Utils/VkCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace VkNet.Utils
+{
+	/// <summary>
+	/// Коллекция данных возвращенных от vk.com
+	/// </summary>
+	/// <typeparam name="T">Тип данных.</typeparam>
+	public class VkCollection<T> : ReadOnlyCollectionBase where T: class, IVkModel, new()
+	{
+		/// <summary>
+		/// Общее количество элементов.
+		/// </summary>
+		public ulong TotalCount { get; set; }
+
+		/// <summary>
+		/// Конструктор.
+		/// </summary>
+		/// <param name="response">Ответ от сервера.</param>
+		public VkCollection(VkResponse response)
+		{
+			if (response.ContainsKey("count"))
+			{
+				TotalCount = response["count"];
+			}
+
+			if (response.ContainsKey("items"))
+			{
+				InnerList.AddRange(response["items"].ToReadOnlyCollectionOf(o => (T)new T().FromJson(o)));
+			}
+		}
+	}
+}
