8952bf0bc37e76816b1d7e3325b1bfc5cca273e0
webtarget7777 <webtarget7777@mail.ru>
2017-02-20 16:13:44 +0300

    Модифицирован метод EditManager
    Восстановлена работа метода. Enum AdminLevel заменен на SafetyEnum
    ManagerRole

---
+++ VkNet.UWP/Enums/SafetyEnums/ManagerRole.cs
@@ -0,0 +1,23 @@
+﻿namespace VkNet.Enums.SafetyEnums
+{
+    /// <summary>
+    /// Уровень полномочий пользователя в сообществе (Используется для задания полномочий пользователя в методе EditManager).
+    /// </summary>
+    public sealed class ManagerRole : SafetyEnum<ManagerRole>
+    {
+        /// <summary>
+        /// Пользователь является модератором собщества.
+        /// </summary>
+        public static readonly ManagerRole Moderator = RegisterPossibleValue("moderator");
+
+        /// <summary>
+        /// Пользователь является редактором сообщества.
+        /// </summary>
+        public static readonly ManagerRole Editor = RegisterPossibleValue("editor");
+
+        /// <summary>
+        /// Пользователь является администратором сообщества.
+        /// </summary>
+        public static readonly ManagerRole Administrator = RegisterPossibleValue("administrator");
+    }
+}
