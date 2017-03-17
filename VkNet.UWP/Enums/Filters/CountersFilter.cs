fd896d6bb39daeb4b56c1d8e7a0f5221b57f2b9a
Инютин Максим <inyutin_maxim@mail.ru>
2017-03-17 14:17:50 +0300

    5.61 Метод account.getCounters принимает в параметре filter новое значение friends_suggestions.

--- VkNet.UWP/Enums/Filters/CountersFilter.cs
+++ VkNet.UWP/Enums/Filters/CountersFilter.cs
@@ -5,48 +5,64 @@ namespace VkNet.Enums.Filters
 	/// </summary>
 	public sealed class CountersFilter : MultivaluedFilter<CountersFilter>
 	{
-		/// <summary>
-		/// Количество заявок в друзья
-		/// </summary>
-		public static readonly CountersFilter Friends = RegisterPossibleValue(1 << 0, "friends");
-		/// <summary>
-		/// Количество непрочитанных сообщений
-		/// </summary>
-		public static readonly CountersFilter Messages = RegisterPossibleValue(1 << 1, "messages");
-		/// <summary>
-		/// Количество фото
-		/// </summary>
-		public static readonly CountersFilter Photos = RegisterPossibleValue(1 << 2, "photos");
-		/// <summary>
-		/// Количество видео
-		/// </summary>
-		public static readonly CountersFilter Videos = RegisterPossibleValue(1 << 3, "videos");
-		/// <summary>
-		/// Количество заметок
-		/// </summary>
-		public static readonly CountersFilter Notes = RegisterPossibleValue(1 << 4, "notes");
-		/// <summary>
-		/// Количество подарков
-		/// </summary>
-		public static readonly CountersFilter Gifts = RegisterPossibleValue(1 << 5, "gifts");
-		/// <summary>
-		/// Количество событий
-		/// </summary>
-		public static readonly CountersFilter Events = RegisterPossibleValue(1 << 6, "events");
-		/// <summary>
-		/// Количество групп
-		/// </summary>
-		public static readonly CountersFilter Groups = RegisterPossibleValue(1 << 7, "groups");
-		/// <summary>
-		/// Количество уведомлений
-		/// </summary>
-		public static readonly CountersFilter Notifications = RegisterPossibleValue(1 << 8, "notifications");
+        /// <summary>
+        /// Количество заявок в друзья
+        /// </summary>
+        public static readonly CountersFilter Friends = RegisterPossibleValue("friends");
 
-		/// <summary>
-		/// Все фильтры
-		/// </summary>
-		public static readonly CountersFilter All = Friends | Messages | Photos | Videos
-			| Notes | Gifts | Events | Groups | Notifications;
+        /// <summary>
+        /// Предлагаемые друзья
+        /// </summary>
+        public static readonly CountersFilter FriendsSuggestions = RegisterPossibleValue("friends_suggestions");
 
-	}
+        /// <summary>
+        /// Количество непрочитанных сообщений
+        /// </summary>
+        public static readonly CountersFilter Messages = RegisterPossibleValue("messages");
+
+        /// <summary>
+        /// Количество фото
+        /// </summary>
+        public static readonly CountersFilter Photos = RegisterPossibleValue("photos");
+
+        /// <summary>
+        /// Количество видео
+        /// </summary>
+        public static readonly CountersFilter Videos = RegisterPossibleValue("videos");
+
+        /// <summary>
+        /// Количество подарков
+        /// </summary>
+        public static readonly CountersFilter Gifts = RegisterPossibleValue("gifts");
+
+        /// <summary>
+        /// Количество событий
+        /// </summary>
+        public static readonly CountersFilter Events = RegisterPossibleValue("events");
+
+        /// <summary>
+        /// Количество групп
+        /// </summary>
+        public static readonly CountersFilter Groups = RegisterPossibleValue("groups");
+
+        /// <summary>
+        /// Количество уведомлений
+        /// </summary>
+        public static readonly CountersFilter Notifications = RegisterPossibleValue("notifications");
+
+        /// <summary>
+        /// Количество уведомлений
+        /// </summary>
+        public static readonly CountersFilter Sdk = RegisterPossibleValue("sdk");
+
+        /// <summary>
+        /// Количество уведомлений
+        /// </summary>
+        public static readonly CountersFilter AppRequests = RegisterPossibleValue("app_requests");
+
+        /// <summary>
+        /// Все фильтры
+        /// </summary>
+        public static readonly CountersFilter All = Friends | FriendsSuggestions | Messages | Photos | Videos | Gifts | Events | Groups | Notifications | Sdk | AppRequests;
+    }
 }
