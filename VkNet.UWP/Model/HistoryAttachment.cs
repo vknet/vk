105a0b9380c315661c842363bf0c6af7868dae1d
Инютин Максим <inyutin_maxim@mail.ru>
2017-03-17 14:57:37 +0300

    5.62 Метод messages.getHistoryAttachments возвращает поле message_id для вложений.

---
+++ VkNet.UWP/Model/HistoryAttachment.cs
@@ -0,0 +1,35 @@
+﻿using VkNet.Model.Attachments;
+using VkNet.Utils;
+
+namespace VkNet.Model
+{
+    /// <summary>
+    /// Вложения материалов диалога или беседы
+    /// </summary>
+    public class HistoryAttachment
+    {
+        /// <summary>
+        /// Идентификатор сообщения, в котором было отправлено вложение.
+        /// </summary>
+        public int MessageId { get; set; }
+
+        /// <summary>
+        /// Информация о вложении.
+        /// </summary>
+        public Attachment Attachment { get; set; }
+
+        /// <summary>
+        /// Разобрать из json.
+        /// </summary>
+        /// <param name="response">Ответ сервера.</param>
+        /// <returns></returns>
+        public static HistoryAttachment FromJson(VkResponse response)
+        {
+            return new HistoryAttachment
+            {
+                MessageId = response["message_id"],
+                Attachment = response["attachment"]
+            };
+        }
+    }
+}
