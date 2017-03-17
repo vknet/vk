b96905b05507037c5ef2748bc28d1e5b3eb5d3cf
Инютин Максим <inyutin_maxim@mail.ru>
2017-03-17 14:06:19 +0300

    5.61 В объекте сообщества возвращается новое поле cover.

---
+++ VkNet.UWP/Model/GroupCover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using VkNet.Utils;
+
+namespace VkNet.Model
+{
+    /// <summary>
+    /// Обложка сообщества.
+    /// </summary>
+    public class GroupCover
+    {
+        /// <summary>
+        /// Информация о том, включена ли обложка (1 — да, 0 — нет);
+        /// </summary>
+        public bool? Enabled { get; set; }
+
+        /// <summary>
+        /// Копии изображений обложки.
+        /// </summary>
+        public IEnumerable<GroupCoverImage> Images { get; set; }
+
+        /// <summary>
+        /// Разобрать из json.
+        /// </summary>
+        /// <param name="response">Ответ сервера.</param>
+        /// <returns></returns>
+        public static GroupCover FromJson(VkResponse response)
+        {
+            return new GroupCover
+            {
+                Enabled = response["enabled"],
+                Images = response["images"].ToReadOnlyCollectionOf<GroupCoverImage>(o => o)
+            };
+        }
+    }
+}
---
+++ VkNet.UWP/Model/GroupCoverImage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using VkNet.Utils;
+
+namespace VkNet.Model
+{
+    /// <summary>
+    /// Копия изображения обложки.
+    /// </summary>
+    public class GroupCoverImage
+    {
+        /// <summary>
+        /// URL копии;
+        /// </summary>
+        public Uri Url { get; set; }
+
+        /// <summary>
+        /// Ширина копии;
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Высота копии.
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Разобрать из json.
+        /// </summary>
+        /// <param name="response">Ответ сервера.</param>
+        /// <returns></returns>
+        public static GroupCoverImage FromJson(VkResponse response)
+        {
+            return new GroupCoverImage
+            {
+                Url = response["url"],
+                Width = response["width"],
+                Height = response["height"]
+            };
+        }
+    }
+}
