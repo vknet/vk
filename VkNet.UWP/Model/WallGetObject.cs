b5f9c687dc0e4746e8a63c75a9c922cdae0b01a2
Инютин Максим <inyutin_maxim@mail.ru>
2017-03-17 17:02:48 +0300

    #345 WallGetObject handling 1 returned item (object reference exception)

--- VkNet.UWP/Model/WallGetObject.cs
+++ VkNet.UWP/Model/WallGetObject.cs
@@ -40,13 +40,13 @@ namespace VkNet.Model
 			WallGetObject wallGetObject;
 			if (response.ContainsKey("items"))
 			{
-				wallGetObject = new WallGetObject
-				{
-					TotalCount = response["count"],
-					WallPosts = response["items"].ToReadOnlyCollectionOf<Post>(r => r),
-					Profiles = response["profiles"].ToReadOnlyCollectionOf<User>(r => r),
-					Groups = response["groups"].ToReadOnlyCollectionOf<Group>(r => r)
-				};
+			    wallGetObject = new WallGetObject
+			    {
+			        WallPosts = response["items"].ToReadOnlyCollectionOf<Post>(r => r),
+			        Profiles = response["profiles"].ToReadOnlyCollectionOf<User>(r => r),
+			        Groups = response["groups"].ToReadOnlyCollectionOf<Group>(r => r),
+			        TotalCount = response["count"] ?? 1UL
+			    };
 			}
 			else
 			{
