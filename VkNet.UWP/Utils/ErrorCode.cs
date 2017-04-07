a4c4a254eb56bf13d734c6d384ced344b00f1c47
Oleg Gaufman <n640@bk.ru>
2017-04-07 21:37:10 +0400

    Revert "1"
    This reverts commit 9bffce9079d782ea4fbd906babd686753781026a.

--- VkNet.UWP/Utils/ErrorCode.cs
+++ VkNet.UWP/Utils/ErrorCode.cs
@@ -95,16 +95,11 @@
 		/// </summary>
 		public const int NeedValidationOfUser = 17;
 
-        /// <summary>
-        /// Пользователь не найден или заблокирован
-        /// </summary>
-        public const int UserDeletedOrBanned = 18;
-
-        /// <summary>
-        /// Данное действие запрещено для не Standalone приложений.
-        /// Если ошибка возникает несмотря на то, что Ваше приложение имеет тип Standalone, убедитесь, что при авторизации Вы используете redirect_uri=https://oauth.vk.com/blank.html. Подробнее см. http://vk.com/dev/auth_mobile.
-        /// </summary>
-        public const int NonStandaloneApplications = 20;
+		/// <summary>
+		/// Данное действие запрещено для не Standalone приложений.
+		/// Если ошибка возникает несмотря на то, что Ваше приложение имеет тип Standalone, убедитесь, что при авторизации Вы используете redirect_uri=https://oauth.vk.com/blank.html. Подробнее см. http://vk.com/dev/auth_mobile.
+		/// </summary>
+		public const int NonStandaloneApplications = 20;
 
 		/// <summary>
 		/// Данное действие разрешено только для Standalone и Open API приложений.
@@ -171,20 +166,10 @@
 		/// </summary>
 		public const int UserAccessDenied = 170;
 
-        /// <summary>
-        /// Создано максимальное количество списков.
-        /// </summary>
-        public const int ListAmountMaximum = 171;
-
-        /// <summary>
-        /// Недопустимый идентификатор списка.
-        /// </summary>
-        public const int ListAmountMaximum = 173;
-
-        /// <summary>
-        /// Невозможно добавить в друзья самого себя.
-        /// </summary>
-        public const int CannotAddYourself = 174;
+		/// <summary>
+		/// Невозможно добавить в друзья самого себя.
+		/// </summary>
+		public const int CannotAddYourself = 174;
 
         /// <summary>
         /// Невозможно добавить в друзья пользователя, который занес Вас в свой черный список. 
