d97efdd682d0779460821ca26e174726826de474
Inyutin Maxim <inyutin_maxim@mail.ru>
2017-11-27 00:27:58 +0300

    VkAuthorization.cs и Browser.cs рефакторинг

---
+++ VkNet.Tests/BrowserTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using VkNet.Enums.Filters;
+using VkNet.Utils;
+
+namespace VkNet.Tests
+{
+    [TestFixture]
+    public class BrowserTests: BaseTest
+    {
+        /// <summary>
+        /// todo: поправить тест
+        /// </summary>
+        [Test, Ignore("TODO: временно")]
+        public void Authorize()
+        {
+            var browser = new Browser();
+
+            var result = browser.Authorize(123, "qwe@qwe.ru", "pass", Settings.All);
+            
+            Assert.NotNull(result);
+        }
+    }
+}
