bbec2bc1011323ff41f07306186dd049d8c197d3
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-01-08 20:27:35 +0300

    привести в соответствие с документацией vk основные объекты #521 VideoCatalog

---
+++ VkNet/Enums/SafetyEnums/VideoView.cs
@@ -0,0 +1,26 @@
+﻿namespace VkNet.Enums.SafetyEnums
+{
+    /// <summary>
+    /// предпочтительный способ отображения контента
+    /// </summary>
+    public class VideoView: SafetyEnum<VideoView>
+    {
+        /// <summary>
+        /// горизонтально с дополнительной информацией
+        /// </summary>
+        public static readonly VideoView Horizontal = RegisterPossibleValue("horizontal");
+        /// <summary>
+        /// оризонтально без дополнительной информации
+        /// </summary>
+        public static readonly VideoView HorizontalCompact = RegisterPossibleValue("horizontal_compact");
+        /// <summary>
+        /// вертикально с дополнительной информацией
+        /// </summary>
+        public static readonly VideoView Vertical = RegisterPossibleValue("vertical");
+
+        /// <summary>
+        /// вертикально с дополнительной информацией
+        /// </summary>
+        public static readonly VideoView VerticalCompact = RegisterPossibleValue("vertical_compact");        
+    }
+}
