bbd80777746923bbc0c57d13934f130d66a65bf6
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2018-02-14 16:12:09 +0300

    Change access right Settings
    Settings become a self-sufficient class and now implements Mask.
    Add support for sets of acces right.
    Now 'all' handle correctly.
    Lot of incredible bugs to explore related to this class

--- VkNet/Enums/Filters/Settings.cs
+++ VkNet/Enums/Filters/Settings.cs
@@ -1,111 +1,276 @@
-﻿namespace VkNet.Enums.Filters
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using VkNet.Utils;
+
+namespace VkNet.Enums.Filters
 {
-	/// <summary>
+    /// <summary>
     /// Права доступа приложений.
     /// См. описание <see href="http://vk.com/pages?oid=-1&amp;p=Права_доступа_приложений"/>.
     /// </summary>
-    public sealed class Settings : MultivaluedFilter<Settings>
+    public sealed class Settings
     {
+        private static readonly Dictionary<string, UInt64> _maskMap = new Dictionary<string, ulong>()
+            {
+                { "notify", 1},
+                { "friends", 2},
+                { "photos", 4},
+                { "audio", 8},
+                { "video", 16},
+                { "pages", 128},
+                { "addlinktoleftmenu", 256},
+                { "status", 1024},
+                { "notes", 2048},
+                { "messages", 4096},
+                { "wall", 8192},
+                { "ads", 32768},
+                { "offline", 65536},
+                { "docs", 131072},
+                { "groups", 262144},
+                { "notifications", 524288},
+                { "stats", 1048576},
+                { "email", 4194304},
+                { "market", 134217728}
+            };
+        private static readonly Dictionary<string, string> _alias = new Dictionary<string, string>()
+        {
+            { "all", string.Join(",", _maskMap.Keys) }
+        };
+
+        private static Settings GetByName(string name)
+        {
+            string n = name.ToLower();
+
+            if (_maskMap.ContainsKey(n))
+            {
+                return new Settings()
+                {
+                    Mask = _maskMap[n],
+                    _settings = new List<string>() { n }
+                };
+            }
+
+            if (_alias.ContainsKey(n))
+            {
+                Settings res = new Settings();
+                foreach (var v in _alias[n].Split(','))
+                    res = res | GetByName(v);
+                return res;
+            }
+
+            throw new ArgumentException($"Undefined access setting '{n}'");
+        }
+
+        /// <summary>
+        /// Числовая маска выбранных прав доступа
+        /// </summary>
+        public UInt64 Mask { get; private set; }
+
+        private List<string> _settings;
+        /// <summary>
+        /// Список выбранных прав
+        /// </summary>
+        public IEnumerable<string> Selected
+        {
+            get => _settings ?? (_settings = new List<string>());
+        }
+
+        /// <summary>
+        /// Получить из json
+        /// </summary>
+        /// <param name="response">Ответ Vk</param>
+        /// <returns></returns>
+        public static Settings FromJson(VkResponse response)
+        {
+            var value = response.ToString();
+            return FromJsonString(value);
+        }
+        /// <summary>
+        /// Получить из json
+        /// </summary>
+        /// <param name="val">Json строка</param>
+        /// <returns></returns>
+        public static Settings FromJsonString(string val)
+        {
+            var vals = val.Split(',').Select(x => x.Trim());
+
+            Settings res = new Settings();
+            foreach (var v in vals)
+            {
+                if (_maskMap.ContainsKey(v.ToLower()))
+                {
+                    res = res | GetByName(v);
+                }
+                if (_alias.ContainsKey(v.ToLower()))
+                {
+                    res = res | GetByName(v);
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Преобразовать в строку
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Join(",", Selected.ToArray());
+        }
+        /// <summary>
+        /// Преобразовать в числовую маску выбранные права доступа
+        /// </summary>
+        /// <returns></returns>
+        public UInt64 ToUInt64()
+        {
+            return Mask;
+        }
+
+        /// <summary>
+        /// Сравнение с другим набором прав
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            Settings other = obj as Settings;
+
+            if (other == null)
+                return false;
+
+            return Mask == other.Mask;
+        }
+        /// <summary>
+        /// Get Hash
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Selected?.GetHashCode() ?? 0;
+        }
+
         /// <summary>
         /// Пользователь разрешил отправлять ему уведомления.
         /// </summary>
-        public static readonly Settings Notify = RegisterPossibleValue(1 << 0, "notify");
+        public static Settings Notify => GetByName("notify");
 
         /// <summary>
         /// Доступ к друзьям.
         /// </summary>
-        public static readonly Settings Friends = RegisterPossibleValue(1 << 1, "friends");
+        public static Settings Friends => GetByName("friends");
 
         /// <summary>
         /// Доступ к фотографиям.
         /// </summary>
-        public static readonly Settings Photos = RegisterPossibleValue(1 << 2, "photos");
+        public static Settings Photos => GetByName("photos");
 
         /// <summary>
         /// Доступ к аудио записям.
         /// </summary>
-        public static readonly Settings Audio = RegisterPossibleValue(1 << 3, "audio");
+        public static Settings Audio => GetByName("audio");
 
         /// <summary>
         /// Доступ к видеозаписям.
         /// </summary>
-        public static readonly Settings Video = RegisterPossibleValue(1 << 4, "video");
+        public static Settings Video => GetByName("video");
 
         /// <summary>
         /// Доступ к wiki-страницам.
         /// </summary>
-        public static readonly Settings Pages = RegisterPossibleValue(1 << 7, "pages");
+        public static Settings Pages => GetByName("pages");
 
 		/// <summary>
 		/// Добавление ссылки на приложение в меню слева.
 		/// </summary>
-		public static readonly Settings AddLinkToLeftMenu = RegisterPossibleValue(1 << 8, "addLinkToLeftMenu");
+		public static Settings AddLinkToLeftMenu => GetByName("addLinkToLeftMenu");
 
 		/// <summary>
 		/// Доступ к статусу пользователя.
 		/// </summary>
-		public static readonly Settings Status = RegisterPossibleValue(1 << 10, "status");
+		public static Settings Status => GetByName("status");
 
         /// <summary>
         /// Доступ заметкам пользователя.
         /// </summary>
-        public static readonly Settings Notes = RegisterPossibleValue(1 << 11, "notes");
+        public static Settings Notes => GetByName("notes");
 
         /// <summary>
         /// Доступ к расширенным методам работы с сообщениями.
         /// </summary>
-        public static readonly Settings Messages = RegisterPossibleValue(1 << 12, "messages");
+        public static Settings Messages => GetByName("messages");
 
         /// <summary>
         /// Доступ к обычным и расширенным методам работы со стеной.
         /// </summary>
-        public static readonly Settings Wall = RegisterPossibleValue(1 << 13, "wall");
+        public static Settings Wall => GetByName("wall");
 
         /// <summary>
         /// Доступ к расширенным методам работы с рекламным API.
         /// </summary>
-        public static readonly Settings Ads = RegisterPossibleValue(1 << 15, "ads");
+        public static Settings Ads => GetByName("ads");
 
         /// <summary>
         /// Доступ к API в любое время со стороннего сервера.
         /// </summary>
-        public static readonly Settings Offline = RegisterPossibleValue(1 << 16, "offline");
+        public static Settings Offline => GetByName("offline");
 
         /// <summary>
         /// Доступ к документам.
         /// </summary>
-        public static readonly Settings Documents = RegisterPossibleValue(1 << 17, "docs");
+        public static Settings Documents => GetByName("docs");
 
         /// <summary>
         /// Доступ к группам пользователя.
         /// </summary>
-        public static readonly Settings Groups = RegisterPossibleValue(1 << 18, "groups");
+        public static Settings Groups => GetByName("groups");
 
         /// <summary>
         /// Доступ к оповещениям об ответах пользователю.
         /// </summary>
-        public static readonly Settings Notifications = RegisterPossibleValue(1 << 19, "notifications");
+        public static Settings Notifications => GetByName("notifications");
 
         /// <summary>
         /// Доступ к статистике групп и приложений пользователя, администратором которых он является.
         /// </summary>
-        public static readonly Settings Statistic = RegisterPossibleValue(1 << 20, "stats");
+        public static Settings Statistic => GetByName("stats");
 
         /// <summary>
         /// Доступ к email пользователя. Доступно только для сайтов.
         /// </summary>
-        public static readonly Settings Email = RegisterPossibleValue(1 << 22, "email");
+        public static Settings Email => GetByName("email");
 
 		/// <summary>
 		/// Доступ к товарам. Доступно только для сайтов.
 		/// </summary>
-		public static readonly Settings Market = RegisterPossibleValue(1 << 27, "market");
+		public static Settings Market => GetByName("market");
 
-		/// <summary>
-		/// Доступ ко всем возможным операциям (без Off line и NoHttps).
-		/// </summary>
-		public static readonly Settings All = Notify | Friends | Photos | Audio | Video | Pages | Status | Notes | Messages
-			| Wall | Ads | Documents | Groups | Notifications | Statistic | Email | Market;
+        /// <summary>
+        /// Доступ ко всем возможным операциям (без Off line и NoHttps).
+        /// </summary>
+        public static Settings All => GetByName("all");
+
+        /// <summary>
+        /// Объединяет наборы настройки
+        /// </summary>
+        /// <param name="a">Первый набор</param>
+        /// <param name="b">Второй набор</param>
+        /// <returns></returns>
+        public static Settings operator |(Settings a, Settings b)
+        {
+            foreach (var s in b.Selected)
+            {
+                if (!a.Selected.Contains(s))
+                {
+                    a.Mask += _maskMap[s];
+                    a._settings.Add(s);
+                }
+            }
+            return a;
+        }
+
 
 	}
 }
