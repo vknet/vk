b15811de40ea407de734735c0c1984d1de76386e
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-04-26 19:37:09 +0300

    #588 ITokenManager.cs

---
+++ VkNet.Core.Abstractions/ITokenManager.cs
@@ -0,0 +1,27 @@
+﻿namespace VkNet.Core.Abstractions
+{
+    /// <summary>
+    /// Менеджер управления токеном
+    /// </summary>
+    public interface ITokenManager
+    {
+        /// <summary>
+        /// Токен приложения
+        /// </summary>
+        string Token { get; }
+        /// <summary>
+        /// <c>true</c> - если истек
+        /// </summary>
+        bool IsExpired { get; }
+        /// <summary>
+        /// Обновить токен
+        /// </summary>
+        /// <returns><c>true</c> - если обновление прошло успешно</returns>
+        bool RefreshToken();
+        /// <summary>
+        /// Установить значение токена
+        /// </summary>
+        /// <param name="token">Токен</param>
+        void SetToken(string token);
+    }
+}
