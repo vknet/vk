97cefa89ed62b0c6373343511f801f0536e41af7
Inyutin Maxim <inyutin_maxim@mail.ru>
2018-06-24 00:11:40 +0300

    5.75 Новый формат ответа в методе stats.get. #601

--- VkNet/Categories/StatsCategory.cs
+++ VkNet/Categories/StatsCategory.cs
@@ -25,81 +25,33 @@ namespace VkNet.Categories
 			_vk = vk;
 		}
 
-		/// <summary>
-		/// Возвращает статистику сообщества или приложения.
-		/// </summary>
-		/// <param name="groupId"> Идентификатор сообщества. </param>
-		/// <param name="dateFrom"> The date from. </param>
-		/// <param name="dateTo"> The date to. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/stats.get
-		/// </remarks>
+		/// <inheritdoc />
 		public ReadOnlyCollection<StatsPeriod> GetByGroup(long groupId, DateTime dateFrom, DateTime? dateTo = null)
 		{
 			return Get(dateFrom: dateFrom, dateTo: dateTo, groupId: groupId);
 		}
 
-		/// <summary>
-		/// Возвращает статистику сообщества или приложения.
-		/// </summary>
-		/// <param name="appId"> Идентификатор приложения. </param>
-		/// <param name="dateFrom"> The date from. </param>
-		/// <param name="dateTo"> The date to. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/stats.get
-		/// </remarks>
+		/// <inheritdoc />
 		public ReadOnlyCollection<StatsPeriod> GetByApp(long appId, DateTime dateFrom, DateTime? dateTo = null)
 		{
 			return Get(dateFrom: dateFrom, dateTo: dateTo, groupId: null, appId: appId);
 		}
 
-		/// <summary>
-		/// Добавляет данные о текущем сеансе в статистику посещаемости приложения..
-		/// </summary>
-		/// <returns>
-		/// В случае успешной обработки данных метод вернет <c> true </c>.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте http://vk.com/dev/stats.trackVisitor
-		/// </remarks>
+		/// <inheritdoc />
 		public bool TrackVisitor()
 		{
 			return _vk.Call(methodName: "stats.trackVisitor", parameters: VkParameters.Empty);
 		}
 
-		/// <summary>
-		/// Возвращает статистику для записи на стене.
-		/// </summary>
-		/// <param name="ownerId">
-		/// Идентификатор сообщества — владельца записи. Указывается
-		/// со знаком «минус».
-		/// </param>
-		/// <param name="postId">
-		/// Идентификатор записи. Обратите внимание — данные по статистике доступны только
-		/// для 300
-		/// последних(самых свежих) записей на стене сообщества.
-		/// </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Необходимо входить в число руководителей этого сообщества.
-		/// Страница документации ВКонтакте https://vk.com/dev/stats.getPostReach
-		/// </remarks>
+		/// <inheritdoc />
 		public PostReach GetPostReach(long ownerId, long postId)
 		{
 			VkErrors.ThrowIfNumberIsNegative(expr: () => postId);
 
 			var parameters = new VkParameters
 			{
-					{ "owner_id", ownerId }
-					, { "post_id", postId }
+				{ "owner_id", ownerId },
+				{ "post_id", postId }
 			};
 
 			return _vk.Call(methodName: "stats.getPostReach", parameters: parameters);
@@ -125,9 +77,9 @@ namespace VkNet.Categories
 		{
 			var parameters = new VkParameters
 			{
-					{ "group_id", groupId }
-					, { "app_id", appId }
-					, { "date_from", dateFrom.ToString(format: "yyyy-MM-dd") }
+				{ "group_id", groupId },
+				{ "app_id", appId },
+				{ "date_from", dateFrom.ToString(format: "yyyy-MM-dd") }
 			};
 
 			if (dateTo != null)
