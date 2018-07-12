cda4de94d0379e801d71869b362f7f99a1135e3c
atckun <32883483+atckun@users.noreply.github.com>
2018-07-12 15:07:57 +0300

    Update AccountRegisterDeviceParams.cs

--- VkNet/Model/RequestParams/Account/AccountRegisterDeviceParams.cs
+++ VkNet/Model/RequestParams/Account/AccountRegisterDeviceParams.cs
@@ -66,7 +66,7 @@ namespace VkNet.Model.RequestParams
 					, { "device_year", p.DeviceYear }
 					, { "device_id", p.DeviceId }
 					, { "system_version", p.SystemVersion }
-					, { "settings", JsonConvert.SerializeObject(p.Settings); }
+					, { "settings", JsonConvert.SerializeObject(p.Settings) }
 					, { "sandbox", p.Sandbox }
 			};
 
