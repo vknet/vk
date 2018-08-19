2548ae531246f600fb2c2dffd4564da8c9ba44d3
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-08-19 23:46:27 +0300

    #531 Тест для UpdateSubscription

---
+++ VkNet.Tests/Categories/Orders/OrdersUpdateSubscriptionTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace VkNet.Tests.Categories.Orders
+{
+	[TestFixture]
+	public class OrdersUpdateSubscriptionTests : BaseTest
+	{
+		[Test]
+		public void UpdateSubscription()
+		{
+			Url = "https://api.vk.com/method/orders.updateSubscription";
+
+			Json = @"{
+				'response': 1
+			}";
+
+			var result = Api.Orders.UpdateSubscription(123, 234, 500);
+
+			Assert.IsTrue(result);
+		}
+	}
+}
