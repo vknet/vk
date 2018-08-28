5f63c05840bc600d4c3aabffc22d41fa3ced5b11
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-08-28 23:00:23 +0300

    Проверка GPG подписи
    Signed-off-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- VkNet.Tests/Categories/Account/AccountChangePasswordTests.cs
+++ VkNet.Tests/Categories/Account/AccountChangePasswordTests.cs
@@ -8,12 +8,14 @@ namespace VkNet.Tests.Categories
 		public void ChangePassword()
 		{
 			Url = "https://api.vk.com/method/account.changePassword";
+
 			Json = @"{
 				'response': {
 					'token': 'token',
 					'secret': 'secret'
 				}
 			}";
+
 			var result = Api.Account.ChangePassword("old", "new");
 			Assert.That(result.Token, Is.EqualTo("token"));
 		}
