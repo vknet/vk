ab971fc67922887125f433c318ddaa96ca8cd263
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-09-06 23:24:15 +0300

    Метод messages.unpin
    Signed-off-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- VkNet.Tests/Categories/Messages/MessagesPinTests.cs
+++ VkNet.Tests/Categories/Messages/MessagesPinTests.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using NUnit.Framework;
 
 namespace VkNet.Tests.Categories.Messages
 {
 	[TestFixture]
+	[ExcludeFromCodeCoverage]
 	public class MessagesPinTests: BaseTest
 	{
 		[Test]
