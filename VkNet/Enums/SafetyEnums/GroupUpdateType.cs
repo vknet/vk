5705215bd26a505149a37c59c23e042bb5157405
Mifort <themifort@gmail.com>
2018-09-25 12:57:18 +0300

    Rename GroupLongPollUpdateType to GroupUpdateType

--- VkNet/Enums/SafetyEnums/GroupUpdateType.cs
+++ VkNet/Enums/SafetyEnums/GroupUpdateType.cs
@@ -3,191 +3,191 @@ namespace VkNet.Enums.SafetyEnums
 	/// <summary>
 	/// Тип обновления
 	/// </summary>
-	public class GroupLongPollUpdateType : SafetyEnum<GroupLongPollUpdateType>
+	public class GroupUpdateType : SafetyEnum<GroupUpdateType>
 	{
 		/// <summary>
 		/// Новое сообщение
 		/// </summary>
-		public GroupLongPollUpdateType MessageNew = RegisterPossibleValue("message_new");
+		public GroupUpdateType MessageNew = RegisterPossibleValue("message_new");
 
 		/// <summary>
 		/// Новое исходящее сообщение
 		/// </summary>
-		public GroupLongPollUpdateType MessageReply = RegisterPossibleValue("message_reply"); 
+		public GroupUpdateType MessageReply = RegisterPossibleValue("message_reply"); 
 
 		/// <summary>
 		/// Редактирование сообщения
 		/// </summary>
-		public GroupLongPollUpdateType MessageEdit = RegisterPossibleValue("message_edit"); 
+		public GroupUpdateType MessageEdit = RegisterPossibleValue("message_edit"); 
 
 		/// <summary>
 		/// Подписка на сообщения от сообщества
 		/// </summary>
-		public GroupLongPollUpdateType MessageAllow = RegisterPossibleValue("message_allow"); 
+		public GroupUpdateType MessageAllow = RegisterPossibleValue("message_allow"); 
 
 		/// <summary>
 		/// Новый запрет сообщений от сообщества
 		/// </summary>
-		public GroupLongPollUpdateType MessageDeny = RegisterPossibleValue("message_deny"); 
+		public GroupUpdateType MessageDeny = RegisterPossibleValue("message_deny"); 
 
 		/// <summary>
 		/// Добавление фотографии
 		/// </summary>
-		public GroupLongPollUpdateType PhotoNew = RegisterPossibleValue("photo_new"); 
+		public GroupUpdateType PhotoNew = RegisterPossibleValue("photo_new"); 
 
 		/// <summary>
 		/// Добавление комментария к фотографии
 		/// </summary>
-		public GroupLongPollUpdateType PhotoCommentNew = RegisterPossibleValue("photo_comment_new"); 
+		public GroupUpdateType PhotoCommentNew = RegisterPossibleValue("photo_comment_new"); 
 
 		/// <summary>
 		/// Редактирование комментария к фотографии
 		/// </summary>
-		public GroupLongPollUpdateType PhotoCommentEdit = RegisterPossibleValue("photo_comment_edit"); 
+		public GroupUpdateType PhotoCommentEdit = RegisterPossibleValue("photo_comment_edit"); 
 
 		/// <summary>
 		/// Восстановление комментария к фотографии
 		/// </summary>
-		public GroupLongPollUpdateType PhotoCommentRestore = RegisterPossibleValue("photo_comment_restore"); 
+		public GroupUpdateType PhotoCommentRestore = RegisterPossibleValue("photo_comment_restore"); 
 
 		/// <summary>
 		/// Удаление комментария к фотографии
 		/// </summary>
-		public GroupLongPollUpdateType PhotoCommentDelete = RegisterPossibleValue("photo_comment_delete"); 
+		public GroupUpdateType PhotoCommentDelete = RegisterPossibleValue("photo_comment_delete"); 
 
 		/// <summary>
 		/// Добавление аудио
 		/// </summary>
-		public GroupLongPollUpdateType AudioNew = RegisterPossibleValue("audio_new"); 
+		public GroupUpdateType AudioNew = RegisterPossibleValue("audio_new"); 
 
 		/// <summary>
 		/// Добавление видео
 		/// </summary>
-		public GroupLongPollUpdateType VideoNew = RegisterPossibleValue("video_new"); 
+		public GroupUpdateType VideoNew = RegisterPossibleValue("video_new"); 
 
 		/// <summary>
 		/// Добавление комментария к видео
 		/// </summary>
-		public GroupLongPollUpdateType VideoCommentNew = RegisterPossibleValue("video_comment_new"); 
+		public GroupUpdateType VideoCommentNew = RegisterPossibleValue("video_comment_new"); 
 
 		/// <summary>
 		/// Редактирование комментария к видео
 		/// </summary>
-		public GroupLongPollUpdateType VideoCommentEdit = RegisterPossibleValue("video_comment_edit"); 
+		public GroupUpdateType VideoCommentEdit = RegisterPossibleValue("video_comment_edit"); 
 
 		/// <summary>
 		/// Восстановление комментария к видео
 		/// </summary>
-		public GroupLongPollUpdateType VideoCommentRestore = RegisterPossibleValue("video_comment_restore"); 
+		public GroupUpdateType VideoCommentRestore = RegisterPossibleValue("video_comment_restore"); 
 
 		/// <summary>
 		/// Удаление комментария к видео
 		/// </summary>
-		public GroupLongPollUpdateType VideoCommentDelete = RegisterPossibleValue("video_comment_delete"); 
+		public GroupUpdateType VideoCommentDelete = RegisterPossibleValue("video_comment_delete"); 
 
 		/// <summary>
 		/// Добавление записи на стене
 		/// </summary>
-		public GroupLongPollUpdateType WallPostNew = RegisterPossibleValue("wall_post_new"); 
+		public GroupUpdateType WallPostNew = RegisterPossibleValue("wall_post_new"); 
 
 		/// <summary>
 		/// Репост записи на стене
 		/// </summary>
-		public GroupLongPollUpdateType WallRepost = RegisterPossibleValue("wall_repost"); 
+		public GroupUpdateType WallRepost = RegisterPossibleValue("wall_repost"); 
 
 		/// <summary>
 		/// Добавление комментария на стене
 		/// </summary>
-		public GroupLongPollUpdateType WallReplyNew = RegisterPossibleValue("wall_reply_new"); 
+		public GroupUpdateType WallReplyNew = RegisterPossibleValue("wall_reply_new"); 
 
 		/// <summary>
 		/// Редактирование комментария на стене
 		/// </summary>
-		public GroupLongPollUpdateType WallReplyEdit = RegisterPossibleValue("wall_reply_edit"); 
+		public GroupUpdateType WallReplyEdit = RegisterPossibleValue("wall_reply_edit"); 
 
 		/// <summary>
 		/// Восстановление комментария на стене
 		/// </summary>
-		public GroupLongPollUpdateType WallReplyRestore = RegisterPossibleValue("wall_reply_restore"); 
+		public GroupUpdateType WallReplyRestore = RegisterPossibleValue("wall_reply_restore"); 
 
 		/// <summary>
 		/// Удаление комментария на стене
 		/// </summary>
-		public GroupLongPollUpdateType WallReplyDelete = RegisterPossibleValue("wall_reply_delete"); 
+		public GroupUpdateType WallReplyDelete = RegisterPossibleValue("wall_reply_delete"); 
 
 		/// <summary>
 		/// Добавление комментария в обсуждении
 		/// </summary>
-		public GroupLongPollUpdateType BoardPostNew = RegisterPossibleValue("board_post_new"); 
+		public GroupUpdateType BoardPostNew = RegisterPossibleValue("board_post_new"); 
 
 		/// <summary>
 		/// Редактирование комментария в обсуждении
 		/// </summary>
-		public GroupLongPollUpdateType BoardPostEdit = RegisterPossibleValue("board_post_edit"); 
+		public GroupUpdateType BoardPostEdit = RegisterPossibleValue("board_post_edit"); 
 
 		/// <summary>
 		/// Восстановление комментария в обсуждении
 		/// </summary>
-		public GroupLongPollUpdateType BoardPostRestore = RegisterPossibleValue("board_post_restore"); 
+		public GroupUpdateType BoardPostRestore = RegisterPossibleValue("board_post_restore"); 
 
 		/// <summary>
 		/// Удаление комментария в обсуждении
 		/// </summary>
-		public GroupLongPollUpdateType BoardPostDelete = RegisterPossibleValue("board_post_delete"); 
+		public GroupUpdateType BoardPostDelete = RegisterPossibleValue("board_post_delete"); 
 
 		/// <summary>
 		/// Добавление комментария к товару
 		/// </summary>
-		public GroupLongPollUpdateType MarketCommentNew = RegisterPossibleValue("market_comment_new"); 
+		public GroupUpdateType MarketCommentNew = RegisterPossibleValue("market_comment_new"); 
 
 		/// <summary>
 		/// Редактирование комментария к товару
 		/// </summary>
-		public GroupLongPollUpdateType MarketCommentEdit = RegisterPossibleValue("market_comment_edit"); 
+		public GroupUpdateType MarketCommentEdit = RegisterPossibleValue("market_comment_edit"); 
 
 		/// <summary>
 		/// Восстановление комментария к товару
 		/// </summary>
-		public GroupLongPollUpdateType MarketCommentRestore = RegisterPossibleValue("market_comment_restore"); 
+		public GroupUpdateType MarketCommentRestore = RegisterPossibleValue("market_comment_restore"); 
 
 		/// <summary>
 		/// Удаление комментария к товару
 		/// </summary>
-		public GroupLongPollUpdateType MarketCommentDelete = RegisterPossibleValue("market_comment_delete"); 
+		public GroupUpdateType MarketCommentDelete = RegisterPossibleValue("market_comment_delete"); 
 
 		/// <summary>
 		/// Удаление участника из группы
 		/// </summary>
-		public GroupLongPollUpdateType GroupLeave = RegisterPossibleValue("group_leave"); 
+		public GroupUpdateType GroupLeave = RegisterPossibleValue("group_leave"); 
 
 		/// <summary>
 		/// Добавление участника или заявки на вступление в сообщество
 		/// </summary>
-		public GroupLongPollUpdateType GroupJoin = RegisterPossibleValue("group_join"); 
+		public GroupUpdateType GroupJoin = RegisterPossibleValue("group_join"); 
 
 		/// <summary>
 		/// Добавление пользователя в черный список
 		/// </summary>
-		public GroupLongPollUpdateType UserBlock = RegisterPossibleValue("user_block"); 
+		public GroupUpdateType UserBlock = RegisterPossibleValue("user_block"); 
 
 		/// <summary>
 		/// Удаление пользователя из черного списка
 		/// </summary>
-		public GroupLongPollUpdateType UserUnblock = RegisterPossibleValue("user_unblock"); 
+		public GroupUpdateType UserUnblock = RegisterPossibleValue("user_unblock"); 
 
 		/// <summary>
 		/// Добавление голоса в публичном опросе
 		/// </summary>
-		public GroupLongPollUpdateType PollVoteNew = RegisterPossibleValue("poll_vote_new"); 
+		public GroupUpdateType PollVoteNew = RegisterPossibleValue("poll_vote_new"); 
 
 		/// <summary>
 		/// Редактирование списка руководителей
 		/// </summary>
-		public GroupLongPollUpdateType GroupOfficersEdit = RegisterPossibleValue("group_officers_edit"); 
+		public GroupUpdateType GroupOfficersEdit = RegisterPossibleValue("group_officers_edit"); 
 
 		/// <summary>
 		/// Изменение главного фото
 		/// </summary>
-		public GroupLongPollUpdateType GroupChangePhoto = RegisterPossibleValue("group_change_photo"); 
+		public GroupUpdateType GroupChangePhoto = RegisterPossibleValue("group_change_photo"); 
 	}
 }
