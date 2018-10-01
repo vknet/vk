9db920546d86decac90af88dd093d658791afa6d
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-10-01 23:21:52 +0300

    #684 В messages.send появился параметр dont_parse_links.

--- VkNet/Model/RequestParams/Messages/MessagesSendParams.cs
+++ VkNet/Model/RequestParams/Messages/MessagesSendParams.cs
@@ -16,7 +16,7 @@ namespace VkNet.Model.RequestParams
 		/// <summary>
 		/// Идентификатор пользователя, которому отправляется сообщение.
 		/// </summary>
-		[JsonProperty(propertyName: "user_id")]
+		[JsonProperty("user_id")]
 		public long? UserId { get; set; }
 
 		/// <summary>
@@ -24,58 +24,58 @@ namespace VkNet.Model.RequestParams
 		/// одинакового сообщения.
 		/// Сохраняется вместе с сообщением и доступен в истории сообщений.
 		/// </summary>
-		[JsonProperty(propertyName: "random_id")]
+		[JsonProperty("random_id")]
 		public long? RandomId { get; set; }
 
 		/// <summary>
 		/// Идентификатор назначения. Для групповой беседы: 2000000000 + id беседы. Для
 		/// сообщества: -id сообщества.
 		/// </summary>
-		[JsonProperty(propertyName: "peer_id")]
+		[JsonProperty("peer_id")]
 		public long? PeerId { get; set; }
 
 		/// <summary>
 		/// Короткий адрес пользователя (например, illarionov).
 		/// </summary>
-		[JsonProperty(propertyName: "domain")]
+		[JsonProperty("domain")]
 		public string Domain { get; set; }
 
 		/// <summary>
 		/// Идентификатор беседы, к которой будет относиться сообщение.
 		/// </summary>
-		[JsonProperty(propertyName: "chat_id")]
+		[JsonProperty("chat_id")]
 		public long? ChatId { get; set; }
 
 		/// <summary>
 		/// Идентификаторы получателей сообщения (при необходимости отправить сообщение
 		/// сразу нескольким пользователям).
 		/// </summary>
-		[JsonProperty(propertyName: "user_ids")]
+		[JsonProperty("user_ids")]
 		public IEnumerable<long> UserIds { get; set; }
 
 		/// <summary>
 		/// Текст личного сообщения (является обязательным, если не задан параметр
 		/// attachment)
 		/// </summary>
-		[JsonProperty(propertyName: "message")]
+		[JsonProperty("message")]
 		public string Message { get; set; }
 
 		/// <summary>
 		/// Latitude - широта при добавлении местоположения.
 		/// </summary>
-		[JsonProperty(propertyName: "lat")]
+		[JsonProperty("lat")]
 		public double? Lat { get; set; }
 
 		/// <summary>
 		/// Longitude - долгота при добавлении местоположения.
 		/// </summary>
-		[JsonProperty(propertyName: "long")]
+		[JsonProperty("long")]
 		public double? Longitude { get; set; }
 
 		/// <summary>
 		/// Медиавложения к личному сообщению, перечисленные через запятую.
 		/// </summary>
-		[JsonProperty(propertyName: "attachment")]
+		[JsonProperty("attachment")]
 		public IEnumerable<MediaAttachment> Attachments { get; set; }
 
 		/// <summary>
@@ -83,39 +83,50 @@ namespace VkNet.Model.RequestParams
 		/// Перечисленные сообщения отправителя будут
 		/// отображаться в теле письма у получателя.
 		/// </summary>
-		[JsonProperty(propertyName: "forward_messages")]
+		[JsonProperty("forward_messages")]
 		public IEnumerable<long> ForwardMessages { get; set; }
 
 		/// <summary>
-		/// Отправляемая клавиатура.
-		/// (Используется для сообщений сообщества с включенной клавиатурой)
+		/// Объект, описывающий клавиатуру для бота.
 		/// </summary>
-		[JsonProperty(propertyName: "keyboard")]
+		[JsonProperty("keyboard")]
 		public MessageKeyboard Keyboard { get; set; }
 
 		/// <summary>
 		/// Идентификатор стикера.
 		/// </summary>
-		[JsonProperty(propertyName: "sticker_id")]
+		[JsonProperty("sticker_id")]
 		public uint? StickerId { get; set; }
 
 		/// <summary>
 		/// Идентификатор капчи
 		/// </summary>
-		[JsonProperty(propertyName: "captcha_sid")]
+		[JsonProperty("captcha_sid")]
 		public long? CaptchaSid { get; set; }
 
 		/// <summary>
 		/// текст, который ввел пользователь
 		/// </summary>
-		[JsonProperty(propertyName: "captcha_key")]
+		[JsonProperty("captcha_key")]
 		public string CaptchaKey { get; set; }
-		
+
 		/// <summary>
 		/// Полезная нагрузка (максимальная длина 1000)
 		/// </summary>
-		[JsonProperty(propertyName: "payload")]
+		[JsonProperty("payload")]
 		public string Payload { get; set; }
+
+		/// <summary>
+		/// Идентификатор сообщества (для сообщений сообщества с ключом доступа пользователя). положительное число
+		/// </summary>
+		[JsonProperty("group_id")]
+		public ulong GroupId { get; set; }
+
+		/// <summary>
+		/// 1 — не создавать сниппет ссылки из сообщения флаг, может принимать значения 1 или 0, по умолчанию
+		/// </summary>
+		[JsonProperty("dont_parse_links")]
+		public bool DontParseLinks { get; set; }
 
 		/// <summary>
 		/// Привести к типу VkParameters.
@@ -126,22 +137,24 @@ namespace VkNet.Model.RequestParams
 		{
 			return new VkParameters
 			{
-					{ "user_id", p.UserId }
-					, { "domain", p.Domain }
-					, { "chat_id", p.ChatId }
-					, { "user_ids", p.UserIds }
-					, { "message", p.Message }
-					, { "random_id", p.RandomId }
-					, { "lat", p.Lat }
-					, { "long", p.Longitude }
-					, { "attachment", p.Attachments }
-					, { "forward_messages", p.ForwardMessages }
-					, { "keyboard" , p.Keyboard!=null ? JsonConvert.SerializeObject(p.Keyboard) : "" }
-					, { "sticker_id", p.StickerId }
-					, { "captcha_sid", p.CaptchaSid }
-					, { "captcha_key", p.CaptchaKey }
-					, { "peer_id", p.PeerId }
-					, { "payload", p.Payload }
+				{ "user_id", p.UserId },
+				{ "domain", p.Domain },
+				{ "chat_id", p.ChatId },
+				{ "user_ids", p.UserIds },
+				{ "message", p.Message },
+				{ "random_id", p.RandomId },
+				{ "lat", p.Lat },
+				{ "long", p.Longitude },
+				{ "attachment", p.Attachments },
+				{ "forward_messages", p.ForwardMessages },
+				{ "keyboard", p.Keyboard != null ? JsonConvert.SerializeObject(p.Keyboard) : "" },
+				{ "sticker_id", p.StickerId },
+				{ "captcha_sid", p.CaptchaSid },
+				{ "captcha_key", p.CaptchaKey },
+				{ "peer_id", p.PeerId },
+				{ "payload", p.Payload },
+				{ "group_id", p.GroupId },
+				{ "dont_parse_links", p.DontParseLinks }
 			};
 		}
 	}
