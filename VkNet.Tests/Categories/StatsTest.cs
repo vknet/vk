6208348e94cae810667ccf550fb3426901a5fcde
Daniel Vygolov <dvygolov@users.noreply.github.com>
2018-10-25 22:04:45 +0400

    Добавил конвертер и десериализацию пустого Activity, которая приходит из ВК в виде пустого массива (#706)

--- VkNet.Tests/Categories/StatsTest.cs
+++ VkNet.Tests/Categories/StatsTest.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Diagnostics.CodeAnalysis;
-using NUnit.Framework;
 using VkNet.Categories;
 
 namespace VkNet.Tests.Categories
@@ -229,6 +229,111 @@ namespace VkNet.Tests.Categories
 				new DateTime(2015, 11, 11, 0, 0, 0, DateTimeKind.Utc));
 
 			Assert.NotNull(statsPeriods[0]);
+
+			Assert.That(new DateTime(2013, 09, 08), Is.EqualTo(statsPeriods[0].PeriodFrom));
+		}
+
+		[Test]
+		public void GetByGroup_EmptyActivityCase()
+		{
+			const string url = "https://api.vk.com/method/stats.get";
+
+			const string json =
+				@"{
+					response: [{
+						period_from: '2013-09-08',
+						period_to: '2013-09-08',
+						visitors: {
+							views: 3688,
+							visitors: 2768,
+							mobile_views: 130,
+							sex: [
+								{
+									count: 339,
+									value: 'f'
+								},
+								{
+									count: 857,
+									value: 'm'
+								}
+							],
+							age: [
+								{
+									count: 326,
+									value: '12-18'
+								}
+							],
+							sex_age: [
+								{
+									count: 112,
+									value: 'f;12-18'
+								}
+							],
+							countries: [
+								{
+									count: 2372,
+									value: 1,
+									code: 'RU',
+									name: 'Россия'
+								}
+							],
+							cities: [
+								{
+									count: 303,
+									value: 1,
+									name: 'Москва'
+								}
+							]
+						},
+						reach: {
+							reach: 458,
+							reach_subscribers: 119,
+							mobile_reach: 68,
+							sex: [
+								{
+									count: 162,
+									value: 'f'
+								}
+							],
+							age: [
+								{
+									count: 130,
+									value: '12-18'
+								}
+							],
+							sex_age: [
+								{
+									count: 53,
+									value: 'f;12-18'
+								}
+							],
+							countries: [
+								{
+									count: 344,
+									value: 1,
+									code: 'RU',
+									name: 'Россия'
+								}
+							],
+							cities: [
+								{
+									count: 72,
+									value: 1,
+									name: 'Москва'
+								}
+							]
+						},
+						activity: []
+					}]
+				  }";
+
+			var mockedStatsCategory = GetMockedStatsCategory(url, json);
+
+			var statsPeriods = mockedStatsCategory.GetByGroup(1,
+				new DateTime(2015, 11, 11, 0, 0, 0, DateTimeKind.Utc));
+
+			Assert.NotNull(statsPeriods[0]);
+			Assert.Null(statsPeriods[0].Activity);
 
 			Assert.That(new DateTime(2013, 09, 08), Is.EqualTo(statsPeriods[0].PeriodFrom));
 		}
---
+++ VkNet/Utils/JsonConverter/IgnoreUnexpectedArraysConverter.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+
+namespace VkNet.Utils.JsonConverter
+{
+	public class IgnoreUnexpectedArraysConverter<T> : IgnoreUnexpectedArraysConverterBase
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return typeof(T).IsAssignableFrom(objectType);
+		}
+	}
+
+	public class IgnoreUnexpectedArraysConverter : IgnoreUnexpectedArraysConverterBase
+	{
+		private readonly IContractResolver resolver;
+
+		public IgnoreUnexpectedArraysConverter(IContractResolver resolver)
+		{
+			if (resolver == null)
+				throw new ArgumentNullException();
+			this.resolver = resolver;
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			if (objectType.IsPrimitive || objectType == typeof(string))
+				return false;
+			return resolver.ResolveContract(objectType) is JsonObjectContract;
+		}
+	}
+
+	public abstract class IgnoreUnexpectedArraysConverterBase : Newtonsoft.Json.JsonConverter
+	{
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			var contract = serializer.ContractResolver.ResolveContract(objectType);
+			if (!(contract is JsonObjectContract))
+			{
+				throw new JsonSerializationException(string.Format("{0} is not a JSON object", objectType));
+			}
+
+			do
+			{
+				if (reader.TokenType == JsonToken.Null)
+				{
+					return null;
+				} else if (reader.TokenType == JsonToken.StartArray)
+				{
+					var array = JArray.Load(reader);
+					if (array.Count > 0)
+						throw new JsonSerializationException("Array was not empty.");
+					return null;
+				} else if (reader.TokenType == JsonToken.StartObject)
+				{
+					// Prevent infinite recursion by using Populate()
+					existingValue = existingValue ?? contract.DefaultCreator();
+					serializer.Populate(reader, existingValue);
+					return existingValue;
+				} else
+				{
+					throw new JsonSerializationException(string.Format("Unexpected token {0}", reader.TokenType));
+				}
+			}
+			while (reader.Read());
+			throw new JsonSerializationException("Unexpected end of JSON.");
+		}
+
+		public override bool CanWrite { get { return false; } }
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
