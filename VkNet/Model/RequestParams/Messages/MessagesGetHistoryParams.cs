77a917547242742260abdc913fdf0b06107283e5
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-11-05 00:49:49 +0300

    Доработать параметры messages.getHistoryAttachments #723

--- VkNet/Model/RequestParams/Messages/MessagesGetHistoryParams.cs
+++ VkNet/Model/RequestParams/Messages/MessagesGetHistoryParams.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using VkNet.Utils;
 
 namespace VkNet.Model.RequestParams
@@ -12,26 +13,36 @@ namespace VkNet.Model.RequestParams
 	public class MessagesGetHistoryParams
 	{
 		/// <summary>
+		/// Идентификатор пользователя, историю переписки с которым необходимо вернуть.
+		/// </summary>
+		[JsonProperty("user_id")]
+		public long? UserId { get; set; }
+
+		/// <summary>
+		/// Список дополнительных полей профилей и сообществ, которые необходимо вернуть.
+		/// </summary>
+		[JsonProperty("fields")]
+		public IEnumerable<string> Fields { get; set; }
+
+		/// <summary>
 		/// Смещение, необходимое для выборки определенного подмножества сообщений,
 		/// должен быть 0 или положительным, если не передан параметр start_message_id, и
 		/// должен быть 0 или отрицательным, если
 		/// передан.
 		/// </summary>
+		[JsonProperty("offset")]
 		public long? Offset { get; set; }
 
 		/// <summary>
 		/// Количество сообщений, которое необходимо получить (но не более 200)
 		/// </summary>
+		[JsonProperty("count")]
 		public long? Count { get; set; }
-
-		/// <summary>
-		/// Идентификатор пользователя, историю переписки с которым необходимо вернуть.
-		/// </summary>
-		public long? UserId { get; set; }
 
 		/// <summary>
 		/// Идентификатор назначения.
 		/// </summary>
+		[JsonProperty("peer_id")]
 		public long? PeerId { get; set; }
 
 		/// <summary>
@@ -40,12 +51,14 @@ namespace VkNet.Model.RequestParams
 		/// если же передано значение -1, то к значению параметра offset прибавляется
 		/// количество входящих непрочитанных сообщений в конце диалога
 		/// </summary>
+		[JsonProperty("start_message_id")]
 		public long? StartMessageId { get; set; }
 
 		/// <summary>
 		/// Возвращать сообщения в хронологическом порядке или обратном (по умолчанию в
 		/// обратном). Недоступен при переданном start_message_id.
 		/// </summary>
+		[JsonProperty("rev")]
 		public bool? Reversed { get; set; }
 
 		/// <summary>
@@ -53,17 +66,14 @@ namespace VkNet.Model.RequestParams
 		/// то будет возвращена информация о пользователях и сообществах, являющихся авторами сообщений.
 		/// <remarks>По умолчанию <c>false</c>.</remarks>
 		/// </summary>
+		[JsonProperty("extended")]
 		public bool? Extended { get; set; }
 
 		/// <summary>
 		/// Идентификатор сообщества (для сообщений сообщества с ключом доступа пользователя).
 		/// </summary>
+		[JsonProperty("group_id")]
 		public ulong? GroupId { get; set; }
-
-		/// <summary>
-		/// Список дополнительных полей профилей и сообществ, которые необходимо вернуть.
-		/// </summary>
-		public IEnumerable<string> Fields { get; set; }
 
 		/// <summary>
 		/// Привести к типу VkParameters.
@@ -74,14 +84,14 @@ namespace VkNet.Model.RequestParams
 		{
 			return new VkParameters
 			{
+				{ "user_id", p.UserId },
+				{ "fields", p.Fields },
 				{ "offset", p.Offset },
 				{ "count", p.Count },
-				{ "user_id", p.UserId },
 				{ "peer_id", p.PeerId },
 				{ "start_message_id", p.StartMessageId },
 				{ "rev", p.Reversed },
 				{ "extended", p.Extended },
-				{ "fields", p.Fields },
 				{ "group_id", p.GroupId }
 			};
 		}
