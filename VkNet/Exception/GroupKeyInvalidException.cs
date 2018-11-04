2633806c7389e4c36ebee27f0c7d5e5d65a5742f
itsokto <36706257+itsokto@users.noreply.github.com>
2018-11-04 19:28:24 +0300

    #689 Исправлены конструкторы для некоторых Exceptions (#713)

--- VkNet/Exception/GroupKeyInvalidException.cs
+++ VkNet/Exception/GroupKeyInvalidException.cs
@@ -14,6 +14,7 @@ namespace VkNet.Exception
 		/// <inheritdoc />
 		public GroupKeyInvalidException(VkResponse response) : base(message: response[key: "error_msg"])
 		{
+			ErrorCode = response[key: "error_code"];
 		}
 	}
 }
