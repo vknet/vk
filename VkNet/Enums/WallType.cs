871ae419723db109848fb2ef0daafc9e1741d264
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-11-05 21:09:45 +0300

    #715 В Group у поля wall поменялись значения.

---
+++ VkNet/Enums/WallType.cs
@@ -0,0 +1,28 @@
+namespace VkNet.Enums
+{
+	/// <summary>
+	/// Тип стены
+	/// </summary>
+	public enum WallType
+	{
+		/// <summary>
+		/// Выключена
+		/// </summary>
+		Off = 0,
+
+		/// <summary>
+		/// Открытая
+		/// </summary>
+		Open = 1,
+
+		/// <summary>
+		/// Ограниченная
+		/// </summary>
+		Limited = 2,
+
+		/// <summary>
+		/// Закрытая
+		/// </summary>
+		Closed = 3
+	}
+}
