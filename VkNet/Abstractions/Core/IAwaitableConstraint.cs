a803ae578fdf430172573d7800a23731de01b259
itsokto <picnet9@gmail.com>
2018-11-13 16:50:11 +0300

    Написал документацию

--- VkNet/Abstractions/Core/IAwaitableConstraint.cs
+++ VkNet/Abstractions/Core/IAwaitableConstraint.cs
@@ -4,10 +4,25 @@ using System.Threading.Tasks;
 
 namespace VkNet.Abstractions.Core
 {
+	/// <summary>
+	/// The <see cref="IAwaitableConstraint"/>.
+	/// </summary>
 	public interface IAwaitableConstraint
 	{
+		/// <summary>
+		/// Starts a <see cref="Task"/> that will complete after readiness.
+		/// </summary>
 		Task<IDisposable> WaitForReadiness(CancellationToken cancellationToken);
 
-		void SetRate(int count, TimeSpan timeSpan);
+		/// <summary>
+		/// Sets a rate for a constraint.
+		/// </summary>
+		/// <param name="number">
+		/// Maximum <paramref name="number"/> times per <paramref name="timeSpan"/>.
+		/// </param>
+		/// <param name="timeSpan">
+		/// Time interval.
+		/// </param>
+		void SetRate(int number, TimeSpan timeSpan);
 	}
 }
