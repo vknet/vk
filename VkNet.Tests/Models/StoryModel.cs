470b1220d26044be69cb34f09c3a69688d428c7f
itsokto <noreplyitsokto@gmail.com>
2018-12-21 19:39:03 +0300

    Тест для модели Story

---
+++ VkNet.Tests/Models/StoryModel.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.Attachments;
+
+namespace VkNet.Tests.Models
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class StoryModel : BaseTest
+	{
+		[Test]
+		public void ShouldDeserializeFromVkResponseToAttachment()
+		{
+			ReadJsonFile("Models/story_attachment");
+
+			var response = GetResponse();
+
+			var attachment = Attachment.FromJson(response);
+
+			Assert.True(attachment.Instance is Story);
+		}
+	}
+}
