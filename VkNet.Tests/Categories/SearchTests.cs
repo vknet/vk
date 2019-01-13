7c55e21b8400cfd5983fa2b209bb3d18a74b7b69
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-01-13 23:23:08 +0300

    #700 SearchTests

--- VkNet.Tests/Categories/SearchTests.cs
+++ VkNet.Tests/Categories/SearchTests.cs
@@ -1,55 +1,24 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using NUnit.Framework;
 using VkNet.Model.RequestParams;
+using VkNet.Tests.Infrastructure;
 
 namespace VkNet.Tests.Categories
 {
 	[TestFixture]
 	[ExcludeFromCodeCoverage]
-	public class SearchTests : BaseTest
+	public class SearchTests : CategoryBaseTest
 	{
+		protected override string Folder => "Search";
+
 		[Test]
 		public void GetHints()
 		{
-			Json = @"
-            {
-                'response': {
-                    'count': 189,
-                    'items': [
-                        {
-                            'type': 'group',
-                            'group': {
-                                'id': 1,
-                                'name': 'ВКонтакте API',
-                                'screen_name': 'apiclub',
-                                'is_closed': 0,
-                                'type': 'group',
-                                'is_admin': 0,
-                                'is_member': 1,
-                                'photo_50': 'https://pp.userap...fc3/ei2DXpoO0h8.jpg',
-                                'photo_100': 'https://pp.userap...fc2/nlmcXQP0V6c.jpg',
-                                'photo_200': 'https://pp.userap...fc0/_QdFMmxSRyY.jpg'
-                            },
-                            'section': 'groups',
-                            'description': 'Группа, 320 437 участников',
-                            'global': 1
-                        },
-                        {
-                            'type': 'profile',
-                            'profile': {
-                                'id': 72815776,
-                                'first_name': 'Антон',
-                                'last_name': 'Минаев'
-                            },
-                            'section': 'friends',
-                            'description': 'ЮРГТУ (НПИ), Новочеркасск'
-                        }
-                    ]
-                }
-            }";
-
 			Url = "https://api.vk.com/method/search.getHints";
+			ReadCategoryJsonPath(nameof(GetHints));
+
 			var result = Api.Search.GetHints(new SearchGetHintsParams());
+
 			Assert.NotNull(result);
 		}
 	}
