a7ef47b5d2edb8492c05303d47a8b6c3668907fd
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-01-13 23:31:58 +0300

    #700 PlacesCategoryTests

--- VkNet.Tests/Categories/PlacesCategoryTests.cs
+++ VkNet.Tests/Categories/PlacesCategoryTests.cs
@@ -1,23 +1,21 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using NUnit.Framework;
 using VkNet.Model.RequestParams;
+using VkNet.Tests.Infrastructure;
 
 namespace VkNet.Tests.Categories
 {
 	[TestFixture]
 	[ExcludeFromCodeCoverage]
-	public class PlacesCategoryTests : BaseTest
+	public class PlacesCategoryTests : CategoryBaseTest
 	{
+		protected override string Folder => "Places";
+
 		[Test]
 		public void Add()
 		{
 			Url = "https://api.vk.com/method/places.add";
-
-			Json = @"{
-				response: {
-					id: 6162171
-				}
-			}";
+			ReadCategoryJsonPath(nameof(Add));
 
 			var result = Api.Places.Add(new PlacesAddParams());
 
@@ -28,10 +26,7 @@ namespace VkNet.Tests.Categories
 		public void Checkin()
 		{
 			Url = "https://api.vk.com/method/places.checkin";
-
-			Json = @"{
-				response: 6162171
-			}";
+			ReadCategoryJsonPath(nameof(Checkin));
 
 			var result = Api.Places.Checkin(new PlacesCheckinParams());
 
@@ -42,20 +37,7 @@ namespace VkNet.Tests.Categories
 		public void GetById()
 		{
 			Url = "https://api.vk.com/method/places.getById";
-
-			Json = @"{
-				response: [{
-					id: 4980426,
-					title: 'test',
-					latitude: 25,
-					longitude: 25,
-					created: 1364117621,
-					icon: 'http://vk.com/images/places/home.png',
-					checkins: 18,
-					updated: 1375926671,
-					type: 1
-				}]
-			}";
+			ReadCategoryJsonPath(nameof(GetById));
 
 			var result = Api.Places.GetById(new ulong[] { 123 });
 
@@ -66,30 +48,7 @@ namespace VkNet.Tests.Categories
 		public void GetCheckins()
 		{
 			Url = "https://api.vk.com/method/places.getCheckins";
-
-			Json = @"{
-				response: {
-					count: 68,
-					items: [{
-					id: '973782_2187',
-						user_id: 973782,
-						post_id: 2187,
-						date: 1530002871,
-						latitude: 0,
-						longitude: 0,
-						text: 'text',
-						place_id: 0,
-						place_title: '',
-						place_country: 0,
-						place_city: 0,
-						place_address: '',
-						place_type: 0,
-						place_icon: 'http://vk.com/images/places/place.png',
-						distance: 7062029
-					}]
-				}
-			}";
-
+			ReadCategoryJsonPath(nameof(GetCheckins));
 
 			var result = Api.Places.GetCheckins(new PlacesGetCheckinsParams());
 
@@ -100,15 +59,7 @@ namespace VkNet.Tests.Categories
 		public void GetTypes()
 		{
 			Url = "https://api.vk.com/method/places.getTypes";
-
-			Json = @"{
-				response: [{
-					id: 1,
-					title: 'Home',
-					icon: 'http://vk.com/images/places/home.png'
-				}]
-			}";
-
+			ReadCategoryJsonPath(nameof(GetTypes));
 
 			var result = Api.Places.GetTypes();
 
@@ -119,28 +70,7 @@ namespace VkNet.Tests.Categories
 		public void Search()
 		{
 			Url = "https://api.vk.com/method/places.search";
-
-			Json = @"{
-				response: {
-					count: 1999,
-					items: [{
-						id: 213784,
-						title: 'Дом Зингера',
-						latitude: 59.935624,
-						longitude: 30.325875,
-						created: 1295369575,
-						icon: 'http://vk.com/images/places/work.png',
-						checkins: 355,
-						updated: 1439407347,
-						type: 2,
-						country: 1,
-						city: 2,
-						address: 'Невский проспект 28',
-						distance: 3
-					}]
-				}
-			}";
-
+			ReadCategoryJsonPath(nameof(Search));
 
 			var result = Api.Places.Search(new PlacesSearchParams());
 
