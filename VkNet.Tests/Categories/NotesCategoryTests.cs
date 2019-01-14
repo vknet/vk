156d978e2a40e7a662bfc12190ef4bca7a2e29fc
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-01-14 22:39:09 +0300

    #700 NotesCategoryTests

--- VkNet.Tests/Categories/NotesCategoryTests.cs
+++ VkNet.Tests/Categories/NotesCategoryTests.cs
@@ -14,26 +14,22 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void Add()
 		{
-			Json = @"
-            {
-				'response': 11825220
-			}";
-
 			Url = "https://api.vk.com/method/notes.add";
+			ReadCategoryJsonPath(nameof(Add));
+
 			var result = Api.Notes.Add(new NotesAddParams());
+
 			Assert.AreEqual(11825220, result);
 		}
 
 		[Test]
 		public void CreateComment()
 		{
-			Json = @"
-			{
-				'response': 11825220
-			}";
-
 			Url = "https://api.vk.com/method/notes.createComment";
+			ReadCategoryJsonPath(nameof(CreateComment));
+
 			var result = Api.Notes.CreateComment(new NotesCreateCommentParams());
+
 			Assert.AreEqual(11825220, result);
 		}
 
@@ -84,71 +80,33 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void Get()
 		{
-			Json = @"
-			{
-				'response': {
-					'count': 123,
-					items: [
-						{
-							'id': 11801953,
-							'owner_id': 66748,
-							'comments': 0,
-							'date': 1437379505,
-							'title': 'Без названия',
-							'text': 'text',
-							'view_url': 'https://m.vk.com/63a975c&lang=en'
-						}
-					]
-				}
-			}";
-
 			Url = "https://api.vk.com/method/notes.get";
+			ReadCategoryJsonPath(nameof(Get));
+
 			var result = Api.Notes.Get(new NotesGetParams());
+
 			Assert.IsNotEmpty(result);
 		}
 
 		[Test]
 		public void GetById()
 		{
-			Json = @"
-			{
-				'response': {
-					'id': 11801953,
-					'owner_id': 66748,
-					'comments': 0,
-					'date': 1437379505,
-					'title': 'Без названия',
-					'text': 'text',
-					'view_url': 'https://m.vk.com/63a975c&lang=en'
-				}
-			}";
-
 			Url = "https://api.vk.com/method/notes.getById";
+			ReadCategoryJsonPath(nameof(GetById));
+
 			var result = Api.Notes.GetById(new NotesGetByIdParams());
+
 			Assert.IsNotNull(result);
 		}
 
 		[Test]
 		public void GetComments()
 		{
-			Json = @"
-			{
-				'response': {
-					'count': 0,
-					'items': [{
-						'id': '26575410',
-						'uid': '185014513',
-						'nid': '11751647',
-						'date': '1367265714',
-						'message': '<!--rid:2360-->new comment ',
-						'reply_to': '0',
-						'oid': 2314852
-					}]
-				}
-			}";
-
 			Url = "https://api.vk.com/method/notes.getComments";
+			ReadCategoryJsonPath(nameof(GetComments));
+
 			var result = Api.Notes.GetComments(new NotesGetCommentParams());
+
 			Assert.IsNotEmpty(result);
 		}
 
