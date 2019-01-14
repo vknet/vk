5fae825731e01cb7199bcf8c90e95904d99a2467
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-01-14 22:50:44 +0300

    #700 LikesCategoryTest

--- VkNet.Tests/Categories/LikesCategoryTest.cs
+++ VkNet.Tests/Categories/LikesCategoryTest.cs
@@ -1,43 +1,28 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using NUnit.Framework;
-using VkNet.Categories;
 using VkNet.Enums.SafetyEnums;
 using VkNet.Model.RequestParams;
+using VkNet.Tests.Infrastructure;
 
 namespace VkNet.Tests.Categories
 {
 	[TestFixture]
 	[SuppressMessage("ReSharper", "PublicMembersMustHaveComments")]
 	[ExcludeFromCodeCoverage]
-	public class LikesCategoryTest : BaseTest
+	public class LikesCategoryTest : CategoryBaseTest
 	{
-		private LikesCategory GetMockedLikesCategory(string url, string json)
-		{
-			Json = json;
-			Url = url;
-
-			return new LikesCategory(Api);
-		}
+		protected override string Folder => "Likes";
 
 		[Test]
 		public void Add_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/likes.add";
+			Url = "https://api.vk.com/method/likes.add";
+			ReadCategoryJsonPath(nameof(Add_NormalCase));
 
-			const string json =
-					@"{
-					response: {
-						likes: 5
-					}
-				}";
-
-			var likesCategory = GetMockedLikesCategory(url, json);
-
-			var like = likesCategory.Add(new LikesAddParams
+			var like = Api.Likes.Add(new LikesAddParams
 			{
-					Type = LikeObjectType.Post
-					, ItemId = 701
+				Type = LikeObjectType.Post, ItemId = 701
 			});
 
 			Assert.That(like, Is.EqualTo(5));
@@ -46,38 +31,23 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void Delete_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/likes.delete";
+			Url = "https://api.vk.com/method/likes.delete";
+			ReadCategoryJsonPath(nameof(Delete_NormalCase));
 
-			const string json =
-					@"{
-					response: {
-						likes: 4
-					}
-				}";
+			var like = Api.Likes.Delete(LikeObjectType.Post, 701);
 
-			var likesCategory = GetMockedLikesCategory(url, json);
-			var like = likesCategory.Delete(LikeObjectType.Post, 701);
 			Assert.That(like, Is.EqualTo(4));
 		}
 
 		[Test]
 		public void GetList_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/likes.getList";
+			Url = "https://api.vk.com/method/likes.getList";
+			ReadCategoryJsonPath(nameof(GetList_NormalCase));
 
-			const string json =
-					@"{
-					response: {
-						count: 5,
-						items: [32190123, 221634238, 229027572, 210894192, 201173701]
-					}
-				}";
-
-			var likesCategory = GetMockedLikesCategory(url, json);
-
-			var like = likesCategory.GetList(new LikesGetListParams
+			var like = Api.Likes.GetList(new LikesGetListParams
 			{
-					ItemId = 701
+				ItemId = 701
 			});
 
 			Assert.That(like.Count, Is.EqualTo(5));
@@ -86,46 +56,12 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void GetListEx_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/likes.getList";
+			Url = "https://api.vk.com/method/likes.getList";
+			ReadCategoryJsonPath(nameof(GetListEx_NormalCase));
 
-			const string json =
-					@"{
-					response: {
-						count: 5,
-						items: [{
-								type: 'profile',
-								id: 32190123,
-								first_name: 'Максим',
-								last_name: 'Инютин'
-							}, {
-								type: 'profile',
-								id: 221634238,
-								first_name: 'Александр',
-								last_name: 'Инютин'
-							}, {
-								type: 'profile',
-								id: 229027572,
-								first_name: 'Настя',
-								last_name: 'Зубова'
-							}, {
-								type: 'profile',
-								id: 210894192,
-								first_name: 'Елизавета',
-								last_name: 'Емельянова'
-							}, {
-								type: 'profile',
-								id: 201173701,
-								first_name: 'Даниил',
-								last_name: 'Богданов'
-						}]
-					}
-				}";
-
-			var likesCategory = GetMockedLikesCategory(url, json);
-
-			var like = likesCategory.GetListEx(new LikesGetListParams
+			var like = Api.Likes.GetListEx(new LikesGetListParams
 			{
-					ItemId = 701
+				ItemId = 701
 			});
 
 			Assert.That(like.Users.Count, Is.EqualTo(5));
@@ -138,19 +74,12 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void IsLiked_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/likes.isLiked";
-
-			const string json =
-					@"{
-					response: {
-						liked: 1,
-						copied: 0
-					}
-				}";
-
-			var likesCategory = GetMockedLikesCategory(url, json);
+			Url = "https://api.vk.com/method/likes.isLiked";
+			ReadCategoryJsonPath(nameof(IsLiked_NormalCase));
 			bool copied;
-			var like = likesCategory.IsLiked(out copied, LikeObjectType.Post, 701);
+
+			var like = Api.Likes.IsLiked(out copied, LikeObjectType.Post, 701);
+
 			Assert.That(like, Is.EqualTo(true));
 			Assert.That(copied, Is.EqualTo(false));
 		}
