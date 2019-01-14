db208b814cf890c09de0c5b3bf86f3066a608ad7
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-01-15 00:32:06 +0300

    #700 ExecuteCategoryTest

--- VkNet.Tests/Categories/ExecuteCategoryTest.cs
+++ VkNet.Tests/Categories/ExecuteCategoryTest.cs
@@ -1,24 +1,28 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using NUnit.Framework;
 using VkNet.Exception;
 using VkNet.Model;
+using VkNet.Tests.Infrastructure;
 using VkNet.Utils;
 
 namespace VkNet.Tests.Categories
 {
 	[ExcludeFromCodeCoverage]
-	public class ExecuteCategoryTest : BaseTest
+	public class ExecuteCategoryTest : CategoryBaseTest
 	{
+		protected override string Folder => "Execute";
+
 		[Test]
 		public void ExecuteTest()
 		{
-			Url =
-					@"https://api.vk.com/method/execute";
+			Url = "https://api.vk.com/method/execute";
 
-			Json = @"{response: ['Тишко', 'Бледнов', 'Касимова']}";
+			ReadCategoryJsonPath(nameof(ExecuteTest));
 
-			const string code =
-					@"return API.users.get({""user_ids"": API.audio.search({""q"":""Beatles"", ""count"":3}).items@.owner_id})@.last_name;";
+			var code = ReadScript(nameof(ExecuteTest));
 
 			var result = Api.Execute.Execute(code);
 			Assert.That(result.RawJson, Is.EqualTo(Json));
@@ -27,23 +31,10 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void ExecuteTopicsFeedTest()
 		{
-			Url =
-					@"https://api.vk.com/method/execute";
+			Url = "https://api.vk.com/method/execute";
 
-			Json = @"{
-	""response"": {
-		""count"": 1,
-		""items"": [{
-			""id"": 74,
-			""from_id"": -60774666,
-			""date"": 1506698266,
-			""text"": ""«Твоятерритория.онлайн»""
-		}]
-	}
-}";
-
-			const string code =
-					@"return API.users.get({""user_ids"": API.audio.search({""q"":""Beatles"", ""count"":3}).items@.owner_id})@.last_name;";
+			ReadCategoryJsonPath(nameof(ExecuteTopicsFeedTest));
+			var code = ReadScript(nameof(ExecuteTopicsFeedTest));
 
 			var result = Api.Execute.Execute<TopicsFeed>(code);
 			Assert.That(result, Is.Not.Null);
@@ -53,20 +44,9 @@ namespace VkNet.Tests.Categories
 		public void ExecuteGetUniversitiesTest()
 		{
 			Url = "https://api.vk.com/method/execute";
+			ReadCategoryJsonPath(nameof(ExecuteGetUniversitiesTest));
 
-			Json =
-					@"{
-                    'response': {
-                      count: 93,
-                      items: [{
-                        'id': 1,
-                        'title': 'СПбГУ'
-                      }]
-					}
-                  }";
-
-			const string code =
-					@"return API.database.getUniversities({""country_id"": 1, ""city_id"": 2, ""q"": ""СПб"", count: 1}); ";
+			var code = ReadScript(nameof(ExecuteGetUniversitiesTest));
 
 			var result = Api.Execute.Execute<VkCollection<University>>(code);
 			Assert.That(result, Is.Not.Null);
@@ -77,33 +57,31 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void ExecuteErrorTest()
 		{
-			Url =
-					@"https://api.vk.com/method/execute";
+			Url = "https://api.vk.com/method/execute";
+			ReadErrorsJsonFile(12);
 
-			Json = @"{
-				error: {
-					error_code: 12,
-					error_msg: ""Unable to compile code: ';' expected, '' found in line 1"",
-					request_params: [{
-						key: 'oauth',
-						value: '1'
-					}, {
-						key: 'method',
-						value: 'execute'
-					}, {
-						key: 'code',
-						value: 'return API.users.get({""user_ids"": API.audio.search({""q"":""Beatles"", ""count"":3}).items@.owner_id})@.last_name'
-					}, {
-						key: 'v',
-						value: '5.50'
-					}]
-				}
-			}";
-
-			const string code =
-					@"return API.users.get({""user_ids"": API.audio.search({""q"":""Beatles"", ""count"":3}).items@.owner_id})@.last_name";
+			var code = ReadScript(nameof(ExecuteErrorTest));
 
 			Assert.That(() => Api.Execute.Execute(code), Throws.InstanceOf<VkApiException>());
+		}
+
+		private string ReadScript(string scriptPath)
+		{
+			var folders = new List<string>
+			{
+				AppContext.BaseDirectory, "TestData"
+			};
+
+			folders.AddRange(new[] { "Categories", Folder, scriptPath });
+
+			var path = Path.Combine(folders.ToArray()) + ".vkscript";
+
+			if (!File.Exists(path))
+			{
+				throw new FileNotFoundException(path);
+			}
+
+			return File.ReadAllText(path);
 		}
 	}
 }
