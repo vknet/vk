f04f848db3858603ad35aef70cac9de70c336cb4
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-01-15 00:09:42 +0300

    #700 FaveCategoryTest

--- VkNet.Tests/Categories/FaveCategoryTest.cs
+++ VkNet.Tests/Categories/FaveCategoryTest.cs
@@ -2,50 +2,28 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using NUnit.Framework;
-using VkNet.Categories;
 using VkNet.Enums;
 using VkNet.Enums.SafetyEnums;
 using VkNet.Model.Attachments;
 using VkNet.Tests.Helper;
+using VkNet.Tests.Infrastructure;
 
 namespace VkNet.Tests.Categories
 {
 	[TestFixture]
 	[SuppressMessage("ReSharper", "PublicMembersMustHaveComments")]
 	[ExcludeFromCodeCoverage]
-	public class FaveCategoryTest : BaseTest
+	public class FaveCategoryTest : CategoryBaseTest
 	{
-		private FaveCategory GetMockedFaveCategory(string url, string json)
-		{
-			Json = json;
-			Url = url;
-
-			return new FaveCategory(Api);
-		}
+		protected override string Folder => "Fave";
 
 		[Test]
 		public void GetLinks_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/fave.getLinks";
+			Url = "https://api.vk.com/method/fave.getLinks";
+			ReadCategoryJsonPath(nameof(GetLinks_NormalCase));
 
-			const string json = @"
-			{
-				response: {
-					count: 4,
-					items: [{
-						id: '2_32190123_1',
-						url: 'https://vk.com/apiclub',
-						title: 'ВКонтакте API',
-						description: 'Сообщество',
-						photo_50: 'https://pp.vk.me/c400/g00001/e_5ba03323.jpg',
-						photo_100: 'https://pp.vk.me/c400/g00001/e_5ba03323.jpg'
-					}]
-				}
-			}";
-
-			var cat = GetMockedFaveCategory(url, json);
-
-			var links = cat.GetLinks(1, 1);
+			var links = Api.Fave.GetLinks(1, 1);
 
 			Assert.That(links.Count, Is.EqualTo(1));
 			var link = links.FirstOrDefault();
@@ -62,60 +40,10 @@ namespace VkNet.Tests.Categories
 		public void GetMarketItems_AllParams()
 		{
 			Url = "https://api.vk.com/method/fave.getMarketItems";
-
-			Json = @"{
-				response: {
-					count: 1,
-					items: [{
-						id: 94992,
-						owner_id: -103292418,
-						title: 'Название',
-						description: 'Описание',
-						price: {
-							amount: '12300',
-							currency: {
-								id: 643,
-								name: 'RUB'
-							},
-							text: '123руб.'
-						},
-						category: {
-							id: 300,
-							name: 'Компьютеры',
-							section: {
-								id: 3,
-								name: 'Компьютернаятехника'
-							}
-						},
-						date: 1452002460,
-						thumb_photo: 'https://pp.vk.me/c628717/v628717123/3852b/Kli3SztJwDE.jpg',
-						availability: 0,
-						albums_ids: [1],
-						photos: [{
-							id: 396740679,
-							album_id: -53,
-							owner_id: -103292418,
-							user_id: 32190123,
-							photo_75: 'https://pp.vk.me/c628717/v628717123/38520/w70n0UtkOm0.jpg',
-							photo_130: 'https://pp.vk.me/c628717/v628717123/38521/mi9r7yOxsUQ.jpg',
-							photo_604: 'https://pp.vk.me/c628717/v628717123/38522/o--iWrn5qgI.jpg',
-							width: 550,
-							height: 412,
-							text: '',
-							date: 1452002447
-						}],
-						can_comment: 1,
-						can_repost: 1,
-						likes: {
-							user_likes: 1,
-							count: 1
-						},
-						views_count: 10
-					}]
-				}
-			}";
+			ReadCategoryJsonPath(nameof(GetMarketItems_AllParams));
 
 			var marketItems = Api.Fave.GetMarketItems(1, 0, true);
+
 			Assert.NotNull(marketItems);
 			Assert.That(marketItems.TotalCount, Is.EqualTo(1));
 			CollectionAssert.IsNotEmpty(marketItems);
@@ -125,39 +53,10 @@ namespace VkNet.Tests.Categories
 		public void GetMarketItems_count()
 		{
 			Url = "https://api.vk.com/method/fave.getMarketItems";
-
-			Json = @"{
-				response: {
-					count: 1,
-					items: [{
-						id: 94992,
-						owner_id: -103292418,
-						title: 'Название',
-						description: 'Описание',
-						price: {
-							amount: '12300',
-							currency: {
-								id: 643,
-								name: 'RUB'
-							},
-							text: '123руб.'
-						},
-						category: {
-							id: 300,
-							name: 'Компьютеры',
-							section: {
-								id: 3,
-								name: 'Компьютернаятехника'
-							}
-						},
-						date: 1452002460,
-						thumb_photo: 'https://pp.vk.me/c628717/v628717123/3852b/Kli3SztJwDE.jpg',
-						availability: 0
-					}]
-				}
-			}";
+			ReadCategoryJsonPath(nameof(GetMarketItems_count));
 
 			var marketItems = Api.Fave.GetMarketItems(1);
+
 			Assert.NotNull(marketItems);
 			Assert.That(marketItems.TotalCount, Is.EqualTo(1));
 			CollectionAssert.IsNotEmpty(marketItems);
@@ -167,39 +66,10 @@ namespace VkNet.Tests.Categories
 		public void GetMarketItems_countAndOffset()
 		{
 			Url = "https://api.vk.com/method/fave.getMarketItems";
-
-			Json = @"{
-				response: {
-					count: 1,
-					items: [{
-						id: 94992,
-						owner_id: -103292418,
-						title: 'Название',
-						description: 'Описание',
-						price: {
-							amount: '12300',
-							currency: {
-								id: 643,
-								name: 'RUB'
-							},
-							text: '123руб.'
-						},
-						category: {
-							id: 300,
-							name: 'Компьютеры',
-							section: {
-								id: 3,
-								name: 'Компьютернаятехника'
-							}
-						},
-						date: 1452002460,
-						thumb_photo: 'https://pp.vk.me/c628717/v628717123/3852b/Kli3SztJwDE.jpg',
-						availability: 0
-					}]
-				}
-			}";
+			ReadCategoryJsonPath(nameof(GetMarketItems_countAndOffset));
 
 			var marketItems = Api.Fave.GetMarketItems(1, 0);
+
 			Assert.NotNull(marketItems);
 			Assert.That(marketItems.TotalCount, Is.EqualTo(1));
 			CollectionAssert.IsNotEmpty(marketItems);
@@ -209,39 +79,10 @@ namespace VkNet.Tests.Categories
 		public void GetMarketItems_WithoutParams()
 		{
 			Url = "https://api.vk.com/method/fave.getMarketItems";
-
-			Json = @"{
-				response: {
-					count: 1,
-					items: [{
-						id: 94992,
-						owner_id: -103292418,
-						title: 'Название',
-						description: 'Описание',
-						price: {
-							amount: '12300',
-							currency: {
-								id: 643,
-								name: 'RUB'
-							},
-							text: '123руб.'
-						},
-						category: {
-							id: 300,
-							name: 'Компьютеры',
-							section: {
-								id: 3,
-								name: 'Компьютернаятехника'
-							}
-						},
-						date: 1452002460,
-						thumb_photo: 'https://pp.vk.me/c628717/v628717123/3852b/Kli3SztJwDE.jpg',
-						availability: 0
-					}]
-				}
-			}";
+			ReadCategoryJsonPath(nameof(GetMarketItems_WithoutParams));
 
 			var marketItems = Api.Fave.GetMarketItems();
+
 			Assert.NotNull(marketItems);
 			Assert.That(marketItems.TotalCount, Is.EqualTo(1));
 			CollectionAssert.IsNotEmpty(marketItems);
@@ -250,64 +91,11 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void GetPhotos_ExtendedCase()
 		{
-			const string url = "https://api.vk.com/method/fave.getPhotos";
+			Url = "https://api.vk.com/method/fave.getPhotos";
+			ReadCategoryJsonPath(nameof(GetPhotos_ExtendedCase));
 
-			const string json =
-				@"{
-				response: {
-					count: 1061,
-					items: [{
-						id: 390044361,
-						album_id: -7,
-						owner_id: -66589869,
-						user_id: 100,
-						sizes: [{
-							url: 'http://cs629301.vk.me/v629301456/1caaf/XpHNgelMOc0.jpg',
-							width: 75,
-							height: 67,
-							type: 's'
-							}, {
-							url: 'http://cs629301.vab0/khhRkq0uhoo.jpg',
-							width: 130,
-							height: 116,
-							type: 'm'
-							}, {
-							url: 'http://cs629301.vab1/cNehIdjHNXg.jpg',
-							width: 604,
-							height: 537,
-							type: 'x'
-							}, {
-							url: 'http://cs629301.vab2/reFF7eJG23U.jpg',
-							width: 130,
-							height: 116,
-							type: 'o'
-							}, {
-							url: 'http://cs629301.vab3/6YYP7c34Vp4.jpg',
-							width: 200,
-							height: 178,
-							type: 'p'
-							}, {
-							url: 'http://cs629301.vab4/I3fCEQCPuec.jpg',
-							width: 320,
-							height: 285,
-							type: 'q'
-							}, {
-							url: 'http://cs629301.vab5/VtX_ZADIFXg.jpg',
-							width: 510,
-							height: 453,
-							type: 'r'
-						}],
-						text: '',
-						date: 1447419206,
-						post_id: 154560,
-						access_key: '1e2008462f1a012b95'
-					}]
-					}
-			}";
+			var photos = Api.Fave.GetPhotos(3, 1, true);
 
-			var cat = GetMockedFaveCategory(url, json);
-
-			var photos = cat.GetPhotos(3, 1, true);
 			Assert.That(photos, Is.Not.Null);
 			Assert.That(photos.Count, Is.EqualTo(1));
 			var photo = photos.FirstOrDefault();
@@ -332,47 +120,11 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void GetPhotos_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/fave.getPhotos";
+			Url = "https://api.vk.com/method/fave.getPhotos";
+			ReadCategoryJsonPath(nameof(GetPhotos_NormalCase));
 
-			const string json =
-				@"{
-					'response': {
-					  'count': 3,
-					  'items': [
-						{
-						  'id': 263113261,
-						  'album_id': 136592355,
-						  'owner_id': 1,
-						  'photo_75': 'http://cs9591.vk.me/u00001/136592355/s_47267f71.jpg',
-						  'photo_130': 'http://cs9591.vk.me/u00001/136592355/m_dc54094a.jpg',
-						  'photo_604': 'http://cs9591.vk.me/u00001/136592355/x_3216ccc1.jpg',
-						  'photo_807': 'http://cs9591.vk.me/u00001/136592355/y_e10ee835.jpg',
-						  'photo_1280': 'http://cs9591.vk.me/u00001/136592355/z_a8fd75ba.jpg',
-						  'photo_2560': 'http://cs9591.vk.me/u00001/136592355/w_62aef149.jpg',
-						  'text': '',
-						  'date': 1307628890
-						},
-						{
-						  'id': 319770573,
-						  'album_id': -7,
-						  'owner_id': -25397178,
-						  'user_id': 100,
-						  'photo_75': 'http://cs310923.vk.me/v310923070/c28b/VEtf7pX6MXM.jpg',
-						  'photo_130': 'http://cs310923.vk.me/v310923070/c28c/cjCqKn_EGxE.jpg',
-						  'photo_604': 'http://cs310923.vk.me/v310923070/c28d/IFtj16H-KwI.jpg',
-						  'width': 604,
-						  'height': 530,
-						  'text': '',
-						  'date': 1390533904,
-						  'post_id': 88997
-						}
-					  ]
-					}
-				  }";
+			var photos = Api.Fave.GetPhotos(3, 1);
 
-			var cat = GetMockedFaveCategory(url, json);
-
-			var photos = cat.GetPhotos(3, 1);
 			Assert.That(photos, Is.Not.Null);
 			Assert.That(photos.Count, Is.EqualTo(2));
 			var photo = photos.FirstOrDefault();
@@ -427,91 +179,10 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void GetPosts_Extended()
 		{
-			const string url = "https://api.vk.com/method/fave.getPosts";
+			Url = "https://api.vk.com/method/fave.getPosts";
+			ReadCategoryJsonPath(nameof(GetPosts_Extended));
 
-			const string json =
-				@"{
-					response: {
-						count: 2623,
-						items: [{
-							id: 1258365,
-							from_id: -30666517,
-							owner_id: -30666517,
-							date: 1447668333,
-							post_type: 'post',
-							text: 'Видео с наглядными инструкциями, как правильно отрефакторить плохо написанный код, сделав его намного более читаемым, чем было изначально.',
-							attachments: [{
-								type: 'video',
-								video: {
-									id: 171514588,
-									owner_id: 235845316,
-									title: 'Clean Code: Learn to write clean, maintainable and robust code',
-									duration: 2058,
-									description: 'Clean Code: Learn to write clean, maintainable and robust code.
-
-									In this video, I take a poorly-written piece of code and refactor in a step-by-step fashion. I also teach you some productivity tips along the way that helps you write code fast with Visual Studio.
-
-									If you enjoy my teaching style and want to learn more from me, check out my courses on Udemy:
-
-									C# Intermediate: Classes, Interfaces and Object-oriented Programming
-
-									https://www.udemy.com/csharp-intermediate-classes-interfaces-and-oop/?couponCode=',
-									date: 1447535648,
-									views: 1613,
-									comments: 0,
-									photo_130: 'https://pp.vk.me/c627830/u235845316/video/s_856d4cf3.jpg',
-									photo_320: 'https://pp.vk.me/c627830/u235845316/video/l_e2fc316e.jpg',
-									photo_800: 'https://pp.vk.me/c627830/u235845316/video/y_dca48fdd.jpg',
-									photo_640: 'https://pp.vk.me/c627830/u235845316/video/y_dca48fdd.jpg',
-									access_key: '733701ff4d7eb85ed7'
-								}
-							}],
-							post_source: {
-								type: 'vk'
-							},
-							comments: {
-								count: 9,
-								can_post: 1
-							},
-							likes: {
-								count: 413,
-								user_likes: 1,
-								can_like: 0,
-								can_publish: 1
-							},
-							reposts: {
-								count: 91,
-								user_reposted: 0
-							}
-						}],
-						profiles: [{
-							id: 235845316,
-							first_name: 'Лапанильда',
-							last_name: 'Кошкодавленко',
-							sex: 1,
-							screen_name: 'deadlymanul',
-							photo_50: 'https://pp.vk.me/c621918/v621918316/3e98c/-t0a2WEOZDU.jpg',
-							photo_100: 'https://pp.vk.me/c621918/v621918316/3e98b/tqlsDgLIgzE.jpg',
-							online: 1
-						}],
-						groups: [{
-							id: 30666517,
-							name: 'Типичный программист | tproger',
-							screen_name: 'tproger',
-							is_closed: 0,
-							type: 'page',
-							is_admin: 0,
-							is_member: 1,
-							photo_50: 'https://pp.vk.me/c625628/v625628973/43c4a/MUFXdlLGg-I.jpg',
-							photo_100: 'https://pp.vk.me/c625628/v625628973/43c49/qO1HJcRXnaQ.jpg',
-							photo_200: 'https://pp.vk.me/c625628/v625628973/43c48/0ioH05XEjCc.jpg'
-						}]
-					}
-				  }";
-
-			var cat = GetMockedFaveCategory(url, json);
-
-			var posts = cat.GetPosts(3, 1);
+			var posts = Api.Fave.GetPosts(3, 1);
 
 			Assert.That(posts.TotalCount, Is.EqualTo(2623u));
 
@@ -524,7 +195,8 @@ namespace VkNet.Tests.Categories
 			Assert.That(wallPost.PostType, Is.EqualTo(PostType.Post));
 
 			Assert.That(wallPost.Text,
-				Is.EqualTo(@"Видео с наглядными инструкциями, как правильно отрефакторить плохо написанный код, сделав его намного более читаемым, чем было изначально."));
+				Is.EqualTo(
+					@"Видео с наглядными инструкциями, как правильно отрефакторить плохо написанный код, сделав его намного более читаемым, чем было изначально."));
 
 			Assert.That(wallPost.PostSource.Type, Is.EqualTo(PostSourceType.Vk));
 			Assert.That(wallPost.Comments.CanPost, Is.EqualTo(true));
@@ -600,91 +272,10 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void GetPosts_NotExtended()
 		{
-			const string url = "https://api.vk.com/method/fave.getPosts";
+			Url = "https://api.vk.com/method/fave.getPosts";
+			ReadCategoryJsonPath(nameof(GetPosts_NotExtended));
 
-			const string json =
-				@"{
-					response: {
-						count: 2623,
-						items: [{
-							id: 1258365,
-							from_id: -30666517,
-							owner_id: -30666517,
-							date: 1447668333,
-							post_type: 'post',
-							text: 'Видео с наглядными инструкциями, как правильно отрефакторить плохо написанный код, сделав его намного более читаемым, чем было изначально.',
-							attachments: [{
-								type: 'video',
-								video: {
-									id: 171514588,
-									owner_id: 235845316,
-									title: 'Clean Code: Learn to write clean, maintainable and robust code',
-									duration: 2058,
-									description: 'Clean Code: Learn to write clean, maintainable and robust code.
-
-									In this video, I take a poorly-written piece of code and refactor in a step-by-step fashion. I also teach you some productivity tips along the way that helps you write code fast with Visual Studio.
-
-									If you enjoy my teaching style and want to learn more from me, check out my courses on Udemy:
-
-									C# Intermediate: Classes, Interfaces and Object-oriented Programming
-
-									https://www.udemy.com/csharp-intermediate-classes-interfaces-and-oop/?couponCode=',
-									date: 1447535648,
-									views: 1613,
-									comments: 0,
-									photo_130: 'https://pp.vk.me/c627830/u235845316/video/s_856d4cf3.jpg',
-									photo_320: 'https://pp.vk.me/c627830/u235845316/video/l_e2fc316e.jpg',
-									photo_800: 'https://pp.vk.me/c627830/u235845316/video/y_dca48fdd.jpg',
-									photo_640: 'https://pp.vk.me/c627830/u235845316/video/y_dca48fdd.jpg',
-									access_key: '733701ff4d7eb85ed7'
-								}
-							}],
-							post_source: {
-								type: 'vk'
-							},
-							comments: {
-								count: 9,
-								can_post: 1
-							},
-							likes: {
-								count: 413,
-								user_likes: 1,
-								can_like: 0,
-								can_publish: 1
-							},
-							reposts: {
-								count: 91,
-								user_reposted: 0
-							}
-						}],
-						profiles: [{
-							id: 235845316,
-							first_name: 'Лапанильда',
-							last_name: 'Кошкодавленко',
-							sex: 1,
-							screen_name: 'deadlymanul',
-							photo_50: 'https://pp.vk.me/c621918/v621918316/3e98c/-t0a2WEOZDU.jpg',
-							photo_100: 'https://pp.vk.me/c621918/v621918316/3e98b/tqlsDgLIgzE.jpg',
-							online: 1
-						}],
-						groups: [{
-							id: 30666517,
-							name: 'Типичный программист | tproger',
-							screen_name: 'tproger',
-							is_closed: 0,
-							type: 'page',
-							is_admin: 0,
-							is_member: 1,
-							photo_50: 'https://pp.vk.me/c625628/v625628973/43c4a/MUFXdlLGg-I.jpg',
-							photo_100: 'https://pp.vk.me/c625628/v625628973/43c49/qO1HJcRXnaQ.jpg',
-							photo_200: 'https://pp.vk.me/c625628/v625628973/43c48/0ioH05XEjCc.jpg'
-						}]
-					}
-				  }";
-
-			var cat = GetMockedFaveCategory(url, json);
-
-			var posts = cat.GetPosts(3, 1);
+			var posts = Api.Fave.GetPosts(3, 1);
 
 			Assert.That(posts.TotalCount, Is.EqualTo(2623u));
 
@@ -697,7 +288,8 @@ namespace VkNet.Tests.Categories
 			Assert.That(wallPost.PostType, Is.EqualTo(PostType.Post));
 
 			Assert.That(wallPost.Text,
-				Is.EqualTo(@"Видео с наглядными инструкциями, как правильно отрефакторить плохо написанный код, сделав его намного более читаемым, чем было изначально."));
+				Is.EqualTo(
+					@"Видео с наглядными инструкциями, как правильно отрефакторить плохо написанный код, сделав его намного более читаемым, чем было изначально."));
 
 			Assert.That(wallPost.PostSource.Type, Is.EqualTo(PostSourceType.Vk));
 			Assert.That(wallPost.Comments.CanPost, Is.EqualTo(true));
@@ -738,25 +330,11 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void GetUsers_OneItem()
 		{
-			const string url = "https://api.vk.com/method/fave.getUsers";
+			Url = "https://api.vk.com/method/fave.getUsers";
+			ReadCategoryJsonPath(nameof(GetUsers_OneItem));
 
-			const string json =
-				@"{
-					'response': {
-					  'count': 2,
-					  'items': [
-						{
-						  'id': 1,
-						  'first_name': 'Павел',
-						  'last_name': 'Дуров'
-						}
-					  ]
-					}
-				  }";
+			var users = Api.Fave.GetUsers(3, 1);
 
-			var cat = GetMockedFaveCategory(url, json);
-
-			var users = cat.GetUsers(3, 1);
 			Assert.That(users, Is.Not.Null);
 			Assert.That(users.Count, Is.EqualTo(1));
 			var user = users.FirstOrDefault();
@@ -769,32 +347,11 @@ namespace VkNet.Tests.Categories
 		[Test]
 		public void GetVideos_NormalCase()
 		{
-			const string url = "https://api.vk.com/method/fave.getVideos";
+			Url = "https://api.vk.com/method/fave.getVideos";
+			ReadCategoryJsonPath(nameof(GetVideos_NormalCase));
 
-			const string json =
-				@"{
-					'response': {
-					  'count': 2,
-					  'items': [
-						{
-						  'id': 164841344,
-						  'owner_id': 1,
-						  'title': 'This is SPARTA',
-						  'duration': 16,
-						  'description': '',
-						  'date': 1366495075,
-						  'views': 215502,
-						  'comments': 2559,
-						  'photo_130': 'http://cs12761.vk.me/u5705167/video/s_df53315c.jpg',
-						  'photo_320': 'http://cs12761.vk.me/u5705167/video/l_00c6be47.jpg'
-						}
-					  ]
-					}
-				  }";
+			var videos = Api.Fave.GetVideos(3, 1);
 
-			var cat = GetMockedFaveCategory(url, json);
-
-			var videos = cat.GetVideos(3, 1);
 			Assert.That(videos.Count, Is.EqualTo(2));
 			var video = videos.Videos.FirstOrDefault();
 			Assert.That(video.Id, Is.EqualTo(164841344));
