abfa1fff7cbfce7862e3bd8b38ade382479bacc4
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-03-05 23:57:28 +0300

    Выделил абстрактный класс для отправки формы

---
+++ VkNet/Infrastructure/Authorization/ImplicitFlow/AbstractAuthorizationForm.cs
@@ -0,0 +1,67 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+using VkNet.Enums;
+using VkNet.Exception;
+using VkNet.Utils;
+
+namespace VkNet.Infrastructure.Authorization.ImplicitFlow
+{
+	/// <inheritdoc />
+	public abstract class AbstractAuthorizationForm : IAuthorizationForm
+	{
+		private readonly IAuthorizationFormHtmlParser _htmlParser;
+
+		private readonly DefaultHttpClientFactory _httpClientFactory;
+
+		/// <inheritdoc />
+		protected AbstractAuthorizationForm(IAuthorizationFormHtmlParser htmlParser, DefaultHttpClientFactory httpClientFactory)
+		{
+			_htmlParser = htmlParser;
+			_httpClientFactory = httpClientFactory;
+		}
+
+		/// <inheritdoc />
+		public abstract ImplicitFlowPageType GetPageType();
+
+		/// <inheritdoc />
+		public async Task<AuthorizationFormResult> ExecuteAsync(Url url)
+		{
+			var form = _htmlParser.GetForm(url);
+
+			FillFormFields(form);
+
+			using (var cli = new FlurlClient(form.Action).EnableCookies())
+			{
+				if (_httpClientFactory != null)
+				{
+					cli.Configure(s => s.HttpClientFactory = _httpClientFactory);
+				}
+
+				var responseMessage = await cli.Request()
+					.PostMultipartAsync(mp => mp.Add(new FormUrlEncodedContent(form.Fields)))
+					.ConfigureAwait(false);
+
+				if (!responseMessage.IsSuccessStatusCode)
+				{
+					throw new VkAuthorizationException(responseMessage.ReasonPhrase);
+				}
+
+				return new AuthorizationFormResult
+				{
+					RequestUrl = url,
+					ResponseUrl = responseMessage.Headers.Location,
+					Cookies = new Cookies()
+				};
+			}
+		}
+
+		/// <summary>
+		/// Заполнение полей формы
+		/// </summary>
+		/// <param name="form">Форма</param>
+		protected abstract void FillFormFields(VkHtmlFormResult form);
+	}
+}
