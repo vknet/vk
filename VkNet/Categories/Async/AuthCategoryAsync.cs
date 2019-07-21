141851dfb35e221753ab692b0c7aee896496334e
itsokto <picnet9@gmail.com>
2019-07-21 21:55:22 +0300

    Добавить CancellationToken в AuthCategory

--- VkNet/Categories/Async/AuthCategoryAsync.cs
+++ VkNet/Categories/Async/AuthCategoryAsync.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
@@ -8,85 +9,46 @@ namespace VkNet.Categories
 	/// <inheritdoc />
 	public partial class AuthCategory
 	{
-		/// <summary>
-		/// Проверяет правильность введённого номера.
-		/// </summary>
-		/// <param name="phone">
-		/// Номер телефона регистрируемого пользователя. строка, обязательный параметр
-		/// (Строка, обязательный
-		/// параметр).
-		/// </param>
-		/// <param name="clientId">
-		/// Идентификатор Вашего приложения. целое число (Целое
-		/// число).
-		/// </param>
-		/// <param name="clientSecret">
-		/// Секретный ключ приложения, доступный в разделе редактирования приложения.
-		/// строка,
-		/// обязательный параметр (Строка, обязательный параметр).
-		/// </param>
-		/// <param name="authByPhone">
-		/// Флаг, может принимать значения 1 или 0 (Флаг, может
-		/// принимать значения 1 или 0).
-		/// </param>
-		/// <returns>
-		/// В случае, если номер пользователя является правильным, будет возвращён
-		/// <c> true </c>.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте http://vk.com/dev/auth.checkPhone
-		/// </remarks>
-		public Task<bool> CheckPhoneAsync(string phone, string clientSecret, long? clientId = null, bool? authByPhone = null)
+		/// <inheritdoc />
+		public async Task<bool> CheckPhoneAsync(string phone, string clientSecret, long? clientId = null, bool? authByPhone = null,
+												CancellationToken cancellationToken = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>
-					CheckPhone(phone: phone, clientSecret: clientSecret, clientId: clientId, authByPhone: authByPhone));
+			var parameters = new VkParameters
+			{
+				{ "phone", phone },
+				{ "client_id", clientId },
+				{ "client_secret", clientSecret },
+				{ "auth_by_phone", authByPhone }
+			};
+
+			return await _vk.CallAsync("auth.checkPhone", parameters, cancellationToken: cancellationToken).ConfigureAwait(false);
 		}
 
-		/// <summary>
-		/// Регистрирует нового пользователя по номеру телефона.
-		/// </summary>
-		/// <param name="params"> Параметры запроса. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/auth.signup
-		/// </remarks>
-		public Task<string> SignupAsync(AuthSignupParams @params)
+		/// <inheritdoc />
+		public async Task<string> SignupAsync(AuthSignupParams @params, CancellationToken cancellationToken = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>Signup(@params: @params));
+			return await _vk.CallAsync("auth.signup", @params, cancellationToken: cancellationToken).ConfigureAwait(false);
 		}
 
-		/// <summary>
-		/// Завершает регистрацию нового пользователя, начатую методом auth.signup, по
-		/// коду, полученному через SMS.
-		/// </summary>
-		/// <param name="params"> Параметры запроса. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/auth.confirm
-		/// </remarks>
-		public Task<AuthConfirmResult> ConfirmAsync(AuthConfirmParams @params)
+		/// <inheritdoc />
+		public async Task<AuthConfirmResult> ConfirmAsync(AuthConfirmParams @params, CancellationToken cancellationToken = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>Confirm(@params: @params));
+			return await _vk.CallAsync("auth.confirm", @params, cancellationToken: cancellationToken).ConfigureAwait(false);
 		}
 
-		/// <summary>
-		/// Позволяет восстановить доступ к аккаунту, используя код, полученный через SMS.
-		/// </summary>
-		/// <param name="phone"> Номер телефона пользователя. </param>
-		/// <param name="lastName"> Фамилия пользователя. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/auth.restore
-		/// </remarks>
-		public Task<string> RestoreAsync(string phone, string lastName)
+		/// <inheritdoc />
+		public async Task<string> RestoreAsync(string phone, string lastName, CancellationToken cancellationToken = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>Restore(phone: phone, lastName: lastName));
+			var response = await _vk.CallAsync("auth.restore",
+					new VkParameters
+					{
+						{ "phone", phone },
+						{ "last_name", lastName }
+					},
+					cancellationToken: cancellationToken)
+				.ConfigureAwait(false);
+
+			return response[key: "sid"];
 		}
 	}
 }
--- VkNet/Categories/AuthCategory.cs
+++ VkNet/Categories/AuthCategory.cs
@@ -1,13 +1,11 @@
-﻿using VkNet.Abstractions;
+﻿using System.Threading;
+using VkNet.Abstractions;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
-using VkNet.Utils;
 
 namespace VkNet.Categories
 {
-	/// <summary>
-	/// Методы для работы с авторизацией.
-	/// </summary>
+	/// <inheritdoc />
 	public partial class AuthCategory : IAuthCategory
 	{
 		/// <summary>
@@ -24,99 +22,28 @@ namespace VkNet.Categories
 			_vk = vk;
 		}
 
-		/// <summary>
-		/// Проверяет правильность введённого номера.
-		/// </summary>
-		/// <param name="phone">
-		/// Номер телефона регистрируемого пользователя. строка, обязательный параметр
-		/// (Строка, обязательный
-		/// параметр).
-		/// </param>
-		/// <param name="clientId">
-		/// Идентификатор Вашего приложения. целое число (Целое
-		/// число).
-		/// </param>
-		/// <param name="clientSecret">
-		/// Секретный ключ приложения, доступный в разделе редактирования приложения.
-		/// строка,
-		/// обязательный параметр (Строка, обязательный параметр).
-		/// </param>
-		/// <param name="authByPhone">
-		/// Флаг, может принимать значения 1 или 0 (Флаг, может
-		/// принимать значения 1 или 0).
-		/// </param>
-		/// <returns>
-		/// В случае, если номер пользователя является правильным, будет возвращён
-		/// <c> true </c>.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте http://vk.com/dev/auth.checkPhone
-		/// </remarks>
+		/// <inheritdoc />
 		public bool CheckPhone(string phone, string clientSecret, long? clientId = null, bool? authByPhone = null)
 		{
-			var parameters = new VkParameters
-			{
-					{ "phone", phone }
-					, { "client_id", clientId }
-					, { "client_secret", clientSecret }
-					, { "auth_by_phone", authByPhone }
-			};
-
-			return _vk.Call(methodName: "auth.checkPhone", parameters: parameters);
+			return CheckPhoneAsync(phone, clientSecret, clientId, authByPhone, CancellationToken.None).GetAwaiter().GetResult();
 		}
 
-		/// <summary>
-		/// Регистрирует нового пользователя по номеру телефона.
-		/// </summary>
-		/// <param name="params"> Параметры запроса. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/auth.signup
-		/// </remarks>
+		/// <inheritdoc />
 		public string Signup(AuthSignupParams @params)
 		{
-			return _vk.Call(methodName: "auth.signup", parameters: @params);
+			return SignupAsync(@params, CancellationToken.None).GetAwaiter().GetResult();
 		}
 
-		/// <summary>
-		/// Завершает регистрацию нового пользователя, начатую методом auth.signup, по
-		/// коду, полученному через SMS.
-		/// </summary>
-		/// <param name="params"> Параметры запроса. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/auth.confirm
-		/// </remarks>
+		/// <inheritdoc />
 		public AuthConfirmResult Confirm(AuthConfirmParams @params)
 		{
-			return _vk.Call(methodName: "auth.confirm", parameters: @params);
+			return ConfirmAsync(@params, CancellationToken.None).GetAwaiter().GetResult();
 		}
 
-		/// <summary>
-		/// Позволяет восстановить доступ к аккаунту, используя код, полученный через SMS.
-		/// </summary>
-		/// <param name="phone"> Номер телефона пользователя. </param>
-		/// <param name="lastName"> Фамилия пользователя. </param>
-		/// <returns>
-		/// Возвращает результат выполнения метода.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте https://vk.com/dev/auth.restore
-		/// </remarks>
+		/// <inheritdoc />
 		public string Restore(string phone, string lastName)
 		{
-			var response = _vk.Call(methodName: "auth.restore"
-					, parameters: new VkParameters
-					{
-							{ "phone", phone }
-							, { "last_name", lastName }
-					});
-
-			return response[key: "sid"];
+			return RestoreAsync(phone, lastName, CancellationToken.None).GetAwaiter().GetResult();
 		}
 	}
 }
