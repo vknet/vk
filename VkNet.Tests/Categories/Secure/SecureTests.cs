b36d1ee9237643ae2792eba0a6ef18e7e979062d
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-08-04 15:14:29 +0300

    #527 Добавить методы secure GiveEventSticker

--- VkNet.Tests/Categories/Secure/SecureTests.cs
+++ VkNet.Tests/Categories/Secure/SecureTests.cs
@@ -10,53 +10,6 @@ namespace VkNet.Tests.Categories.Secure
 		protected override string Folder => "Secure";
 
 		[Test]
-		public void SendSmsNotification()
-		{
-			Url = "https://api.vk.com/method/secure.sendSMSNotification";
-			ReadJsonFile(JsonPaths.True);
-
-			var app = Api.Secure.SendSmsNotification(123, "SMS Message");
-
-			Assert.That(app, Is.True);
-		}
-
-		[Test]
-		public void SendNotification()
-		{
-			Url = "https://api.vk.com/method/secure.sendNotification";
-			ReadCategoryJsonPath(nameof(SendNotification));
-
-			var app = Api.Secure.SendNotification("Notification", new ulong[] { 123 });
-
-			Assert.IsNotEmpty(app);
-		}
-
-		[Test]
-		public void SetCounter()
-		{
-			Url = "https://api.vk.com/method/secure.setCounter";
-			ReadJsonFile(JsonPaths.True);
-
-			var app = Api.Secure.SetCounter(new[] { "66748:6:1", "6492:2" }, 123, 2, true);
-
-			Assert.That(app, Is.True);
-		}
-
-		[Test]
-		public void GetSmsHistory()
-		{
-			Url = "https://api.vk.com/method/secure.getSMSHistory";
-			ReadCategoryJsonPath(nameof(GetSmsHistory));
-
-			var app = Api.Secure.GetSmsHistory(123);
-
-			Assert.IsNotEmpty(app);
-			var item = app.FirstOrDefault();
-			Assert.NotNull(item);
-			Assert.AreEqual(1238497, item.Id);
-		}
-
-		[Test]
 		public void CheckToken()
 		{
 			Url = "https://api.vk.com/method/secure.checkToken";
@@ -79,6 +32,20 @@ namespace VkNet.Tests.Categories.Secure
 		}
 
 		[Test]
+		public void GetSmsHistory()
+		{
+			Url = "https://api.vk.com/method/secure.getSMSHistory";
+			ReadCategoryJsonPath(nameof(GetSmsHistory));
+
+			var app = Api.Secure.GetSmsHistory(123);
+
+			Assert.IsNotEmpty(app);
+			var item = app.FirstOrDefault();
+			Assert.NotNull(item);
+			Assert.AreEqual(1238497, item.Id);
+		}
+
+		[Test]
 		public void GetTransactionsHistory()
 		{
 			Url = "https://api.vk.com/method/secure.getTransactionsHistory";
@@ -95,9 +62,73 @@ namespace VkNet.Tests.Categories.Secure
 			Url = "https://api.vk.com/method/secure.getUserLevel";
 			ReadCategoryJsonPath(nameof(GetUserLevel));
 
-			var app = Api.Secure.GetUserLevel(new long[]{123});
+			var app = Api.Secure.GetUserLevel(new long[]
+			{
+				123
+			});
 
 			Assert.IsNotEmpty(app);
+		}
+
+		[Test]
+		public void GiveEventSticker()
+		{
+			Url = "https://api.vk.com/method/secure.giveEventSticker";
+			ReadCategoryJsonPath(nameof(GiveEventSticker));
+
+			var app = Api.Secure.GiveEventSticker(new ulong[]
+				{
+					1,
+					2,
+					3
+				},
+				1);
+
+			Assert.IsNotEmpty(app);
+		}
+
+		[Test]
+		public void SendNotification()
+		{
+			Url = "https://api.vk.com/method/secure.sendNotification";
+			ReadCategoryJsonPath(nameof(SendNotification));
+
+			var app = Api.Secure.SendNotification("Notification",
+				new ulong[]
+				{
+					123
+				});
+
+			Assert.IsNotEmpty(app);
+		}
+
+		[Test]
+		public void SendSmsNotification()
+		{
+			Url = "https://api.vk.com/method/secure.sendSMSNotification";
+			ReadJsonFile(JsonPaths.True);
+
+			var app = Api.Secure.SendSmsNotification(123, "SMS Message");
+
+			Assert.That(app, Is.True);
+		}
+
+		[Test]
+		public void SetCounter()
+		{
+			Url = "https://api.vk.com/method/secure.setCounter";
+			ReadJsonFile(JsonPaths.True);
+
+			var app = Api.Secure.SetCounter(new[]
+				{
+					"66748:6:1",
+					"6492:2"
+				},
+				123,
+				2,
+				true);
+
+			Assert.That(app, Is.True);
 		}
 	}
 }
--- VkNet/Abstractions/Category/ISecureCategory.cs
+++ VkNet/Abstractions/Category/ISecureCategory.cs
@@ -9,64 +9,35 @@ namespace VkNet.Abstractions
 	/// <inheritdoc />
 	public interface ISecureCategory : ISecureCategoryAsync
 	{
-		/// <summary>
-		/// Добавляет информацию о достижениях пользователя в приложении.
-		/// </summary>
-		/// <param name = "userId">
-		/// Идентификатор пользователя, для которого нужно записать данные. положительное число, по умолчанию идентификатор текущего пользователя, обязательный параметр
-		/// </param>
-		/// <param name = "activityId">
-		/// Идентификатор достижения. Доступные значения:
-		/// 1 — достигнут новый уровень, работает аналогично secure.setUserLevel;
-		/// 2 — заработано новое число очков;
-		/// положительное число, отличное от 1 и 2 — выполнена миссия с идентификатором activity_id.
-		/// положительное число, обязательный параметр
-		/// </param>
-		/// <param name = "value">
-		/// Номер уровня или заработанное количество очков (соответственно, для activity_id=1 и activity_id=2).
-		/// Параметр игнорируется при значении activity_id, отличном от 1 и 2. положительное число, максимальное значение 10000
-		/// </param>
-		/// <returns>
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте http://vk.com/dev/secure.addAppEvent
-		/// </remarks>
+		/// <inheritdoc cref="ISecureCategoryAsync.AddAppEventAsync" />
 		object AddAppEvent(ulong userId, ulong activityId, ulong? value = null);
 
-		/// <inheritdoc cref="ISecureCategoryAsync.CheckTokenAsync"/>
+		/// <inheritdoc cref="ISecureCategoryAsync.CheckTokenAsync" />
 		CheckTokenResult CheckToken(string token, string ip = null);
 
-		/// <inheritdoc cref="ISecureCategoryAsync.GetAppBalanceAsync"/>
+		/// <inheritdoc cref="ISecureCategoryAsync.GetAppBalanceAsync" />
 		ulong GetAppBalance();
 
-		/// <inheritdoc cref="ISecureCategoryAsync.GetSmsHistoryAsync"/>
+		/// <inheritdoc cref="ISecureCategoryAsync.GetSmsHistoryAsync" />
 		ReadOnlyCollection<SmsHistoryItem> GetSmsHistory(ulong? userId = null, DateTime? dateFrom = null, DateTime? dateTo = null,
 														ulong? limit = null);
 
-		/// <inheritdoc cref="ISecureCategoryAsync.GetTransactionsHistoryAsync"/>
+		/// <inheritdoc cref="ISecureCategoryAsync.GetTransactionsHistoryAsync" />
 		ReadOnlyCollection<Transaction> GetTransactionsHistory();
 
-		/// <summary>
-		/// Возвращает ранее выставленный игровой уровень одного или нескольких пользователей в приложении.
-		/// </summary>
-		/// <param name = "userIds">
-		/// Идентификаторы пользователей, информацию об уровнях которых требуется получить. список целых чисел, разделенных запятыми, обязательный параметр
-		/// </param>
-		/// <returns>
-		/// Возвращает значения игровых уровней пользователей в приложении.
-		/// </returns>
-		/// <remarks>
-		/// Страница документации ВКонтакте http://vk.com/dev/secure.getUserLevel
-		/// </remarks>
+		/// <inheritdoc cref="ISecureCategoryAsync.GetUserLevelAsync" />
 		ReadOnlyCollection<SecureLevel> GetUserLevel(IEnumerable<long> userIds);
 
-		/// <inheritdoc cref="ISecureCategoryAsync.SendNotificationAsync"/>
+		/// <inheritdoc cref="ISecureCategoryAsync.GiveEventStickerAsync" />
+		ReadOnlyCollection<EventSticker> GiveEventSticker(IEnumerable<ulong> userIds, ulong achievementId);
+
+		/// <inheritdoc cref="ISecureCategoryAsync.SendNotificationAsync" />
 		ReadOnlyCollection<ulong> SendNotification(string message, IEnumerable<ulong> userIds = null);
 
-		/// <inheritdoc cref="ISecureCategoryAsync.SendSmsNotificationAsync"/>
+		/// <inheritdoc cref="ISecureCategoryAsync.SendSmsNotificationAsync" />
 		bool SendSmsNotification(ulong userId, string message);
 
-		/// <inheritdoc cref="ISecureCategoryAsync.SetCounterAsync"/>
+		/// <inheritdoc cref="ISecureCategoryAsync.SetCounterAsync" />
 		bool SetCounter(IEnumerable<string> counters, ulong? userId = null, long? counter = null, bool? increment = null);
 	}
 }
