b8069f4c47553bf427ede5ac1d95868b985a486c
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2019-08-15 05:57:47 +0300

    Обновлен и исправлен message.Send
    Результат изменился - поправлено и сохранена совместимость

--- VkNet/Model/MessagesSendResult.cs
+++ VkNet/Model/MessagesSendResult.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using VkNet.Utils;
 
 namespace VkNet.Model
@@ -11,12 +11,17 @@ namespace VkNet.Model
 		/// <summary>
 		/// Идентификатор назначения;
 		/// </summary>
-		public int? PeerId { get; set; }
+		public long? PeerId { get; set; }
 
 		/// <summary>
 		/// Идентификатор сообщения
 		/// </summary>
-		public int? MessageId { get; set; }
+		public long? MessageId { get; set; }
+
+		/// <summary>
+		/// Код ошибки если есть (в наличии в версии 5.101)
+		/// </summary>
+		public int ErrorCode { get; set; }
 
 		/// <summary>
 		/// Cообщение об ошибке, если сообщение не было доставлено получателю.
@@ -30,12 +35,37 @@ namespace VkNet.Model
 		/// <returns> </returns>
 		public static MessagesSendResult FromJson(VkResponse response)
 		{
-			return new MessagesSendResult
+			// В новых версиях изменился формат ответа, поддержка для других версий
+			var res = new MessagesSendResult();
+
+			if (response.ContainsKey("peer_id"))
+				res.PeerId = response[key: "peer_id"];
+			if (response.ContainsKey("message_id"))
+				res.MessageId = response[key: "message_id"];
+
+			if (response.ContainsKey("error"))
 			{
-					PeerId = response[key: "peer_id"]
-					, MessageId = response[key: "message_id"]
-					, Error = response[key: "error"]
-			};
+				var error = response[key: "error"];
+				if (error.ContainsKey("code"))
+				{
+					res.ErrorCode = error[key: "code"];
+					res.Error = error[key: "description"];
+				} else
+				{
+					try
+					{
+						res.Error = response[key: "error"];
+					}
+#pragma warning disable S2486 // Generic exceptions should not be ignored
+					catch
+					{
+						// TODO: ensure no error or handle
+					}
+#pragma warning restore S2486 // Generic exceptions should not be ignored
+				}
+			}
+
+			return res;
 		}
 	}
 }
