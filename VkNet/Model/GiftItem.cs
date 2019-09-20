46b550e3663e41eec156207ec4e2355ee8748305
itsokto <picnet9@gmail.com>
2019-09-20 11:44:41 +0300

    GiftItem: добавить атрибут JsonProperty над свойствами

--- VkNet/Model/GiftItem.cs
+++ VkNet/Model/GiftItem.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using VkNet.Enums;
@@ -23,32 +23,38 @@ namespace VkNet.Model
 		/// Идентификатор пользователя, который отправил подарок, или 0, если отправитель
 		/// скрыт.
 		/// </summary>
+		[JsonProperty("from_id")]
 		public long FromId { get; set; }
 
 		/// <summary>
 		/// Текст сообщения, приложенного к подарку.
 		/// </summary>
+		[JsonProperty("message")]
 		public string Message { get; set; }
 
 		/// <summary>
 		/// Время отправки подарка в формате unixtime.
 		/// </summary>
-		[JsonConverter(converterType: typeof(UnixDateTimeConverter))]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		[JsonProperty("date")]
 		public DateTime? Date { get; set; }
 
 		/// <summary>
 		/// Подарок.
 		/// </summary>
+		[JsonProperty("gift")]
 		public Gift Gift { get; set; }
 
 		/// <summary>
 		/// Значение приватности подарка (только для текущего пользователя).
 		/// </summary>
+		[JsonProperty("privacy")]
 		public GiftPrivacy Privacy { get; set; }
 
 		/// <summary>
 		/// Хеш подарка
 		/// </summary>
+		[JsonProperty("gift_hash")]
 		public string GiftHash { get; set; }
 
 		/// <summary>
@@ -60,13 +66,13 @@ namespace VkNet.Model
 		{
 			return new GiftItem
 			{
-					Id = response[key: "id"]
-					, FromId = response[key: "from_id"]
-					, Message = response[key: "message"]
-					, Date = response[key: "date"]
-					, Gift = response[key: "gift"]
-					, Privacy = response[key: "privacy"]
-					, GiftHash = response[key: "gift_hash"]
+				Id = response["id"],
+				FromId = response["from_id"],
+				Message = response["message"],
+				Date = response["date"],
+				Gift = response["gift"],
+				Privacy = response["privacy"],
+				GiftHash = response["gift_hash"]
 			};
 		}
 	}
