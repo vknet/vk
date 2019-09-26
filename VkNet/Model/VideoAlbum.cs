a8a3e9f5785a36aa1342c7b211f206fafabcae8d
itsokto <picnet9@gmail.com>
2019-09-26 20:28:28 +0300

    VideoAlbum: пометить свойства атрибутом JsonProperty

--- VkNet/Model/VideoAlbum.cs
+++ VkNet/Model/VideoAlbum.cs
@@ -18,33 +18,39 @@ namespace VkNet.Model
 		/// <summary>
 		/// Идентификатор альбома.
 		/// </summary>
+		[JsonProperty("id")]
 		public long? Id { get; set; }
 
 		/// <summary>
 		/// Идентификатор владельца альбома.
 		/// </summary>
+		[JsonProperty("owner_id")]
 		public long? OwnerId { get; set; }
 
 		/// <summary>
 		/// Название альбома.
 		/// </summary>
+		[JsonProperty("title")]
 		public string Title { get; set; }
 
 		/// <summary>
 		/// Количество видеозаписей в альбоме.
 		/// </summary>
+		[JsonProperty("count")]
 		public long? Count { get; set; }
 
 		/// <summary>
 		/// URL изображения предпросмотра альбома шириной в 160 пикселов.
 		/// </summary>
 		[Obsolete("Это свойство устарело в версии api 5.101. Используйте свойство IEnumerable<VideoImage> Image")]
+		[JsonProperty("photo_160")]
 		public string Photo160 { get; set; }
 
 		/// <summary>
 		/// URL изображения предпросмотра альбома шириной в 320 пикселов.
 		/// </summary>
 		[Obsolete("Это свойство устарело в версии api 5.101. Используйте свойство IEnumerable<VideoImage> Image")]
+		[JsonProperty("photo_320")]
 		public string Photo320 { get; set; }
 
 		/// <summary>
@@ -56,8 +62,8 @@ namespace VkNet.Model
 		/// <summary>
 		/// время последнего обновления в формате unixtime
 		/// </summary>
-		[JsonProperty(propertyName: "updated_time")]
-		[JsonConverter(converterType: typeof(UnixDateTimeConverter))]
+		[JsonProperty("updated_time")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
 		public DateTime? UpdatedTime { get; set; }
 
 		/// <summary>
