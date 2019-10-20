0f1ec8d4bb0d7788145b86c7bdddac7fc4f01b43
CreateLab <33497292+CreateLab@users.noreply.github.com>
2019-10-21 00:06:22 +0300

    Update Extensions.cs

--- VkNet/Extensions.cs
+++ VkNet/Extensions.cs
@@ -10,24 +10,6 @@ namespace VkNet
 	public static class Extensions
 	{
 		/// <summary>
-		///  Return registration date
-		/// </summary>
-		/// <param name="user"></param>
-		/// <param name="id">id of user</param>
-		/// <returns></returns>
-		public static async Task<DateTime> GetRegistrationDate(this User user, long id)
-		{
-			var str = await $"https://vk.com/foaf.php?id={id}".GetStringAsync();
-			var doc = new HtmlDocument();
-			doc.LoadHtml(str);
-			var created = doc.DocumentNode.Descendants("ya:created").FirstOrDefault();
-
-			var dataStr = created?.Attributes["dc:date"]?.Value ?? throw new InvalidOperationException("can't parse meta files");
-
-			return Convert.ToDateTime(dataStr);
-		}
-
-		/// <summary>
 		/// return self registration date
 		/// </summary>
 		/// <param name="user"></param>
