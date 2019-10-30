ccd828d9efb4741e844b4fa12003a1c48f2b8c32
Kirill Kuznetsov <16289034+equuskk@users.noreply.github.com>
2019-10-30 12:43:29 +0300

    Добавить интенты bot_ad_invite и bot_ad_promo (#892)

--- VkNet/Enums/SafetyEnums/Intent.cs
+++ VkNet/Enums/SafetyEnums/Intent.cs
@@ -9,5 +9,15 @@ namespace VkNet.Enums.SafetyEnums
 		/// Интент, который должен сопровождать рекламную рассылку для ботов.
 		/// </summary>
 		public static readonly Intent PromoNewsletter = RegisterPossibleValue(value: "promo_newsletter");
+
+		/// <summary>
+		/// Интент, который должен сопровождать сообщения, запрашивающее подтверждение пользователя на отправку этому пользователю рекламы.
+		/// </summary>
+		public static readonly Intent BotAdInvite = RegisterPossibleValue(value: "bot_ad_invite");
+
+		/// <summary>
+		/// Интент, который должен сопровождать сообщение содержащее рекламу от бота.
+		/// </summary>
+		public static readonly Intent BotAdPromo = RegisterPossibleValue(value: "bot_ad_promo");
 	}
 }
