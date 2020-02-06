3f35e4b09eea2000390db62f9ece50c01cfe5646
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-02-06 22:49:55 +0300

    Добавить тесты appWidgets

---
+++ VkNet.Tests/Categories/AppWidgets/GetGroupImageUploadServerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetGroupImageUploadServerTest : CategoryBaseTest
+	{
+		protected override string Folder => "AppWidgets";
+
+		[Test]
+		public void GetGroupImageUploadServer()
+		{
+			Url = "https://api.vk.com/method/appWidgets.getGroupImageUploadServer";
+
+			ReadCategoryJsonPath(nameof(GetGroupImageUploadServer));
+
+			var result = Api.AppWidgets.GetGroupImageUploadServer(AppWidgetImageType.FiftyOnFifty);
+
+			Assert.IsNotNull(result.UploadUrl);
+		}
+	}
+}
---
+++ VkNet.Tests/Categories/AppWidgets/GetImagesByIdTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using NUnit.Framework;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetImagesByIdTest : CategoryBaseTest
+	{
+		protected override string Folder => "AppWidgets";
+
+		[Test]
+		public void GetAppImages()
+		{
+			Url = "https://api.vk.com/method/appWidgets.getImagesById";
+
+			ReadCategoryJsonPath(nameof(GetAppImages));
+
+			var result = Api.AppWidgets.GetImagesById("7309583_1192027");
+
+			Assert.IsNotNull(result);
+			Assert.AreEqual("7309583_1192027", result.First().Id);
+		}
+	}
+}
