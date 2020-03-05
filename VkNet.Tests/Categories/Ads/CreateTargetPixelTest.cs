35c1a43f86ac96979424f4c9bbeabfd71076e8d3
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-05 18:18:29 +0300

    Написать тест CreateTargetPixelTest

---
+++ VkNet.Tests/Categories/Ads/CreateTargetPixelTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class CreateTargetPixelTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void CreateTargetPixel()
+		{
+			Url = "https://api.vk.com/method/ads.createTargetPixel";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.CreateTargetPixel));
+
+			var result = Api.Ads.CreateTargetPixel(new CreateTargetPixelParams
+			{
+				AccountId = 1605245430,
+				CategoryId = 1
+			});
+
+			Assert.That(result.Id, Is.EqualTo(462641));
+			Assert.That(result.Pixel, Is.EqualTo("code..."));
+		}
+	}
+}
