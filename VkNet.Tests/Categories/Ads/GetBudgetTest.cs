106f2d2efcd70f94e884391454663f154b6cd775
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-06 00:39:28 +0300

    Написать тест GetBudget

---
+++ VkNet.Tests/Categories/Ads/GetBudgetTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetBudgetTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void GetBudget()
+		{
+			Url = "https://api.vk.com/method/ads.getBudget";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.GetBudget));
+
+
+			var result = Api.Ads.GetBudget(1605245430);
+
+			Assert.That(result, Is.EqualTo(100.00));
+		}
+	}
+}
