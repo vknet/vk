94a3882a7bd3baf3f7754201d488fd1679b6dc56
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-05 19:10:47 +0300

    Написать тест UpdateTargetPixelTest

---
+++ VkNet.Tests/Categories/Ads/UpdateTargetPixelTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json.Converters;
+using NUnit.Framework;
+using VkNet.Enums;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class UpdateTargetPixelTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void UpdateTargetPixel()
+		{
+			Url = "https://api.vk.com/method/ads.updateTargetPixel";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.UpdateTargetPixel));
+
+
+			var result = Api.Ads.UpdateTargetPixel(new UpdateTargetPixelParams
+			{
+				AccountId = 1605245430,
+				Name = "123123",
+				CategoryId = 2,
+				TargetPixelId = 462641
+			});
+
+			Assert.That(result, Is.EqualTo(true));
+		}
+	}
+}
