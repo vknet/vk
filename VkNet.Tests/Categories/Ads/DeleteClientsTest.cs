e5638e55853089229bbbc4efa62e7b8df162f6ad
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-05 23:44:29 +0300

    Добавить тест DeleteClients

---
+++ VkNet.Tests/Categories/Ads/DeleteClientsTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class DeleteClientsTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void DeleteClients()
+		{
+			Url = "https://api.vk.com/method/ads.deleteClients";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.DeleteClients));
+
+			string[] a = new[]
+			{
+				"1",
+				"2"
+			};
+
+			var result = Api.Ads.DeleteClients(new DeleteClientsParams
+			{
+				AccountId = 1605245430,
+				Ids = a
+			});
+
+			Assert.That(result[0], Is.EqualTo(true));
+			Assert.That(result[1], Is.EqualTo(true));
+		}
+	}
+}
