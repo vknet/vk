9b691f995574080ecfe1e5dfefd1026287d322b3
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-05 19:10:36 +0300

    Написать тест UpdateTargetGroupTest

---
+++ VkNet.Tests/Categories/Ads/UpdateTargetGroupTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json.Converters;
+using NUnit.Framework;
+using VkNet.Enums;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class UpdateTargetGroupTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void UpdateTargetGroup()
+		{
+			Url = "https://api.vk.com/method/ads.updateTargetGroup";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.UpdateTargetGroup));
+
+
+			var result = Api.Ads.UpdateTargetGroup(new UpdateTargetGroupParams
+			{
+				AccountId = 1605245430,
+				Name = "123123",
+				TargetGroupId = 1,
+				TargetPixelId = 462641,
+				Lifetime = 365
+			});
+
+			Assert.That(result, Is.EqualTo(true));
+		}
+	}
+}
