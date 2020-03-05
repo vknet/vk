1f46512cf20ad8c81f57b9498032b2d647f275c8
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-05 17:29:45 +0300

    Добавить тест GetLookalikeRequestsTest

---
+++ VkNet.Tests/Categories/Ads/GetLookalikeRequestsTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetLookalikeRequestsTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void GetLookalikeRequests()
+		{
+			Url = "https://api.vk.com/method/ads.getLookalikeRequests";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.GetLookalikeRequests));
+
+
+			var result = Api.Ads.GetLookalikeRequests(new GetLookalikeRequestsParams()
+			{
+				AccountId = 1605245430
+			});
+
+			Assert.That(result.Count, Is.EqualTo(1));
+			Assert.That(result.Items[0].Id, Is.EqualTo(37564));
+		}
+	}
+}
