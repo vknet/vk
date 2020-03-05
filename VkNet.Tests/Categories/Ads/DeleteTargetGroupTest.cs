6864e6715b03d8c51e33d42a7c945dc017c35b94
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-05 23:19:45 +0300

    Написать тест DeleteTargetGroup

---
+++ VkNet.Tests/Categories/Ads/DeleteTargetGroupTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class DeleteTargetGroupTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void DeleteTargetGroup()
+		{
+			Url = "https://api.vk.com/method/ads.deleteTargetGroup";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.DeleteTargetGroup));
+
+			var result = Api.Ads.DeleteTargetGroup(new DeleteTargetGroupParams
+			{
+				AccountId = 1605245430,
+				TargetGroupId = 1
+			});
+
+			Assert.That(result, Is.EqualTo(true));
+		}
+	}
+}
