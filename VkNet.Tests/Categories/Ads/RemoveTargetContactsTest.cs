8affa263bc5318492069f69ca29a02b27edff48a
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-05 21:55:13 +0300

    Написать тест RemoveTargetContacts

---
+++ VkNet.Tests/Categories/Ads/RemoveTargetContactsTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class RemoveTargetContactsTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void RemoveTargetContacts()
+		{
+			Url = "https://api.vk.com/method/ads.removeTargetContacts";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.RemoveTargetContacts));
+
+			var result = Api.Ads.RemoveTargetContacts(new RemoveTargetContactsParams
+			{
+				AccountId = 1605245430,
+				Contacts = new List<string> { "79534998632", "79534998633" },
+				TargetGroupId = 29859003
+			});
+
+			Assert.That(result, Is.EqualTo(2));
+		}
+	}
+}
