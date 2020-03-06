ad0fc3139240e21776be10afeac7d1ccb7745bf0
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-06 14:40:01 +0300

    Написать тест GetCategories

---
+++ VkNet.Tests/Categories/Ads/GetCategoriesTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Enums;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetCategoriesTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void GetCategories()
+		{
+			Url = "https://api.vk.com/method/ads.getCategories";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.GetCategories));
+
+			var result = Api.Ads.GetCategories(Language.Ru);
+
+			Assert.That(result.V1[0].Name, Is.EqualTo("Авто/мото"));
+		}
+	}
+}
