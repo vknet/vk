16bbeaf6558f2c1fd3ad6159730e3d3f95ad9f9c
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-06 16:54:21 +0300

    Написать тест GetOfficeUsers

---
+++ VkNet.Tests/Categories/Ads/GetOfficeUsersTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetOfficeUsersTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void GetOfficeUsers()
+		{
+			Url = "https://api.vk.com/method/ads.getOfficeUsers";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.GetOfficeUsers));
+
+			var result = Api.Ads.GetOfficeUsers(123213);
+			Assert.That(result[0].UserId, Is.EqualTo(504736359));
+			Assert.That(result[0].Accesses[0].Role, Is.EqualTo(AccessRole.Admin));
+		}
+	}
+}
