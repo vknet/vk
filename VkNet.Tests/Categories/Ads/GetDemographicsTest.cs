74b6c685d95bbcc1e735a10d2a521f5589f5b9d1
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-06 16:33:33 +0300

    Написать тест GetDemographics

---
+++ VkNet.Tests/Categories/Ads/GetDemographicsTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetDemographicsTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void GetDemographics()
+		{
+			Url = "https://api.vk.com/method/ads.getDemographics";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.GetDemographics));
+
+			var result = Api.Ads.GetDemographics(new GetDemographicsParams
+			{
+				AccountId = 123,
+				DateFrom = "123",
+				DateTo = "123",
+				Ids = "123,123",
+				IdsType = DemographicsType.Ad,
+				Period = "123"
+			});
+
+			Assert.That(result[0].Id, Is.EqualTo(1012219949));
+		}
+	}
+}
