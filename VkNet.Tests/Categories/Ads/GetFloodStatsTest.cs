f583d7772077c731adb7165a1345dca05bfdaa81
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-06 15:35:04 +0300

    Написать тест GetFloodStats

---
+++ VkNet.Tests/Categories/Ads/GetFloodStatsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetFloodStatsTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void GetFloodStats()
+		{
+			Url = "https://api.vk.com/method/ads.getFloodStats";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.GetFloodStats));
+
+			var result = Api.Ads.GetFloodStats(123213);
+
+			Assert.That(result.Left, Is.EqualTo(4998));
+		}
+	}
+}
