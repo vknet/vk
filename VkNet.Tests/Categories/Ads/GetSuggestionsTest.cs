7f452bf6dd95e389efb76249d86b24f7fa843e07
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-03-06 19:11:33 +0300

    Написать тест GetSuggestions

---
+++ VkNet.Tests/Categories/Ads/GetSuggestionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model.RequestParams.Ads;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Ads
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetSuggestionsTest : CategoryBaseTest
+	{
+		protected override string Folder => "Ads";
+
+		[Test]
+		public void GetSuggestions()
+		{
+			Url = "https://api.vk.com/method/ads.getSuggestions";
+
+			ReadCategoryJsonPath(nameof(Api.Ads.GetSuggestions));
+
+			var result = Api.Ads.GetSuggestions(new GetSuggestionsParams
+			{
+				Section = "schools",
+				Country = 1,
+				Cities = "1",
+				Q = "1"
+			});
+			Assert.That(result[0].Id, Is.EqualTo("123"));
+		}
+	}
+}
