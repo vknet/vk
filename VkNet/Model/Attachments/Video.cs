944a37e826de4ce0cdb26a2e39d7eb932ff9ead5
1dndn <idndn@inbox.ru>
2020-04-22 19:30:30 +0300

    Поправлена документация видеозаписи

--- VkNet/Model/Attachments/Video.cs
+++ VkNet/Model/Attachments/Video.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Newtonsoft.Json;
@@ -165,7 +165,7 @@ namespace VkNet.Model.Attachments
 		public string Platform { set; get; }
 
 		/// <summary>
-		/// поле возвращается, если пользователь может редактировать видеозапись, всегда
+		/// Поле возвращается, если пользователь может редактировать видеозапись, всегда
 		/// содержит 1.
 		/// </summary>
 		[JsonProperty("can_edit")]
@@ -178,7 +178,7 @@ namespace VkNet.Model.Attachments
 		public bool? CanAdd { get; set; }
 
 		/// <summary>
-		/// поле возвращается, если видеозапись приватная (например, была загружена в
+		/// Поле возвращается, если видеозапись приватная (например, была загружена в
 		/// личное сообщение), всегда содержит 1.
 		/// </summary>
 		[JsonProperty("is_private")]
