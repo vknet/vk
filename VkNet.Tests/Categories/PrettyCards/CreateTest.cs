2debd745d7890995861805d1274857aeb55136ab
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-05-02 10:45:29 +0300

    Add PrettyCards.Create test

---
+++ VkNet.Tests/Categories/PrettyCards/CreateTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.PrettyCards
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class CreateTest : CategoryBaseTest
+	{
+		protected override string Folder => "PrettyCards";
+
+		[Test]
+		public void Create()
+		{
+			Url = "https://api.vk.com/method/prettyCards.create";
+
+			ReadCategoryJsonPath(nameof(Api.PrettyCards.Create));
+
+			var result = Api.PrettyCards.Create(new PrettyCardsCreateParams
+			{
+				OwnerId = -126102803,
+				Photo = "-126102803_457239118",
+				Price = "123",
+				PriceOld = "140",
+				Title = "123"
+			});
+
+			Assert.NotNull(result);
+			Assert.AreEqual(result.CardId, 545435);
+		}
+	}
+}
