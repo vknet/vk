19747285fd680dccbc8eab7dd15e9281716f5f63
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-05-02 10:45:16 +0300

    Add PrettyCards.Create

---
+++ VkNet/Model/Results/PrettyCards/PrettyCardsCreateResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace VkNet.Model
+{
+	[Serializable]
+	public class PrettyCardsCreateResult
+	{
+		/// <summary>
+		/// Идентификатор владельца карточки.
+		/// </summary>
+		[JsonProperty("owner_id")]
+		public long? OwnerId { get; set; }
+
+		/// <summary>
+		/// Идентификатор карточки.
+		/// </summary>
+		[JsonProperty("card_id")]
+		public long? CardId { get; set; }
+	}
+}
