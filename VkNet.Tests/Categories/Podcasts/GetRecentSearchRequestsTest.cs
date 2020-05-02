df78d3c07ba3a93471f95c1805812825df823747
Panuchi <41389318+Panuchi@users.noreply.github.com>
2020-05-02 15:56:23 +0300

    Add Podcasts Category Tests

---
+++ VkNet.Tests/Categories/Podcasts/GetRecentSearchRequestsTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Podcasts
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class GetRecentSearchRequestsTest : CategoryBaseTest
+	{
+		protected override string Folder => "Podcasts";
+
+		[Test]
+		public void GetRecentSearchRequests()
+		{
+			Url = "https://api.vk.com/method/podcasts.getRecentSearchRequests";
+
+			ReadCategoryJsonPath(nameof(Api.Podcasts.GetRecentSearchRequests));
+
+			var result = Api.Podcasts.GetRecentSearchRequests();
+
+			Assert.NotNull(result);
+			Assert.AreEqual(result[0], "kukluxklan");
+			Assert.AreEqual(result[1], "ted");
+		}
+	}
+}
---
+++ VkNet.Tests/Categories/Podcasts/SearchTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using VkNet.Model.RequestParams;
+using VkNet.Tests.Infrastructure;
+
+namespace VkNet.Tests.Categories.Podcasts
+{
+	[TestFixture]
+	[ExcludeFromCodeCoverage]
+	public class SearchTest : CategoryBaseTest
+	{
+		protected override string Folder => "Podcasts";
+
+		[Test]
+		public void Search()
+		{
+			Url = "https://api.vk.com/method/podcasts.search";
+
+			ReadCategoryJsonPath(nameof(Api.Podcasts.Search));
+
+			var result = Api.Podcasts.Search(new PodcastsSearchParams
+			{
+				SearchString = "дудь",
+				Offset = 0,
+				Count = 100
+			});
+
+			Assert.NotNull(result);
+			Assert.AreEqual(result.Podcasts[0].OwnerId, -189167851);
+			Assert.AreEqual(result.Episodes[0].Id, 456239643);
+			Assert.AreEqual(result.Groups[0].Id, 189167851);
+		}
+	}
+}
