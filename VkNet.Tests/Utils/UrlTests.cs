ffe3ec4c805f46303a8601135f39b15b6334022f
itsokto <picnet9@gmail.com>
2020-05-14 04:18:43 +0300

    Реализовать апи для построения Url

---
+++ VkNet.Tests/Utils/UrlTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using NUnit.Framework;
+using VkNet.Utils;
+
+namespace VkNet.Tests.Utils
+{
+	[TestFixture, Parallelizable]
+	[ExcludeFromCodeCoverage]
+	public class UrlTests
+	{
+		[Test]
+		public void Query_From()
+		{
+			var parameters = new Dictionary<string, string>
+			{
+				{ "key1", "value1" },
+				{ "key2", "value2" }
+			};
+
+			var query = Url.QueryFrom(parameters.ToArray());
+
+			Assert.AreEqual("key1=value1&key2=value2", query);
+		}
+
+		[Test]
+		public void Combine_With_Query()
+		{
+			const string testUrl = "https://www.google.com/search?q=dictionary&sourceid=chrome&ie=UTF-8";
+			const string expectedUrl = "https://www.google.com/search?q=dictionary&sourceid=chrome&ie=UTF-8&key1=value1&key2=value2";
+			const string query = "key1=value1&key2=value2";
+
+			var actualUrl = Url.Combine(testUrl, query);
+
+			Assert.AreEqual(expectedUrl, actualUrl);
+		}
+	}
+}
