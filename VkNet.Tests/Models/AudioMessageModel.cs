263041fbca4503b70d3d1513fa0d1902af043e14
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-07-01 00:29:56 +0300

    Добавить тесты на AudioMessage для проверки внесения данных транскрипции

--- VkNet.Tests/Models/AudioMessageModel.cs
+++ VkNet.Tests/Models/AudioMessageModel.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using NUnit.Framework;
+using VkNet.Enums.SafetyEnums;
 using VkNet.Model.Attachments;
 
 namespace VkNet.Tests.Models
 {
 	[TestFixture]
 	[ExcludeFromCodeCoverage]
-	public class AudioMessageModel
+	public class AudioMessageModel : BaseTest
 	{
 		[Test]
 		public void ToString_AudioMessageShouldHaveAccessKey()
@@ -21,6 +23,41 @@ namespace VkNet.Tests.Models
 			var result = audioMessage.ToString();
 
 			Assert.AreEqual(result, "audio_message1234_1234_test");
+		}
+
+		[Test]
+		public void ShouldDeserializeFromVkResponseToAudioMessageWithTranscript()
+		{
+			ReadJsonFile("Models", "audio_message_with_transcription");
+
+			var response = GetResponse();
+
+			var attachment = Attachment.FromJson(response);
+			Assert.True(attachment.Instance is AudioMessage);
+
+			var audioMessage = attachment.Instance as AudioMessage;
+			Assert.That(audioMessage.Duration, Is.EqualTo(25));
+			Assert.That(audioMessage.LinkOgg, Is.EqualTo(new Uri("https://psv4.userapi.com/c205420//u111874665/audiomsg/d12/794c8440a8.ogg")));
+			Assert.That(audioMessage.LinkMp3, Is.EqualTo(new Uri("https://psv4.userapi.com/c205420//u111874665/audiomsg/d12/794c8440a8.mp3")));
+			Assert.That(audioMessage.Transcript, Is.EqualTo("demo message"));
+			Assert.That(audioMessage.TranscriptState, Is.EqualTo(TranscriptStates.Done));
+		}
+
+		[Test]
+		public void ShouldDeserializeFromVkResponseToAudioMessage()
+		{
+			ReadJsonFile("Models", "audio_message_without_transcription");
+
+			var response = GetResponse();
+
+			var attachment = Attachment.FromJson(response);
+			Assert.True(attachment.Instance is AudioMessage);
+
+			var audioMessage = attachment.Instance as AudioMessage;
+			Assert.That(audioMessage.Duration, Is.EqualTo(25));
+			Assert.That(audioMessage.LinkOgg, Is.EqualTo(new Uri("https://psv4.userapi.com/c205420//u111874665/audiomsg/d12/794c8440a8.ogg")));
+			Assert.That(audioMessage.LinkMp3, Is.EqualTo(new Uri("https://psv4.userapi.com/c205420//u111874665/audiomsg/d12/794c8440a8.mp3")));
+			Assert.That(audioMessage.Transcript, Is.EqualTo(null));
 		}
 	}
 }
