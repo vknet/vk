08c6639b9d3cca651cb192e6a890a02ecdebbf54
wex335 <77948630+wex335@users.noreply.github.com>
2021-07-15 00:58:23 +0300

    #1069 Добавляет в GroupUpdate событие GroupChangeSettings
    * Примерно работает +-
    
    * добавил GroupChangeSettings в GroupUpdate
    
    * Небольшие правки
    
    * Поправил XMLDOC

---
+++ VkNet/Enums/SafetyEnums/GroupChangeSettings.cs
@@ -0,0 +1,63 @@
+using System;
+using Newtonsoft.Json;
+using VkNet.Utils;
+using VkNet.Utils.JsonConverter;
+using VkNet.Enums.SafetyEnums;
+
+namespace VkNet.Model.GroupUpdate
+{
+	/// <summary>
+	/// Событие о изменении настроек сообщества
+	/// </summary>
+	[Serializable]
+	public class GroupChangeSettings
+	{
+		/// <summary>
+		/// Идентификатор пользователя, который внёс изменения;
+		/// </summary>
+		[JsonProperty("user_id")]
+		public long? UserId { get; set; }
+
+		/// <summary>
+		/// Описание внесённых изменений
+		/// </summary>
+		[JsonProperty("changes")]
+		public Changes Changes { get; set; }
+
+
+		#region Методы
+
+		/// <summary>
+		/// </summary>
+		/// <param name="response"> </param>
+		/// <returns> </returns>
+		public static GroupChangeSettings FromJson(VkResponse response)
+		{
+			return new GroupChangeSettings
+			{
+				UserId = response["user_id"],
+				Changes = response["changes"],
+
+			};
+		}
+
+		/// <summary>
+		/// Преобразование класса <see cref="GroupChangeSettings" /> в <see cref="VkParameters" />
+		/// </summary>
+		/// <param name="response"> Ответ сервера. </param>
+		/// <returns>Результат преобразования в <see cref="GroupChangeSettings" /></returns>
+		public static implicit operator GroupChangeSettings(VkResponse response)
+		{
+			if (response == null)
+			{
+				return null;
+			}
+
+			return response.HasToken()
+				? FromJson(response)
+				: null;
+		}
+
+		#endregion
+	}
+}
