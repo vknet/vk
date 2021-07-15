9c156c02425a4d1a3a3031c85824a0d9deb796ef
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2021-07-15 22:04:39 +0300

    #1183 Добавляет тесты для события GroupChangeSettings

---
+++ VkNet.Tests/Models/GroupChangeSettingsTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using VkNet.Model.GroupUpdate;
+
+namespace VkNet.Tests.Models
+{
+	[TestFixture]
+	public class GroupChangeSettingsTests : BaseTest
+	{
+		[Test]
+		public void GroupChangeSettings_should_correct_deserialize_from_VkResponse()
+		{
+			ReadJsonFile("Models", nameof(GroupChangeSettings));
+
+			var response = GetResponse();
+
+			var result = GroupChangeSettings.FromJson(response);
+
+			result.Should().NotBeNull();
+			result.UserId.Should().Be(641766519);
+			result.Changes.Should().HaveCount(2);
+			result.Changes.Should().ContainKey("title");
+			result.Changes.Should().ContainKey("description");
+		}
+
+		[Test]
+		public void GroupChangeSettings_should_correct_deserialize_from_JsonConverter()
+		{
+			ReadJsonFile("Models", nameof(GroupChangeSettings));
+
+			var result = JsonConvert.DeserializeObject<GroupChangeSettings>(Json);
+
+			result.Should().NotBeNull();
+
+			if (result == null)
+			{
+				return;
+			}
+
+			result.UserId.Should().Be(641766519);
+			result.Changes.Should().HaveCount(2);
+			result.Changes.Should().ContainKey("title");
+			result.Changes.Should().ContainKey("description");
+		}
+	}
+}
