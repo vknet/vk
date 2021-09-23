c779bd06a506de04d630f7240f00a929f7ad0e62
Bird Egop <sampletext32@bk.ru>
2021-09-23 03:52:22 +0300

    Mark cyrillic properties as obsolete.

--- VkNet/Model/Audio/AudioCatalogAudioAds.cs
+++ VkNet/Model/Audio/AudioCatalogAudioAds.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
+using VkNet.Utils;
 
 namespace VkNet.Model
 {
@@ -12,8 +13,15 @@ namespace VkNet.Model
 		/// <summary>
 		/// Идентификатор контента.
 		/// </summary>
+		[Obsolete(ObsoleteText.ObsoleteCyrillicProperty, true)]
 		[JsonProperty("content_id")]
 		public string СontentId { get; set; }
+
+		/// <summary>
+		/// Идентификатор контента.
+		/// </summary>
+		[JsonProperty("content_id")]
+		public string ContentId { get; set; }
 
 		/// <summary>
 		/// Длительность.
