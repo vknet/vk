672d042b816c83d774570ae18eb45164aab2faa4
Bird Egop <sampletext32@bk.ru>
2021-09-24 10:21:31 +0300

    Фикс времени AwaitableConstraint и его теста. (#1200)
    * Fix Ignored Test to use Interlocked with parallel increment
    
    * Fix AwaitableConstraint waiting timing inaccuracy.
    Fix invalid slots amount after awaiting for constraint.

--- VkNet.Tests/VkApiTest.cs
+++ VkNet.Tests/VkApiTest.cs
@@ -53,7 +53,7 @@ namespace VkNet.Tests
 			Assert.IsTrue(isUpdated);
 		}
 
-		[Test, Ignore("Тест врет и не проверяет стабильное воспроизведение, ошибка либо в реализации либо в базовом классе теста")]
+		[Test, /*Ignore("Тест врет и не проверяет стабильное воспроизведение, ошибка либо в реализации либо в базовом классе теста")*/]
 		public async Task Call_NotMoreThen3CallsPerSecond()
 		{
 			Url = "https://api.vk.com/method/friends.getRequests";
@@ -66,7 +66,7 @@ namespace VkNet.Tests
 
 			for (var i = 0; i < callsCount + 1; i++)
 			{
-				taskList.Add(Api.CallAsync("friends.getRequests", VkParameters.Empty, true).ContinueWith((_) => calls++));
+				taskList.Add(Api.CallAsync("friends.getRequests", VkParameters.Empty, true).ContinueWith(_ => Interlocked.Increment(ref calls)));
 			}
 
 			await Task.Delay(1000);
