b23e37fbd59e6c6b68d7f713cdb318995d77d547
Bird Egop <sampletext32@bk.ru>
2021-09-27 23:14:05 +0300

    Add missing "video" field to Call Attachment + Tests
    Signed-off-by: Bird Egop <sampletext32@bk.ru>

---
+++ VkNet.Tests/Categories/Messages/MessagesWithCallTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using VkNet.Model;
+using VkNet.Model.Attachments;
+
+namespace VkNet.Tests.Categories.Messages
+{
+	[TestFixture]
+	public class MessagesWithCallTests : MessagesBaseTests
+	{
+		[Test]
+		public void Message_WithCallAttachment_AllFieldsArePresent()
+		{
+			ReadJsonFile("Models", "message_with_call");
+
+			var message = Message.FromJson(GetResponse());
+
+			Assert.IsNotEmpty(message.Attachments);
+
+			var call = message.Attachments[0].Instance as Call;
+
+			Assert.IsNotNull(call);
+
+			Assert.IsTrue(call.Video);
+
+			Assert.AreEqual("reached", call.State);
+			Assert.AreEqual(12345678, call.InitiatorId);
+			Assert.AreEqual(2012345678, call.ReceiverId);
+			Assert.IsTrue(call.Duration.HasValue);
+			Assert.AreEqual(30, call.Duration.Value);
+
+			Assert.AreEqual(new DateTime(2021,
+					9,
+					27,
+					19,
+					21,
+					21,
+					DateTimeKind.Utc),
+				call.Time.AsUtc());
+		}
+	}
+}
