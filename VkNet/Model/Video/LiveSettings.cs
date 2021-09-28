9e3a7deebfdd8ef55da85e2a93e91f8082b953a0
Bird Egop <sampletext32@bk.ru>
2021-09-29 01:34:45 +0300

    Огромное расширение модели видео (#1208)

---
+++ VkNet/Model/Video/LiveSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace VkNet.Model
+{
+	/// <summary>
+	/// Параметры прямой трансляции
+	/// </summary>
+	[Serializable]
+	public class LiveSettings
+	{
+		/// <summary>
+		/// Можно ли перематывать прямую трансляцию
+		/// </summary>
+		[JsonProperty("can_rewind")]
+		public int? CanRewind { get; set; }
+
+		/// <summary>
+		/// Является ли прямая трансляция бесконечной
+		/// </summary>
+		[JsonProperty("is_endless")]
+		public int? IsEndless { get; set; }
+
+		/// <summary>
+		/// Максимальная длительность прямой трансляции в секундах
+		/// </summary>
+		[JsonProperty("max_duration")]
+		public int? MaxDuration { get; set; }
+	}
+}
