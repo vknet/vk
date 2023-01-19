31a182940845019ce9cddd3f4bdda7611c0e5375
Panuchi <tim.jaager@gmail.com>
2023-01-20 01:17:39 +0200

    добавил свойства + убрал имплисит преобразование

--- VkNet/Model/Results/Wall/RepostResult.cs
+++ VkNet/Model/Results/Wall/RepostResult.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 using VkNet.Utils;
 
 namespace VkNet.Model;
@@ -12,42 +13,24 @@ public class RepostResult
 	/// <summary>
 	/// всегда содержит 1;
 	/// </summary>
+	[JsonProperty("success")]
 	public bool Success { get; set; }
 
 	/// <summary>
 	/// идентификатор созданной записи;
 	/// </summary>
+	[JsonProperty("post_id")]
 	public long? PostId { get; set; }
 
 	/// <summary>
 	/// количество репостов объекта с учетом осуществленного;
 	/// </summary>
+	[JsonProperty("reposts_count")]
 	public int? RepostsCount { get; set; }
 
 	/// <summary>
 	/// число отметок «Мне нравится» у объекта.
 	/// </summary>
+	[JsonProperty("likes_count")]
 	public int? LikesCount { get; set; }
-
-	#region ������
-
-	/// <summary>
-	/// ��������� �� json.
-	/// </summary>
-	/// <param name="response"> ����� �������. </param>
-	/// <returns> </returns>
-	public static RepostResult FromJson(VkResponse response)
-	{
-		var result = new RepostResult
-		{
-			Success = response[key: "success"],
-			PostId = response[key: "post_id"],
-			RepostsCount = response[key: "reposts_count"],
-			LikesCount = response[key: "likes_count"]
-		};
-
-		return result;
-	}
-
-	#endregion
 }
