16fac1bc14b44cec3a1912231fa2638f7d40100d
Panuchi <tim.jaager@gmail.com>
2023-01-20 00:44:21 +0200

    добавил аннотации

--- VkNet/Model/Results/Auth/AuthorizationResult.cs
+++ VkNet/Model/Results/Auth/AuthorizationResult.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace VkNet.Model;
 
@@ -11,20 +12,24 @@ public class AuthorizationResult
 	/// <summary>
 	/// Access Token
 	/// </summary>
+	[JsonProperty("access_token")]
 	public string AccessToken { get; set; }
 
 	/// <summary>
 	/// Время истечения токена
 	/// </summary>
+	[JsonProperty("expires_in")]
 	public int ExpiresIn { get; set; }
 
 	/// <summary>
 	/// Идентификатор пользователя
 	/// </summary>
+	[JsonProperty("user_id")]
 	public long UserId { get; set; }
 
 	/// <summary>
 	/// Произвольная строка, которая будет возвращена вместе с результатом авторизации.
 	/// </summary>
+	[JsonProperty("state")]
 	public string State { get; set; }
 }
