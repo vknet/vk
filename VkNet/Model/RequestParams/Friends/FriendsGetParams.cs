c6a0abb459fdc6805b5e9b17309746e3fb2de6ec
Panuchi <tim.jaager@gmail.com>
2023-01-26 15:23:44 +0200

    Добавляет модель FriendsGetExtended

--- VkNet/Model/RequestParams/Friends/FriendsGetParams.cs
+++ VkNet/Model/RequestParams/Friends/FriendsGetParams.cs
@@ -91,3 +91,78 @@ public class FriendsGetParams
 	[JsonProperty("ref")]
 	public string Reference { get; set; }
 }
+
+/// <summary>
+/// Параметры метода friends.get 2
+/// </summary>
+[Serializable]
+public class FriendsGetParams2
+{
+	/// <summary>
+	/// Идентификатор пользователя, для которого необходимо получить список друзей.
+	/// Если параметр не задан, то считается, что он равен идентификатору текущего
+	/// пользователя
+	/// (справедливо для вызова с передачей access_token).
+	/// целое число.
+	/// </summary>
+	[JsonProperty("user_id")]
+	public long? UserId { get; set; }
+
+	/// <summary>
+	/// Порядок, в котором нужно вернуть список друзей. Допустимые значения:
+	/// hints - сортировать по рейтингу, аналогично тому, как друзья сортируются в
+	/// разделе Мои друзья (данный параметр
+	/// доступен только для Desktop-приложений).
+	/// random - возвращает друзей в случайном порядке.
+	/// mobile - возвращает выше тех друзей, у которых установлены мобильные
+	/// приложения.
+	/// name - сортировать по имени. Данный тип сортировки работает медленно, так как
+	/// сервер будет получать всех друзей а
+	/// не только указанное количество count. (работает только при переданном параметре
+	/// fields)
+	/// строка.
+	/// </summary>
+	[JsonProperty("order")]
+	[JsonConverter(typeof(SafetyEnumJsonConverter))]
+	public FriendsOrder Order { get; set; }
+
+	/// <summary>
+	/// Идентификатор списка друзей, полученный методом friends.getLists, друзей из
+	/// которого необходимо получить. Данный
+	/// параметр учитывается, только когда параметр user_id равен идентификатору
+	/// текущего пользователя.
+	/// Данный параметр доступен только для Desktop-приложений. положительное число.
+	/// </summary>
+	[JsonProperty("list_id")]
+	public long? ListId { get; set; }
+
+	/// <summary>
+	/// Количество друзей, которое нужно вернуть. (по умолчанию – все друзья)
+	/// положительное число.
+	/// </summary>
+	[JsonProperty("count")]
+	public long? Count { get; set; }
+
+	/// <summary>
+	/// Смещение, необходимое для выборки определенного подмножества друзей.
+	/// положительное число.
+	/// </summary>
+	[JsonProperty("offset")]
+	public long? Offset { get; set; }
+
+	/// <summary>
+	/// Падеж для склонения имени и фамилии пользователя. Возможные значения:
+	/// именительный – nom, родительный – gen,
+	/// дательный – dat, винительный – acc, творительный – ins, предложный – abl. По
+	/// умолчанию nom. строка.
+	/// </summary>
+	[JsonProperty("name_case")]
+	[JsonConverter(typeof(SafetyEnumJsonConverter))]
+	public NameCase NameCase { get; set; }
+
+	/// <summary>
+	/// Ссылка?
+	/// </summary>
+	[JsonProperty("ref")]
+	public string Reference { get; set; }
+}
