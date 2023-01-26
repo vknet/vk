cb7da8cfe4a33fc7f14c3bd5f55bb51df25a9c8d
Panuchi <tim.jaager@gmail.com>
2023-01-26 15:19:35 +0200

    Добавляет аннотации к свойствам

--- VkNet/Model/RequestParams/Account/AccountSaveProfileInfoParams.cs
+++ VkNet/Model/RequestParams/Account/AccountSaveProfileInfoParams.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 using VkNet.Enums;
 
 namespace VkNet.Model.RequestParams;
@@ -12,21 +13,25 @@ public class AccountSaveProfileInfoParams
 	/// <summary>
 	/// Имя пользователя. строка.
 	/// </summary>
+	[JsonProperty("first_name")]
 	public string FirstName { get; set; }
 
 	/// <summary>
 	/// Фамилия пользователя. строка.
 	/// </summary>
+	[JsonProperty("last_name")]
 	public string LastName { get; set; }
 
 	/// <summary>
 	/// Девичья фамилия пользователя (только для женского пола). строка.
 	/// </summary>
+	[JsonProperty("maiden_name")]
 	public string MaidenName { get; set; }
 
 	/// <summary>
 	/// Короткое имя страницы. строка.
 	/// </summary>
+	[JsonProperty("screen_name")]
 	public string ScreenName { get; set; }
 
 	/// <summary>
@@ -35,6 +40,7 @@ public class AccountSaveProfileInfoParams
 	/// 2 — мужской.
 	/// положительное число.
 	/// </summary>
+	[JsonProperty("sex")]
 	public Sex? Sex { get; set; }
 
 	/// <summary>
@@ -49,17 +55,20 @@ public class AccountSaveProfileInfoParams
 	/// 0 — не указано.
 	/// положительное число.
 	/// </summary>
+	[JsonProperty("relation")]
 	public RelationType? Relation { get; set; }
 
 	/// <summary>
 	/// Идентификатор пользователя, с которым связано семейное положение. положительное
 	/// число.
 	/// </summary>
+	[JsonProperty("relation_partner")]
 	public User RelationPartner { get; set; }
 
 	/// <summary>
 	/// Дата рождения пользователя в формате DD.MM.YYYY, например "15.11.1984". строка.
 	/// </summary>
+	[JsonProperty("bdate")]
 	public string BirthDate { get; set; }
 
 	/// <summary>
@@ -69,27 +78,32 @@ public class AccountSaveProfileInfoParams
 	/// 0 — не показывать дату рождения.
 	/// положительное число.
 	/// </summary>
+	[JsonProperty("bdate_visibility")]
 	public BirthdayVisibility? BirthdayVisibility { get; set; }
 
 	/// <summary>
 	/// Родной город пользователя. строка.
 	/// </summary>
+	[JsonProperty("home_town")]
 	public string HomeTown { get; set; }
 
 	/// <summary>
 	/// Идентификатор страны пользователя. положительное число.
 	/// </summary>
+	[JsonProperty("country")]
 	public Country Country { get; set; }
 
 	/// <summary>
 	/// Идентификатор города пользователя. положительное число.
 	/// </summary>
+	[JsonProperty("city")]
 	public City City { get; set; }
 
 	/// <summary>
 	/// Статус пользователя, который также может быть изменен методом status.set
 	/// строка.
 	/// </summary>
+	[JsonProperty("status")]
 	public string Status { get; set; }
 
 	/// <summary>
@@ -98,5 +112,6 @@ public class AccountSaveProfileInfoParams
 	/// <remarks>
 	/// Обнаружено опытным путем.
 	/// </remarks>
+	[JsonProperty("phone")]
 	public string Phone { get; set; }
 }
--- VkNet/Model/TopicsFeed.cs
+++ VkNet/Model/TopicsFeed.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using Newtonsoft.Json;
 
 namespace VkNet.Model;
 
@@ -12,20 +13,24 @@ public class TopicsFeed
 	/// <summary>
 	/// Количество.
 	/// </summary>
+	[JsonProperty("count")]
 	public int Count { get; set; }
 
 	/// <summary>
 	/// Массив комментариев.
 	/// </summary>
+	[JsonProperty("items")] //TODO:
 	public ReadOnlyCollection<CommentBoard> Items { get; set; }
 
 	/// <summary>
 	/// Информация о пользователях, которые находятся в списке комментариев.
 	/// </summary>
+	[JsonProperty("profiles")]
 	public ReadOnlyCollection<User> Profiles { get; set; }
 
 	/// <summary>
 	/// Информация о группах, которые находятся в списке комментариев.
 	/// </summary>
+	[JsonProperty("groups")]
 	public ReadOnlyCollection<Group> Groups { get; set; }
 }
