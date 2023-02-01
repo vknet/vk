32482dc9a0deb91dcccaba01f456a282c8c9324b
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-02-01 13:56:26 +0200

    Убрал FromJson  для VideoAdsParams(#1281)
    * Убрал неиспользуемые зависимости
    
    * Удаляет FromJson из категории Donut
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson из категории Ads
    
    * Разделил на 2 класса
    
    * Удаляет FromJson из AppImage
    
    * Убрал неиспользуемые зависимости
    
    * Убрал FromJson
    
    * Добавил JsonProperty
    
    ---------
    
    Co-authored-by: inyutin-maxim <inyutin_maxim@mail.ru>

--- VkNet/Model/Results/Account/AccountChangePasswordResult.cs
+++ VkNet/Model/Results/Account/AccountChangePasswordResult.cs
@@ -1,6 +1,5 @@
 using Newtonsoft.Json;
 using System;
-using VkNet.Utils;
 
 namespace VkNet.Model;
 
@@ -21,20 +20,4 @@ public class AccountChangePasswordResult
 	/// </summary>
 	[JsonProperty("secret")]
 	public string Secret { get; set; }
-
-	/// <summary>
-	/// Разобрать из json.
-	/// </summary>
-	/// <param name="response"> Ответ сервера. </param>
-	/// <returns> </returns>
-	public static AccountChangePasswordResult FromJson(VkResponse response)
-	{
-		var item = new AccountChangePasswordResult
-		{
-			Token = response[key: "token"],
-			Secret = response[key: "secret"]
-		};
-
-		return item;
-	}
 }
--- VkNet/Utils/VkResponseToModelCastsGenerator.cs
+++ VkNet/Utils/VkResponseToModelCastsGenerator.cs
@@ -179,18 +179,6 @@ public partial class VkResponse
 	/// <returns>
 	/// Результат преобразования.
 	/// </returns>
-	public static implicit operator AccountChangePasswordResult(VkResponse response) =>
-		response?._token == null || !response._token.HasValues
-			? null
-			: AccountChangePasswordResult.FromJson(response);
-
-	/// <summary>
-	/// Преобразовать из VkResponse
-	/// </summary>
-	/// <param name="response"> Ответ. </param>
-	/// <returns>
-	/// Результат преобразования.
-	/// </returns>
 	public static implicit operator Application(VkResponse response) => response?._token == null || !response._token.HasValues
 		? null
 		: Application.FromJson(response);
