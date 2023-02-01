239414f61f7337f6c6b492a6ef248180813af57e
Panuchi <tim.jaager@gmail.com>
2023-02-01 22:28:30 +0200

    Удаляет FromJson

--- VkNet/Model/UserSpecification.cs
+++ VkNet/Model/UserSpecification.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
 using VkNet.Enums.SafetyEnums;
-using VkNet.Utils;
 using VkNet.Utils.JsonConverter;
 
 namespace VkNet.Model;
@@ -30,16 +29,4 @@ public class UserSpecification
 	/// </summary>
 	[JsonProperty(propertyName: "client_id")]
 	public long ClientId { get; set; }
-
-	/// <summary>
-	/// Разобрать из json.
-	/// </summary>
-	/// <param name="response"> Ответ сервера. </param>
-	/// <returns> </returns>
-	public static UserSpecification FromJson(VkResponse response) => new()
-	{
-		UserId = response[key: "user_id"],
-		Role = response[key: "role"],
-		ClientId = response["client_id"]
-	};
 }
