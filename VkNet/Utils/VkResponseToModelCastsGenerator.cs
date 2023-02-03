68f83d0237a898227f701a6531818ad08bceb685
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-02-03 17:28:38 +0200

    Удаляет FromJson для AuthConfirmResult (#1290)
    * Убрал неиспользуемые зависимости
    
    * Удаляет FromJson из категории Donut
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson из категории Ads
    
    * Разделил на 2 класса
    
    * Удаляет FromJson из AppImage
    
    * Убрал неиспользуемые зависимости
    
    * Убрал FromJson
    
    * Добавил JsonProperty
    
    * Удалил FromJson
    
    * Удалил FromJson из GroupMember
    
    * Убрал FromJson
    
    * Убрал FromJson
    
    * Удалил FromJson
    
    * Удаляет FromJson из Keyboard
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    ---------
    
    Co-authored-by: inyutin-maxim <inyutin_maxim@mail.ru>

--- VkNet/Utils/VkResponseToModelCastsGenerator.cs
+++ VkNet/Utils/VkResponseToModelCastsGenerator.cs
@@ -178,17 +178,6 @@ public partial class VkResponse
 	/// <returns>
 	/// Результат преобразования.
 	/// </returns>
-	public static implicit operator AuthConfirmResult(VkResponse response) => response?._token == null || !response._token.HasValues
-		? null
-		: AuthConfirmResult.FromJson(response);
-
-	/// <summary>
-	/// Преобразовать из VkResponse
-	/// </summary>
-	/// <param name="response"> Ответ. </param>
-	/// <returns>
-	/// Результат преобразования.
-	/// </returns>
 	public static implicit operator Button(VkResponse response) => response?._token == null || !response._token.HasValues
 		? null
 		: Button.FromJson(response);
