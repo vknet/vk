7f7401ff243dcbd1bc4180a89dc73d23bd566a64
Panuchi <tim.jaager@gmail.com>
2023-02-03 19:07:04 +0200

    Удаляет FromJson

--- VkNet/Utils/VkResponseToModelCastsGenerator.cs
+++ VkNet/Utils/VkResponseToModelCastsGenerator.cs
@@ -46,17 +46,6 @@ public partial class VkResponse
 	/// <returns>
 	/// Результат преобразования.
 	/// </returns>
-	public static implicit operator Email(VkResponse response) => response?._token == null || !response._token.HasValues
-		? null
-		: Email.FromJson(response);
-
-	/// <summary>
-	/// Преобразовать из VkResponse
-	/// </summary>
-	/// <param name="response"> Ответ. </param>
-	/// <returns>
-	/// Результат преобразования.
-	/// </returns>
 	public static implicit operator ShortLink(VkResponse response) => response?._token == null || !response._token.HasValues
 		? null
 		: ShortLink.FromJson(response);
