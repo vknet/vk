b82d0b0dc8493c8c557bcc02296154b1b67883e8
Panuchi <tim.jaager@gmail.com>
2023-02-08 11:28:47 +0200

    Удаляет FromJson для InformationAboutOffers

--- VkNet/Model/InformationAboutOffers.cs
+++ VkNet/Model/InformationAboutOffers.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Newtonsoft.Json;
-using VkNet.Utils;
 
 namespace VkNet.Model;
 
@@ -66,22 +65,4 @@ public class InformationAboutOffers
 	/// </summary>
 	[JsonProperty("price")]
 	public long Price { get; set; }
-
-	/// <summary>
-	/// Разобрать из json.
-	/// </summary>
-	/// <param name="response"> Ответ сервера. </param>
-	/// <returns> </returns>
-	public static InformationAboutOffers FromJson(VkResponse response) => new()
-	{
-		Id = response[key: "id"],
-		Title = response[key: "title"],
-		Instruction = response[key: "instruction"],
-		InstructionHtml = response[key: "instruction_html"],
-		ShortDescription = response[key: "short_description"],
-		Description = response[key: "description"],
-		Img = new(uriString: response[key: "img"]),
-		Tag = response[key: "tag"],
-		Price = response[key: "price"]
-	};
 }
