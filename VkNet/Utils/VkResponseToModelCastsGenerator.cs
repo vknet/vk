e068648dcc319be9533c18b88414a8a2881ee508
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-02-10 15:22:06 +0200

    Удаляет FromJson для UserSpecification(#1311)
    ---------
    
    Co-authored-by: inyutin-maxim <inyutin_maxim@mail.ru>

--- VkNet/Utils/VkResponseToModelCastsGenerator.cs
+++ VkNet/Utils/VkResponseToModelCastsGenerator.cs
@@ -1102,17 +1102,6 @@ public partial class VkResponse
 	/// <returns>
 	/// Результат преобразования.
 	/// </returns>
-	public static implicit operator UserSpecification(VkResponse response) => response?._token == null || !response._token.HasValues
-		? null
-		: UserSpecification.FromJson(response);
-
-	/// <summary>
-	/// Преобразовать из VkResponse
-	/// </summary>
-	/// <param name="response"> Ответ. </param>
-	/// <returns>
-	/// Результат преобразования.
-	/// </returns>
 	public static implicit operator CommentDonut(VkResponse response) => response?._token == null || !response._token.HasValues
 		? null
 		: CommentDonut.FromJson(response);
