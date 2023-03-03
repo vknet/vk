77d76e43843eca3529266bcedef3d62b7e6d932f
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-03-03 17:55:01 +0200

    Удаляет FromJson для ChatPreview (#1334)
    * Убрал неиспользуемые зависимости
    
    * Удаляет FromJson из категории Donut
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson из категории Ads
    
    * Разделил на 2 класса
    
    * Удаляет FromJson из AppImage
    
    * Убрал неиспользуемые зависимости
    
    * Убрал FromJson
    
    * Добавил JsonProperty
    
    * Удалил FromJson
    
    * Удалил FromJson из GroupMember
    
    * Убрал FromJson
    
    * Убрал FromJson
    
    * Удалил FromJson
    
    * Удаляет FromJson из Keyboard
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Проставил Call<T>
    
    * Проставил Call<T>
    
    * Удаляет FromJson
    
    * Проставил Call<T>
    
    * Удаляет FromJson
    
    * Проставил Call<T>
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Исправил значение енам
    
    * Исправил расположение енама
    
    * Исправил расположение моделей
    
    * Добавил атрибут JsonConverter
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Добавил атрибут JsonProperty
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson WallGetObject
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    * Удаляет FromJson
    
    ---------
    
    Co-authored-by: inyutin-maxim <inyutin_maxim@mail.ru>

--- VkNet/Utils/VkResponseToModelCastsGenerator.cs
+++ VkNet/Utils/VkResponseToModelCastsGenerator.cs
@@ -46,17 +46,6 @@ public partial class VkResponse
 	/// <returns>
 	/// Результат преобразования.
 	/// </returns>
-	public static implicit operator ChatPreviewField(VkResponse response) => response?._token == null || !response._token.HasValues
-		? null
-		: ChatPreviewField.FromJson(response);
-
-	/// <summary>
-	/// Преобразовать из VkResponse
-	/// </summary>
-	/// <param name="response"> Ответ. </param>
-	/// <returns>
-	/// Результат преобразования.
-	/// </returns>
 	public static implicit operator ChatPreview(VkResponse response) => response?._token == null || !response._token.HasValues
 		? null
 		: ChatPreview.FromJson(response);
