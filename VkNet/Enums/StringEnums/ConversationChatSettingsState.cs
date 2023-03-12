571d20a1cfb52755fff33ddebc82f8a5c5e49958
Panuchi <tim.jaager@gmail.com>
2023-03-12 19:35:58 +0200

    Заменяет SafetyEnum на StringEnum для ConversationChatSettingsState

---
+++ VkNet/Enums/StringEnums/ConversationChatSettingsState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace VkNet.Enums.SafetyEnums;
+
+/// <summary>
+/// Настройки беседы
+/// </summary>
+[StringEnum]
+[JsonConverter(typeof(StringEnumConverter), typeof(SnakeCaseNamingStrategy))]
+public enum ConversationChatSettingsState
+{
+	/// <summary>
+	/// Состоит в чате.
+	/// </summary>
+	In,
+
+	/// <summary>
+	/// Исключён из чата.
+	/// </summary>
+	Kicked,
+
+	/// <summary>
+	/// Покинул чат.
+	/// </summary>
+	Left
+}
