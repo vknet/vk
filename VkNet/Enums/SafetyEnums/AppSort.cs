e8af54b81f02de919c8b141d7cbc82007f7a0329
Panuchi <tim.jaager@gmail.com>
2023-03-13 00:44:54 +0200

    Заменяет SafetyEnum на StringEnum для AppSort

--- VkNet/Enums/SafetyEnums/AppSort.cs
+++ VkNet/Enums/SafetyEnums/AppSort.cs
@@ -1,35 +1,40 @@
-﻿using VkNet.Utils;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using VkNet.Utils;
 
 namespace VkNet.Enums.SafetyEnums;
 
 /// <summary>
 /// Способ сортировки приложений
 /// </summary>
-public sealed class AppSort : SafetyEnum<AppSort>
+[StringEnum]
+[JsonConverter(typeof(StringEnumConverter), typeof(SnakeCaseNamingStrategy))]
+public enum AppSort
 {
 	/// <summary>
 	/// Популярные за день (по умолчанию);
 	/// </summary>
 	[DefaultValue]
-	public static readonly AppSort PopularToday = RegisterPossibleValue(value: "popular_today");
+	PopularToday,
 
 	/// <summary>
 	/// По посещаемости
 	/// </summary>
-	public static readonly AppSort Visitors = RegisterPossibleValue(value: "visitors");
+	Visitors,
 
 	/// <summary>
 	/// По дате создания приложения
 	/// </summary>
-	public static readonly AppSort CreateDate = RegisterPossibleValue(value: "create_date");
+	CreateDate,
 
 	/// <summary>
 	/// По скорости роста
 	/// </summary>
-	public static readonly AppSort GrowthRate = RegisterPossibleValue(value: "growth_rate");
+	GrowthRate,
 
 	/// <summary>
 	/// Популярные за неделю.
 	/// </summary>
-	public static readonly AppSort PopularWeek = RegisterPossibleValue(value: "popular_week");
+	PopularWeek
 }
