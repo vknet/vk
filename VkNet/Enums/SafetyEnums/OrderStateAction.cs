8aa758093c3af9933cb3593659b808f3f52df766
Panuchi <tim.jaager@gmail.com>
2023-03-12 03:46:33 +0200

    Заменяет SafetyEnum на StringEnum для OrderStateAction

--- VkNet/Enums/SafetyEnums/OrderStateAction.cs
+++ VkNet/Enums/SafetyEnums/OrderStateAction.cs
@@ -1,26 +1,30 @@
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 namespace VkNet.Enums.SafetyEnums;
 
 /// <summary>
 /// Действие статуса заказа.
 /// </summary>
-[Serializable]
-public sealed class OrderStateAction : SafetyEnum<OrderStateAction>
+[StringEnum]
+[JsonConverter(typeof(StringEnumConverter), typeof(SnakeCaseNamingStrategy))]
+public enum OrderStateAction
 {
 	/// <summary>
 	/// cancel — отменить неподтверждённый заказ.
 	/// </summary>
-	public static readonly OrderStateAction Cancel = RegisterPossibleValue("cancel");
+	Cancel,
 
 	/// <summary>
 	/// charge — подтвердить неподтверждённый заказ.
 	/// Применяется только если не удалось обработать уведомление order_change_state.
 	/// </summary>
-	public static readonly OrderStateAction Charge = RegisterPossibleValue("charge");
+	Charge,
 
 	/// <summary>
 	/// cancel — отменить неподтверждённый заказ.
 	/// </summary>
-	public static readonly OrderStateAction Refund = RegisterPossibleValue("refund");
+	Refund
 }
