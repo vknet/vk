205e28b9110032fd847df146a47b29966a18ed14
Panuchi <tim.jaager@gmail.com>
2023-03-13 03:08:24 +0200

    Заменяет SafetyEnum на StringEnum для OrderState

---
+++ VkNet/Utils/JsonConverter/OrderStateJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Exception;
+using VkNet.Model;
+
+namespace VkNet.Utils.JsonConverter;
+
+/// <summary>
+///
+/// </summary>
+public class OrderStateJsonConverter : StringEnumConverter
+{
+	public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+	{
+		var obj = JObject.Load(reader);
+		var responseJToken = obj["response"] ?? obj;
+		var response = new VkResponse(responseJToken);
+
+		return Utilities.Deserialize<OrderState>(response);
+	}
+}
