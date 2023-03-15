7a931df4ea7cb8d1d26eb1116384a2c5af1631c4
Panuchi <tim.jaager@gmail.com>
2023-03-15 19:56:18 +0200

    5.105 В объект приложения добавлены 2 новых поля #994

---
+++ VkNet/Enums/MobileControlsType.cs
@@ -0,0 +1,22 @@
+﻿namespace VkNet.Enums;
+
+/// <summary>
+/// Описывает, как отображаются элементы управления для игр в вебвью в нативных клиентах
+/// </summary>
+public enum MobileControlsType
+{
+	/// <summary>
+	/// Чёрная полоска над областью с игрой
+	/// </summary>
+	BlackLine = 0,
+
+	/// <summary>
+	/// Прозрачный элемент управления поверх области с игрой;
+	/// </summary>
+	Transparent = 1,
+
+	/// <summary>
+	/// Только для vk apps, без контроллов
+	/// </summary>
+	WithoutControllers = 2
+}
