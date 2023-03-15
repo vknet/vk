4e79b3757ddb4489f634a8fbed7c06583c6f6381
Panuchi <tim.jaager@gmail.com>
2023-03-15 19:19:05 +0200

    Убрал лишние свойства (используются в другом объекте в качестве параметров) - дубликат

--- VkNet/Model/Results/LeadForms/LeadFormCreateResult.cs
+++ VkNet/Model/Results/LeadForms/LeadFormCreateResult.cs
@@ -16,54 +16,6 @@ public class LeadFormCreateResult
 	public long? FormId { get; set; }
 
 	/// <summary>
-	/// Название.
-	/// </summary>
-	[JsonProperty("title")]
-	public string Title { get; set; }
-
-	/// <summary>
-	/// Описание.
-	/// </summary>
-	[JsonProperty("description")]
-	public string Description { get; set; }
-
-	[JsonProperty("confirmation")]
-	public string Confirmation { get; set; }
-
-	[JsonProperty("site_link_url")]
-	public string SiteLinkUrl { get; set; }
-
-	[JsonProperty("policy_link_url")]
-	public string PolicyLinkUrl { get; set; }
-
-	/// <summary>
-	/// Вопросы.
-	/// </summary>
-	[JsonProperty("questions")]
-	public LeadFormsQuestionInfo[] Questions { get; set; }
-
-	[JsonProperty("active")]
-	public long Active { get; set; }
-
-	/// <summary>
-	/// Количество заявок
-	/// </summary>
-	[JsonProperty("leads_count")]
-	public long LeadsCount { get; set; }
-
-	[JsonProperty("pixel_code")]
-	public string PixelCode { get; set; }
-
-	[JsonProperty("once_per_user")]
-	public long OncePerUser { get; set; }
-
-	[JsonProperty("notify_admins")]
-	public string NotifyAdmins { get; set; }
-
-	[JsonProperty("notify_emails")]
-	public string NotifyEmails { get; set; }
-
-	/// <summary>
 	/// Ссылка на форму
 	/// </summary>
 	[JsonProperty("url")]
