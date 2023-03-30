769704ce5921a712d7c16e4bacd689df30b8b62e
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-03-31 00:20:02 +0300

    Добавляет CancellationToken в StoriesCategory #815 (#1477)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Добавляет CancellationToken в AccountCategory
    
    * Добавляет CancellationToken в AdsCategory #815
    
    * Добавляет CancellationToken в AppsCategory #815
    
    * Добавляет CancellationToken в AppWidgetsCategory #815
    
    * Добавляет CancellationToken в AudioCategory #815
    
    * Добавляет CancellationToken в AuthCategory #815
    
    * Добавляет CancellationToken в BoardCategory #815
    
    * Рефакторинг #1447 #1448 #1449 #1450 #1451 #1452
    
    * Добавляет CancellationToken в DatabaseCategory #815
    
    * Применяет стили кода
    
    * Добавляет CancellationToken в DocsCategory #815
    
    * Добавляет CancellationToken в DonutCategory #815
    
    * Применяет стили кода
    
    * Добавляет CancellationToken в ExecuteCategory #815
    
    * Добавляет CancellationToken в FaveCategory #815
    
    * Добавляет CancellationToken в FriendsCategory #815
    
    * Добавляет CancellationToken в GiftsCategory #815
    
    * Добавляет CancellationToken в GroupsCategory #815
    
    * Добавляет CancellationToken в LeadFormsCategory #815
    
    * Добавляет CancellationToken в LeadsCategory #815
    
    * Добавляет CancellationToken в LikesCategory #815
    
    * Добавляет CancellationToken в MarketsCategory #815
    
    * Добавляет CancellationToken в MessagesCategory #815
    
    * Добавляет CancellationToken в NewsFeedCategory #815
    
    * Добавляет CancellationToken в NotesCategory #815
    
    * Применяет стили кода
    
    * Добавляет CancellationToken в OrdersCategory #815
    
    * Добавляет CancellationToken в PagesCategory #815
    
    * Добавляет CancellationToken в WidgetsCategory #815
    
    * Добавляет CancellationToken в WallCategory #815
    
    * Добавляет CancellationToken в VideoCategory #815
    
    * Добавляет CancellationToken в UtilsCategory #815
    
    * Добавляет CancellationToken в UsersCategory #815
    
    * Добавляет CancellationToken в StreamingCategory #815
    
    * Добавляет CancellationToken в StoriesCategory #815
    
    ---------
    
    Co-authored-by: inyutin-maxim <inyutin_maxim@mail.ru>

--- VkNet/Categories/Async/StoriesCategoryAsync.cs
+++ VkNet/Categories/Async/StoriesCategoryAsync.cs
@@ -13,76 +13,135 @@ namespace VkNet.Categories;
 public partial class StoriesCategory
 {
 	/// <inheritdoc/>
-	public Task<bool> BanOwnerAsync(IEnumerable<long> ownersIds) => TypeHelper.TryInvokeMethodAsync(() => BanOwner(ownersIds));
+	public Task<bool> BanOwnerAsync(IEnumerable<long> ownersIds,
+									CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			BanOwner(ownersIds));
 
 	/// <inheritdoc/>
-	public Task<bool> DeleteAsync(long ownerId, ulong storyId) => TypeHelper.TryInvokeMethodAsync(() => Delete(ownerId, storyId));
+	public Task<bool> DeleteAsync(long ownerId,
+								ulong storyId,
+								CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			Delete(ownerId, storyId));
 
 	/// <inheritdoc/>
-	public Task<StoryResult<IEnumerable<Story>>> GetAsync(long? ownerId = null, bool? extended = null) =>
-		TypeHelper.TryInvokeMethodAsync(() => Get(ownerId, extended));
+	public Task<StoryResult<IEnumerable<Story>>> GetAsync(long? ownerId = null,
+														bool? extended = null,
+														CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			Get(ownerId, extended));
 
 	/// <inheritdoc/>
-	public Task<StoryResult<long>> GetBannedAsync(IEnumerable<string> fields = null, bool? extended = null) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetBanned(extended, fields));
+	public Task<StoryResult<long>> GetBannedAsync(IEnumerable<string> fields = null,
+												bool? extended = null,
+												CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetBanned(extended, fields));
 
 	/// <inheritdoc/>
-	public Task<StoryResult<Story>> GetByIdAsync(IEnumerable<string> stories, bool? extended = null, IEnumerable<string> fields = null) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetById(stories, extended, fields));
+	public Task<StoryResult<Story>> GetByIdAsync(IEnumerable<string> stories,
+												bool? extended = null,
+												IEnumerable<string> fields = null,
+												CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetById(stories, extended, fields));
 
 	/// <inheritdoc/>
-	public Task<StoryServerUrl> GetPhotoUploadServerAsync(GetPhotoUploadServerParams @params) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetPhotoUploadServer(@params));
+	public Task<StoryServerUrl> GetPhotoUploadServerAsync(GetPhotoUploadServerParams @params,
+														CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetPhotoUploadServer(@params));
 
 	/// <inheritdoc/>
-	public Task<StoryResult<IEnumerable<Story>>> GetRepliesAsync(long ownerId, ulong storyId, string accessKey = null,
-																bool? extended = null, IEnumerable<string> fields = null) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetReplies(ownerId, storyId, accessKey, extended, fields));
+	public Task<StoryResult<IEnumerable<Story>>> GetRepliesAsync(long ownerId,
+																ulong storyId,
+																string accessKey = null,
+																bool? extended = null,
+																IEnumerable<string> fields = null,
+																CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetReplies(ownerId, storyId, accessKey, extended, fields));
 
 	/// <inheritdoc/>
-	public Task<StoryStatsResult> GetStatsAsync(long ownerId, ulong storyId) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetStats(ownerId, storyId));
+	public Task<StoryStatsResult> GetStatsAsync(long ownerId,
+												ulong storyId,
+												CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetStats(ownerId, storyId));
 
 	/// <inheritdoc/>
-	public Task<StoryServerUrl> GetVideoUploadServerAsync(GetVideoUploadServerParams @params) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetVideoUploadServer(@params));
+	public Task<StoryServerUrl> GetVideoUploadServerAsync(GetVideoUploadServerParams @params,
+														CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetVideoUploadServer(@params));
 
 	/// <inheritdoc/>
-	public Task<VkCollection<StoryViewers>> GetViewersAsync(long ownerId, ulong storyId, ulong? count = null, ulong? offset = null) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetViewers(ownerId, storyId, count, offset));
+	public Task<VkCollection<StoryViewers>> GetViewersAsync(long ownerId,
+															ulong storyId,
+															ulong? count = null,
+															ulong? offset = null,
+															CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetViewers(ownerId, storyId, count, offset));
 
 	/// <inheritdoc />
-	public Task<VkCollection<User>> GetViewersExtendedAsync(long ownerId, ulong storyId, ulong? count = null, ulong? offset = null) =>
-		TypeHelper.TryInvokeMethodAsync(() => GetViewersExtended(ownerId, storyId, count, offset));
+	public Task<VkCollection<User>> GetViewersExtendedAsync(long ownerId,
+															ulong storyId,
+															ulong? count = null,
+															ulong? offset = null,
+															CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetViewersExtended(ownerId, storyId, count, offset));
 
 	/// <inheritdoc/>
-	public Task<bool> HideAllRepliesAsync(long ownerId) => TypeHelper.TryInvokeMethodAsync(() => HideAllReplies(ownerId));
+	public Task<bool> HideAllRepliesAsync(long ownerId,
+										CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			HideAllReplies(ownerId));
 
 	/// <inheritdoc/>
-	public Task<bool> HideReplyAsync(long ownerId, ulong storyId, string accessKey = null) =>
-		TypeHelper.TryInvokeMethodAsync(() => HideReply(ownerId, storyId, accessKey));
+	public Task<bool> HideReplyAsync(long ownerId,
+									ulong storyId,
+									string accessKey = null,
+									CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			HideReply(ownerId, storyId, accessKey));
 
 	/// <inheritdoc/>
-	public Task<bool> UnbanOwnerAsync(IEnumerable<long> ownersIds) => TypeHelper.TryInvokeMethodAsync(() => UnbanOwner(ownersIds));
+	public Task<bool> UnbanOwnerAsync(IEnumerable<long> ownersIds,
+									CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			UnbanOwner(ownersIds));
 
 	/// <inheritdoc />
 	public Task<VkCollection<Story>> SaveAsync(StoryServerUrl uploadResults,
 												CancellationToken token) =>
-		TypeHelper.TryInvokeMethodAsync(() => Save(uploadResults));
+		TypeHelper.TryInvokeMethodAsync(() =>
+			Save(uploadResults));
 
 	/// <inheritdoc />
 	[Obsolete("Начиная с версии 5.118 используется только параметр uploadResults")]
-	public Task<VkCollection<Story>> SaveAsync(StoryServerUrl uploadResults, bool extended, IEnumerable<string> fields,
+	public Task<VkCollection<Story>> SaveAsync(StoryServerUrl uploadResults,
+												bool extended,
+												IEnumerable<string> fields,
 												CancellationToken token) =>
-		TypeHelper.TryInvokeMethodAsync(() => Save(uploadResults, extended, fields));
+		TypeHelper.TryInvokeMethodAsync(() =>
+			Save(uploadResults, extended, fields));
 
 	/// <inheritdoc />
-	public Task<StoryResult<Story>> SearchAsync(StoriesSearchParams searchParams, CancellationToken token) =>
-		TypeHelper.TryInvokeMethodAsync(() => Search(searchParams));
+	public Task<StoryResult<Story>> SearchAsync(StoriesSearchParams searchParams,
+												CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			Search(searchParams));
 
 	/// <inheritdoc />
-	public Task<bool> SendInteractionAsync(string accessKey, string message, bool? isBroadcast = null, bool? isAnonymous = null,
+	public Task<bool> SendInteractionAsync(string accessKey,
+											string message,
+											bool? isBroadcast = null,
+											bool? isAnonymous = null,
 											bool? unseenMarker = null,
-											CancellationToken token = default) => TypeHelper.TryInvokeMethodAsync(() =>
-		SendInteraction(accessKey, message, isBroadcast, isAnonymous, unseenMarker));
+											CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			SendInteraction(accessKey, message, isBroadcast, isAnonymous, unseenMarker));
 }
