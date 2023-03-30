a979e1f399300641693ff362380f5ddff9954d1e
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-03-31 00:18:03 +0300

    Добавляет CancellationToken в UtilsCategory #815 (#1474)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Добавляет CancellationToken в AccountCategory
    
    * Добавляет CancellationToken в AdsCategory #815
    
    * Добавляет CancellationToken в AppsCategory #815
    
    * Добавляет CancellationToken в AppWidgetsCategory #815
    
    * Добавляет CancellationToken в AudioCategory #815
    
    * Добавляет CancellationToken в AuthCategory #815
    
    * Добавляет CancellationToken в BoardCategory #815
    
    * Рефакторинг #1447 #1448 #1449 #1450 #1451 #1452
    
    * Добавляет CancellationToken в DatabaseCategory #815
    
    * Применяет стили кода
    
    * Добавляет CancellationToken в DocsCategory #815
    
    * Добавляет CancellationToken в DonutCategory #815
    
    * Применяет стили кода
    
    * Добавляет CancellationToken в ExecuteCategory #815
    
    * Добавляет CancellationToken в FaveCategory #815
    
    * Добавляет CancellationToken в FriendsCategory #815
    
    * Добавляет CancellationToken в GiftsCategory #815
    
    * Добавляет CancellationToken в GroupsCategory #815
    
    * Добавляет CancellationToken в LeadFormsCategory #815
    
    * Добавляет CancellationToken в LeadsCategory #815
    
    * Добавляет CancellationToken в LikesCategory #815
    
    * Добавляет CancellationToken в MarketsCategory #815
    
    * Добавляет CancellationToken в MessagesCategory #815
    
    * Добавляет CancellationToken в NewsFeedCategory #815
    
    * Добавляет CancellationToken в NotesCategory #815
    
    * Применяет стили кода
    
    * Добавляет CancellationToken в OrdersCategory #815
    
    * Добавляет CancellationToken в PagesCategory #815
    
    * Добавляет CancellationToken в WidgetsCategory #815
    
    * Добавляет CancellationToken в WallCategory #815
    
    * Добавляет CancellationToken в VideoCategory #815
    
    * Добавляет CancellationToken в UtilsCategory #815
    
    ---------
    
    Co-authored-by: inyutin-maxim <inyutin_maxim@mail.ru>

--- VkNet/Categories/Async/UtilsCategoryAsync.cs
+++ VkNet/Categories/Async/UtilsCategoryAsync.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
-using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
 using VkNet.Utils;
@@ -11,31 +11,50 @@ namespace VkNet.Categories;
 public partial class UtilsCategory
 {
 	/// <inheritdoc />
-	public Task<CheckLinkResult> CheckLinkAsync(string url) => TypeHelper.TryInvokeMethodAsync(func: () => CheckLink(url: url));
+	public Task<CheckLinkResult> CheckLinkAsync(string url,
+												CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			CheckLink(url));
 
 	/// <inheritdoc />
-	public Task<CheckLinkResult> CheckLinkAsync(Uri url) => TypeHelper.TryInvokeMethodAsync(func: () => CheckLink(url: url));
+	public Task<CheckLinkResult> CheckLinkAsync(Uri url,
+												CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			CheckLink(url));
 
 	/// <inheritdoc />
-	public Task<VkObject> ResolveScreenNameAsync(string screenName) =>
-		TypeHelper.TryInvokeMethodAsync(func: () => ResolveScreenName(screenName: screenName));
+	public Task<VkObject> ResolveScreenNameAsync(string screenName,
+												CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			ResolveScreenName(screenName));
 
 	/// <inheritdoc />
-	public Task<DateTime> GetServerTimeAsync() => TypeHelper.TryInvokeMethodAsync(func: () => GetServerTime());
+	public Task<DateTime> GetServerTimeAsync(CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(GetServerTime);
 
 	/// <inheritdoc />
-	public Task<ShortLink> GetShortLinkAsync(Uri url, bool isPrivate) =>
-		TypeHelper.TryInvokeMethodAsync(func: () => GetShortLink(url, isPrivate));
+	public Task<ShortLink> GetShortLinkAsync(Uri url,
+											bool isPrivate,
+											CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetShortLink(url, isPrivate));
 
 	/// <inheritdoc />
-	public Task<bool> DeleteFromLastShortenedAsync(string key) =>
-		TypeHelper.TryInvokeMethodAsync(func: () => DeleteFromLastShortened(key: key));
+	public Task<bool> DeleteFromLastShortenedAsync(string key,
+													CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			DeleteFromLastShortened(key));
 
 	/// <inheritdoc />
-	public Task<VkCollection<ShortLink>> GetLastShortenedLinksAsync(ulong count = 10, ulong offset = 0) =>
-		TypeHelper.TryInvokeMethodAsync(func: () => GetLastShortenedLinks(count, offset));
+	public Task<VkCollection<ShortLink>> GetLastShortenedLinksAsync(ulong count = 10,
+																	ulong offset = 0,
+																	CancellationToken token = default) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetLastShortenedLinks(count, offset));
 
 	/// <inheritdoc />
-	public Task<LinkStatsResult> GetLinkStatsAsync(LinkStatsParams @params) =>
-		TypeHelper.TryInvokeMethodAsync(func: () => GetLinkStats(@params: @params));
+	public Task<LinkStatsResult> GetLinkStatsAsync(LinkStatsParams @params,
+													CancellationToken token) =>
+		TypeHelper.TryInvokeMethodAsync(() =>
+			GetLinkStats(@params));
 }
