0f0ef9d9938b9a7bf214f8bb6722c46fa4579781
Panuchi <tim.jaager@gmail.com>
2023-03-30 21:43:52 +0300

    Удаляет FromJson для GroupTag

--- VkNet/Model/GroupTag.cs
+++ VkNet/Model/GroupTag.cs
@@ -27,39 +27,5 @@ namespace VkNet.Model
 		/// </summary>
 		[JsonProperty("color")]
 		public string Color { get; set; }
-
-		/// <summary>
-		/// Разобрать из json.
-		/// </summary>
-		/// <param name="response"> Ответ сервера. </param>
-		/// <returns> </returns>
-		public static GroupTag FromJson(VkResponse response)
-		{
-			var result = new GroupTag
-			{
-				Id = response["id"],
-				Name = response["name"],
-				Color = response["color"]
-			};
-
-			return result;
-		}
-
-		/// <summary>
-		/// Преобразование класса <see cref="GroupTag" /> из <see cref="VkResponse" />
-		/// </summary>
-		/// <param name="response"> Ответ сервера. </param>
-		/// <returns> Результат преобразования в <see cref="GroupTag" /> </returns>
-		public static implicit operator GroupTag(VkResponse response)
-		{
-			if (response == null)
-			{
-				return null;
-			}
-
-			return response.HasToken()
-				? FromJson(response)
-				: null;
-		}
 	}
 }
