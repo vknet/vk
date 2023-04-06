e4d12f66587125b11c49cb2e7bacc5d73f6fb43f
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-04-06 12:47:42 +0300

    #1497 Добавляет обработку загружаемого файла (#1502)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Merge vknet/vk:1095 without generators
    
    * Обновляет зависимости
    
    * Добавляет SourceGenerators
    
    * Удаляет неиспользуемый генератор кода
    
    * Удаляет неиспользуемые генераторы кода
    
    * Исправляет доку
    
    * Исправляет реализацию ISerializable
    
    * #573 Предупреждения статических анализаторов
    
    * #573 Предупреждения статических анализаторов
    
    * Удаляет пустую VkNet.Core.Abstractions
    
    * Удаляет пустые проекты
    
    * #573 Предупреждения статических анализаторов
    
    * Исправляет ITokenManager
    
    * Исправляет #1238
    
    * Исправляет #1238
    
    * Добавляет асинхронный двухфакторный вход
    
    * Исправляет асинхронный двухфакторный вход
    
    * Рефакторинг асинхронного двухфакторного входа
    
    * Добавляет постфикс Async к асинхронным методам
    
    * Добавляет значение CancellationToken по умолчанию
    
    * #1497 Добавляет обработку загружаемого файла
    
    ---------
    
    Co-authored-by: inyutin-maxim <inyutin_maxim@mail.ru>

---
+++ VkNet/Exception/InvalidFileException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+using VkNet.Model;
+using VkNet.Utils;
+
+namespace VkNet.Exception;
+
+/// <summary>
+/// Исключение, которое выбрасывается при попытке загрузки на сервер неправильного файла аудиозаписи.
+/// Попробуйте загрузить файл ещё раз.
+/// Код ошибки - 4611
+/// </summary>
+[Serializable]
+[VkError(VkErrorCode.InvalidFile)]
+public sealed class InvalidFileException : VkApiMethodInvokeException
+{
+	/// <inheritdoc />
+	public InvalidFileException(VkError response) : base(response)
+	{
+	}
+
+	/// <inheritdoc />
+	private InvalidFileException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(new())
+	{
+
+	}
+}
