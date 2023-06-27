758b3a25e4932cff0be857c85e0cc23af0aea8f5
Timofei Jääger <41389318+Panuchi@users.noreply.github.com>
2023-06-27 10:03:50 +0300

    Merge #1532 (#1534)
    * Добавляет Template в объект Message #1532
    
    * Добавляет Template в объект Message #1532 (merge)
    
    ---------
    
    Co-authored-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- VkNet.Tests/Categories/BotsLongPoll/BotsLongPollMessageTest.cs
+++ VkNet.Tests/Categories/BotsLongPoll/BotsLongPollMessageTest.cs
@@ -10,68 +10,6 @@ namespace VkNet.Tests.Categories.BotsLongPoll;
 public class BotsLongPollMessageTest : BotsLongPollBaseTest
 {
 	[Fact]
-	public void GetBotsLongPollHistory_MessageNewTemplateTest()
-	{
-		ReadCategoryJsonPath(nameof(GetBotsLongPollHistory_MessageNewTemplateTest));
-
-		var botsLongPollHistory = Api.Groups.GetBotsLongPollHistory(new()
-		{
-			Key = "test",
-			Server = "https://vk.com",
-			Ts = "0",
-			Wait = 10
-		});
-
-		botsLongPollHistory.Updates.Should()
-			.SatisfyRespectively(x =>
-			{
-				switch (x.Instance)
-				{
-					case GroupId:
-						x.Instance.Should()
-							.Be(new GroupId(123456789));
-
-						break;
-
-					case MessageNew:
-					{
-						var a = x.Instance is MessageNew b
-							? b
-							: null;
-
-						a.ClientInfo.ButtonActions.Should()
-							.NotBeEmpty();
-
-						a.ClientInfo.Keyboard.Should()
-							.BeTrue();
-
-						a.ClientInfo.InlineKeyboard.Should()
-							.BeFalse();
-
-						a.ClientInfo.LangId.Should()
-							.Be(Language.Ru);
-
-						a.Message.FromId.Should()
-							.Be(123456789);
-
-						a.Message.Text.Should()
-							.Be("f");
-
-						a.Message.Template.Type.Should()
-							.Be(TemplateType.Carousel);
-
-						a.Message.Template.Elements.FirstOrDefault()
-							.Photo
-							.HasTags.Should()
-							.BeFalse();
-
-						break;
-					}
-				}
-			});
-	}
-
-	[Fact]
 	public void GetBotsLongPollHistory_MessageNewTest()
 	{
 		ReadCategoryJsonPath(nameof(GetBotsLongPollHistory_MessageNewTest));
