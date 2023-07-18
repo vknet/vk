706fbce450a4ed686418fd224220c86d8c850df6
Timofei Jääger <tim.jaager@gmail.com>
2023-07-19 02:17:01 +0300

    Исправляет десериализацию Payload

---
+++ VkNet/Utils/JsonConverter/MessageEventPayloadJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VkNet.Utils.JsonConverter;
+
+/// <inheritdoc />
+public class MessageEventPayloadJsonConverter : Newtonsoft.Json.JsonConverter
+{
+	/// <inheritdoc />
+	public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => throw new NotImplementedException();
+
+	/// <inheritdoc />
+	/// <exception cref="T:System.TypeAccessException"> </exception>
+	public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+	{
+		var obj = JObject.Load(reader);
+		var responseJToken = obj["payload"] ?? obj;
+		var response = new VkResponse(responseJToken);
+
+		var a = Regex.Replace(response.ToString(), @"[ \r\n\t]", "");
+
+		return a;
+	}
+
+	/// <inheritdoc />
+	public override bool CanConvert(Type objectType) => true;
+}
