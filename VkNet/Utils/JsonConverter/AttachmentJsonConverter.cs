1a40f373e8d575864ef9acda3455cfec1541e238
Sem <9l@tut.by>
2023-07-20 13:46:49 +0500

    Сервис для обработки вложений с возможностью расширения

--- VkNet/Utils/JsonConverter/AttachmentJsonConverter.cs
+++ VkNet/Utils/JsonConverter/AttachmentJsonConverter.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using VkNet.Model;
+using VkNet.Utils;
 
 namespace VkNet.Utils.JsonConverter;
 
@@ -73,60 +74,12 @@ public class AttachmentJsonConverter : Newtonsoft.Json.JsonConverter
 
 		foreach (var item in obj)
 		{
-			var type = item["type"].ToString();
-
-			var attachment = type switch
-			{
-				"link" => CreateTyped(item[type].ToObject<Link>()),
-				"photo" or "posted_photo" => CreateTyped(item[type].ToObject<Photo>()),
-				"audio" => CreateTyped(item[type].ToObject<Audio>()),
-				"video" => CreateTyped(item[type].ToObject<Video>()),
-				"doc" => CreateTyped(item[type].ToObject<Document>()),
-				"podcast" => CreateTyped(item[type].ToObject<Podcast>()),
-				"article" => CreateTyped(item[type].ToObject<Article>()),
-				"event" => CreateTyped(item[type].ToObject<Event>()),
-				"graffiti" => CreateTyped(item[type].ToObject<Graffiti>()),
-				"money_transfer" => CreateTyped(item[type].ToObject<MoneyTransfer>()),
-				"money_request" => CreateTyped(item[type].ToObject<MoneyRequest>()),
-				"note" => CreateTyped(item[type].ToObject<Note>()),
-				"poll" => CreateTyped(item[type].ToObject<Poll>()),
-				"page" => CreateTyped(item[type].ToObject<Page>()),
-				"album" => CreateTyped(item[type].ToObject<Album>()),
-				"photos_list" => CreateTyped(item[type].ToObject<PhotosList>()),
-				"wall" => CreateTyped(item[type].ToObject<Wall>()),
-				"sticker" => CreateTyped(item[type].ToObject<Sticker>()),
-				"wall_reply" => CreateTyped(item[type].ToObject<WallReply>()),
-				"market_album" => CreateTyped(item[type].ToObject<MarketAlbum>()),
-				"market" => CreateTyped(item[type].ToObject<Market>()),
-				"pretty_cards" => CreateTyped(item[type].ToObject<PrettyCards>()),
-				"audio_message" => CreateTyped(item[type].ToObject<AudioMessage>()),
-				"call" => CreateTyped(item[type].ToObject<Call>()),
-				"story" => CreateTyped(item[type].ToObject<Story>()),
-				"audio_playlist" => CreateTyped(item[type].ToObject<AudioPlaylist>()),
-				var _ => CreateTyped(item[type].ToObject<UnknownAttachment>())
-			};
-
-			list.Add(attachment);
+			list.Add(AttachmentConverterService.Instance.FromJson(item));
 		}
 
 		return Activator.CreateInstance(vkCollection, list);
 	}
 
-	#region Приватные методы
-
-	private static Attachment CreateTyped<TAttachment>(TAttachment instance)
-		where TAttachment : MediaAttachment
-	{
-		var attachment = new Attachment
-		{
-			Type = typeof(TAttachment),
-			Instance = instance
-		};
-
-		return attachment;
-	}
-
-	#endregion
 
 	/// <inheritdoc />
 	public override bool CanConvert(Type objectType) => typeof(ReadOnlyCollection<>).IsAssignableFrom(c: objectType);
