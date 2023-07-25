250b6001b4fd3fd064c296becb9eb4a9016f0acf
Ivan Melentyev <ivan.saxalin@mail.ru>
2023-07-25 18:51:16 +0300

    Добавить обобщение для BotsLongPollHistoryResponse и метод GroupLongPoolHelpers.GetGroupUpdatesAndErrors

---
+++ VkNet/Utils/GroupLongPoolHelpers.cs
@@ -0,0 +1,36 @@
+#nullable enable
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using VkNet.Model;
+
+namespace VkNet.Utils;
+
+/// <summary>
+/// Методы для обработки событий лонгпула у сообществ.
+/// </summary>
+public static class GroupLongPoolHelpers
+{
+	/// <summary>
+	/// Метод для получения обновлений группы из массива JObject, который не бросает исключений, но вместе с обновлениями возвращает ошибки при десериализации, если таковые имеются.
+	/// </summary>
+	/// <param name="jObjectUpdates">Этот массив получается из метода api.Groups.GetBotsLongPollHistory&lt;BotsLongPollHistoryResponse&lt;JObject&gt;&gt;().Updates</param>
+	public static List<(GroupUpdate? Update, System.Exception? Exception)> GetGroupUpdatesAndErrors(List<JObject> jObjectUpdates)
+	{
+		var updates = new List<(GroupUpdate? Update, System.Exception? Exception)>();
+
+		foreach (var jObjectUpdate in jObjectUpdates)
+		{
+			try
+			{
+				var update = jObjectUpdate.ToObject<GroupUpdate>();
+				updates.Add((Update: update, Exception: null));
+			}
+			catch (System.Exception ex)
+			{
+				updates.Add((Update: null, Exception: ex));
+			}
+		}
+
+		return updates;
+	}
+}
