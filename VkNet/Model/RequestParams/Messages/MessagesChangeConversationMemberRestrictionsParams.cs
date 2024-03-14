b5429f87af9c91dadc5ecd95ceeb2e0a18daac60
xiadosw <xiadosw@gmail.com>
2024-03-14 09:43:26 +0300

    Add support for method messages.changeConversationMemberRestrictions (#1610)

---
+++ VkNet/Model/RequestParams/Messages/MessagesChangeConversationMemberRestrictionsParams.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using VkNet.Enums.StringEnums;
+
+namespace VkNet.Model;
+
+/// <summary>
+/// Список параметров для метода messages.changeConversationMemberRestrictions
+/// </summary>
+[Serializable]
+public class MessagesChangeConversationMemberRestrictionsParams
+{
+	/// <summary>
+	/// Идентификатор назначения.
+	/// </summary>
+	[JsonProperty("peer_id")]
+	public long PeerId { get; set; }
+
+	/// <summary>
+	/// Идентификаторы пользователей.
+	/// </summary>
+	[JsonProperty("member_ids")]
+	public IEnumerable<long> MemberIds { get; set; }
+
+	/// <summary>
+	/// Время в секундах. Если нужно замутить навсегда, то указывать не нужно.
+	/// </summary>
+	[JsonProperty("for", NullValueHandling = NullValueHandling.Ignore)]
+	public long For { get; set; }
+
+	/// <summary>
+	/// Разрешенные действия.
+	/// </summary>
+	[JsonProperty("action")]
+	public ConversationMemberRestrictionsActionType Action { get; set; }
+}
